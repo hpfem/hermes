d6635c716a5949ca5493beca46ea8843a9680f54
Lukas Korous <lukas.korous@gmail.com>
2010-09-11 10:13:21 +0200

    Adding storage specifiers for Win.

--- hermes2d/src/numerical_flux.h
+++ hermes2d/src/numerical_flux.h
@@ -17,13 +17,13 @@ double A_z(int i, int j, double w0, double w1, double w3, double w4);
 double matrix_R(int i, int j, double w0, double w1, double w3, double w4);
 double matrix_R_inv(int i, int j, double w0, double w1, double w3, double w4);
 double matrix_D_minus(int i, int j, double w0, double w1, double w3, double w4);
-void T_rot(double result[4][4], double beta);
-void dot_vector(double result[4], double A[4][4], double B[4]);
+void H2D_API T_rot(double result[4][4], double beta);
+void H2D_API dot_vector(double result[4], double A[4][4], double B[4]);
 void riemann_solver(double result[4], double w_l[4], double w_r[4]);
 void riemann_solver_invert(double result[4], double w_l[4], double w_r[4]);
 void numerical_flux(double result[4], double w_l[4], double w_r[4],
         double nx, double ny);
-double numerical_flux_i(int i, double w_l[4], double w_r[4],
+double H2D_API numerical_flux_i(int i, double w_l[4], double w_r[4],
         double nx, double ny);
 
 #endif
