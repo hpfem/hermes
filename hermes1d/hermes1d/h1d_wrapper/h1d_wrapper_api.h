152fe8ffa43d16e1bb2e92e3dd45d3b549885da2
Ondrej Certik <ondrej@certik.cz>
2010-09-11 18:22:31 -0700

    Commit generated cpp/h files

---
+++ hermes1d/hermes1d/h1d_wrapper/h1d_wrapper_api.h
@@ -0,0 +1,103 @@
+#ifndef __PYX_HAVE_API__hermes1d__h1d_wrapper__h1d_wrapper
+#define __PYX_HAVE_API__hermes1d__h1d_wrapper__h1d_wrapper
+#include "Python.h"
+
+static PyObject *(*c2py_Element)(Element *);
+static PyObject *(*c2py_Mesh)(Mesh *);
+
+#ifndef __PYX_HAVE_API_FUNC_import_module
+#define __PYX_HAVE_API_FUNC_import_module
+
+#ifndef __PYX_HAVE_RT_ImportModule
+#define __PYX_HAVE_RT_ImportModule
+static PyObject *__Pyx_ImportModule(const char *name) {
+    PyObject *py_name = 0;
+    PyObject *py_module = 0;
+
+    #if PY_MAJOR_VERSION < 3
+    py_name = PyString_FromString(name);
+    #else
+    py_name = PyUnicode_FromString(name);
+    #endif
+    if (!py_name)
+        goto bad;
+    py_module = PyImport_Import(py_name);
+    Py_DECREF(py_name);
+    return py_module;
+bad:
+    Py_XDECREF(py_name);
+    return 0;
+}
+#endif
+
+#endif
+
+
+#ifndef __PYX_HAVE_RT_ImportFunction
+#define __PYX_HAVE_RT_ImportFunction
+static int __Pyx_ImportFunction(PyObject *module, const char *funcname, void (**f)(void), const char *sig) {
+    PyObject *d = 0;
+    PyObject *cobj = 0;
+    union {
+        void (*fp)(void);
+        void *p;
+    } tmp;
+
+    d = PyObject_GetAttrString(module, (char *)"__pyx_capi__");
+    if (!d)
+        goto bad;
+    cobj = PyDict_GetItemString(d, funcname);
+    if (!cobj) {
+        PyErr_Format(PyExc_ImportError,
+            "%s does not export expected C function %s",
+                PyModule_GetName(module), funcname);
+        goto bad;
+    }
+#if PY_VERSION_HEX >= 0x02070000 && !(PY_MAJOR_VERSION==3&&PY_MINOR_VERSION==0)
+    if (!PyCapsule_IsValid(cobj, sig)) {
+        PyErr_Format(PyExc_TypeError,
+            "C function %s.%s has wrong signature (expected %s, got %s)",
+             PyModule_GetName(module), funcname, sig, PyCapsule_GetName(cobj));
+        goto bad;
+    }
+    tmp.p = PyCapsule_GetPointer(cobj, sig);
+#else
+    {const char *desc, *s1, *s2;
+    desc = (const char *)PyCObject_GetDesc(cobj);
+    if (!desc)
+        goto bad;
+    s1 = desc; s2 = sig;
+    while (*s1 != '\0' && *s1 == *s2) { s1++; s2++; }
+    if (*s1 != *s2) {
+        PyErr_Format(PyExc_TypeError,
+            "C function %s.%s has wrong signature (expected %s, got %s)",
+             PyModule_GetName(module), funcname, sig, desc);
+        goto bad;
+    }
+    tmp.p = PyCObject_AsVoidPtr(cobj);}
+#endif
+    *f = tmp.fp;
+    if (!(*f))
+        goto bad;
+    Py_DECREF(d);
+    return 0;
+bad:
+    Py_XDECREF(d);
+    return -1;
+}
+#endif
+
+static int import_hermes1d__h1d_wrapper__h1d_wrapper(void) {
+  PyObject *module = 0;
+  module = __Pyx_ImportModule("hermes1d.h1d_wrapper.h1d_wrapper");
+  if (!module) goto bad;
+  if (__Pyx_ImportFunction(module, "c2py_Element", (void (**)(void))&c2py_Element, "PyObject *(Element *)") < 0) goto bad;
+  if (__Pyx_ImportFunction(module, "c2py_Mesh", (void (**)(void))&c2py_Mesh, "PyObject *(Mesh *)") < 0) goto bad;
+  Py_DECREF(module); module = 0;
+  return 0;
+  bad:
+  Py_XDECREF(module);
+  return -1;
+}
+
+#endif
