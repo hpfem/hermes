c2cd39ac8a2a1657b30b06174e11d6def7292b35
Pavel Solin <solin.pavel@gmail.com>
2010-09-30 09:26:04 -0700

    File config.h.cmake fetched from master.

---
+++ hermes3d/src/config.h.cmake
@@ -0,0 +1,36 @@
+#cmakedefine WITH_UMFPACK
+#cmakedefine WITH_PARDISO
+#cmakedefine WITH_MUMPS
+#cmakedefine WITH_PETSC
+#cmakedefine WITH_HDF5
+#cmakedefine WITH_EXODUSII
+#cmakedefine WITH_MPI
+
+// trilinos
+#cmakedefine WITH_TRILINOS
+#cmakedefine HAVE_AMESOS
+#cmakedefine HAVE_AZTECOO
+#cmakedefine HAVE_EPETRA
+#cmakedefine HAVE_IFPACK
+#cmakedefine HAVE_ML
+#cmakedefine HAVE_NOX
+#cmakedefine HAVE_KOMPLEX
+
+#cmakedefine TRACING
+#cmakedefine DEBUG
+#cmakedefine DEBUG_ORDER
+
+// elements
+#cmakedefine WITH_TETRA
+#cmakedefine WITH_HEX
+#cmakedefine WITH_PRISM
+
+#cmakedefine PRELOADING
+
+/* --- */
+#define PACKAGE_BUGREPORT "dandrs@unr.edu"
+#define PACKAGE_NAME "Hermes3D"
+#define PACKAGE_TARNAME "hermes3d"
+#define PACKAGE_VERSION "0.0.1"
+#define PACKAGE_STRING "Hermes3D v" PACKAGE_VERSION
+
