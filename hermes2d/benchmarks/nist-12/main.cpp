e954e2240a8a3588eb8cbc3eb8e2b80f51eb0c2c
Erick Santiago <laviticus@sbcglobal.net>
2010-10-09 20:29:33 -0700

    New benchmark nist-12 in the testing phases - not yet complete

---
+++ hermes2d/benchmarks/nist-12/main.cpp
@@ -0,0 +1,212 @@
+#define H2D_REPORT_WARN
+#define H2D_REPORT_INFO
+#define H2D_REPORT_VERBOSE
+#define H2D_REPORT_FILE "application.log"
+#include "hermes2d.h"
+
+using namespace RefinementSelectors;
+
+//  This is the twelfth in the series of NIST benchmarks with known exact solutions.
+//
+//  Reference: W. Mitchell, A Collection of 2D Elliptic Problems for Testing Adaptive Algorithms, 
+//                          NIST Report 7668, February 2010.
+//
+//  PDE: -Laplace u = f.
+//
+//  Known exact solution: 
+//  See functions fn() and fndd() in "exact_solution.cpp".
+//
+//  Domain: L-shaped domain (-1,1)x(-1,1)\(0,1)x(-1,0), see the file "lshape.mesh".
+//
+//  BC:  Dirichlet, given by exact solution.
+//
+//  The following parameters can be changed:
+
+const int P_INIT = 3;                             // Initial polynomial degree of all mesh elements.
+const int INIT_REF_NUM = 1;                       // Number of initial uniform mesh refinements.
+const double THRESHOLD = 0.3;                     // This is a quantitative parameter of the adapt(...) function and
+                                                  // it has different meanings for various adaptive strategies (see below).
+const int STRATEGY = 0;                           // Adaptive strategy:
+                                                  // STRATEGY = 0 ... refine elements until sqrt(THRESHOLD) times total
+                                                  //   error is processed. If more elements have similar errors, refine
+                                                  //   all to keep the mesh symmetric.
+                                                  // STRATEGY = 1 ... refine all elements whose error is larger
+                                                  //   than THRESHOLD times maximum element error.
+                                                  // STRATEGY = 2 ... refine all elements whose error is larger
+                                                  //   than THRESHOLD.
+                                                  // More adaptive strategies can be created in adapt_ortho_h1.cpp.
+const CandList CAND_LIST = H2D_HP_ANISO_H;        // Predefined list of element refinement candidates. Possible values are
+                                                  // H2D_P_ISO, H2D_P_ANISO, H2D_H_ISO, H2D_H_ANISO, H2D_HP_ISO,
+                                                  // H2D_HP_ANISO_H, H2D_HP_ANISO_P, H2D_HP_ANISO.
+                                                  // See User Documentation for details.
+const int MESH_REGULARITY = -1;                   // Maximum allowed level of hanging nodes:
+                                                  // MESH_REGULARITY = -1 ... arbitrary level hangning nodes (default),
+                                                  // MESH_REGULARITY = 1 ... at most one-level hanging nodes,
+                                                  // MESH_REGULARITY = 2 ... at most two-level hanging nodes, etc.
+                                                  // Note that regular meshes are not supported, this is due to
+                                                  // their notoriously bad performance.
+const double CONV_EXP = 1.0;                      // Default value is 1.0. This parameter influences the selection of
+                                                  // cancidates in hp-adaptivity. See get_optimal_refinement() for details.
+const double ERR_STOP = 3.0;                     // Stopping criterion for adaptivity (rel. error tolerance between the
+                                                  // reference mesh and coarse mesh solution in percent).
+const int NDOF_STOP = 60000;                      // Adaptivity process stops when the number of degrees of freedom grows
+                                                  // over this limit. This is to prevent h-adaptivity to go on forever.
+MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESOS, SOLVER_MUMPS, 
+                                                  // SOLVER_PARDISO, SOLVER_PETSC, SOLVER_UMFPACK.
+
+// Problem parameters.                      
+double OMEGA = 3*M_PI/2;     //useful information about parameters will go here 
+double X_w = 0;              //
+double Y_w = -3/4;
+double R_0 = 3/4;
+double ALPHA_w = 200;
+double X_p = sqrt(5)/4;
+double Y_p = -1/4;
+double ALPHA_p = 1000;
+double EPSILON = 1/100;
+
+ 
+// Exact solution.
+#include "exact_solution.cpp"
+
+// Boundary condition types.
+BCType bc_types(int marker) { return BC_ESSENTIAL;}
+
+// Essential (Dirichlet) boundary condition values.
+scalar essential_bc_values(int ess_bdy_marker, double x, double y) { return fn(x, y);}
+
+// Weak forms.
+#include "forms.cpp"
+
+int main(int argc, char* argv[])
+{
+  // Load the mesh.
+  Mesh mesh;
+  H2DReader mloader;
+  mloader.load("lshape.mesh", &mesh);     // quadrilaterals
+
+  // Perform initial mesh refinements.
+  for (int i=0; i<INIT_REF_NUM; i++) mesh.refine_all_elements();
+
+  // Create an H1 space with default shapeset.
+  H1Space space(&mesh, bc_types, essential_bc_values, P_INIT);
+
+  // Initialize the weak formulation.
+  WeakForm wf;
+  wf.add_matrix_form(callback(bilinear_form), HERMES_SYM);
+  wf.add_vector_form(callback(linear_form));
+
+  // Initialize refinement selector.
+  H1ProjBasedSelector selector(CAND_LIST, CONV_EXP, H2DRS_DEFAULT_ORDER);
+
+  // Set exact solution.
+  ExactSolution exact(&mesh, fndd);
+
+  // Initialize views.
+  ScalarView sview("Solution", new WinGeom(0, 0, 440, 350));
+  sview.show_mesh(false);
+  OrderView  oview("Polynomial orders", new WinGeom(450, 0, 400, 350));
+
+  // DOF and CPU convergence graphs.
+  SimpleGraph graph_dof, graph_cpu, graph_dof_exact, graph_cpu_exact;
+
+  // Time measurement.
+  TimePeriod cpu_time;
+  cpu_time.tick();
+
+  // Adaptivity loop:
+  int as = 1;
+  bool done = false;
+  do
+  {
+    info("---- Adaptivity step %d:", as);
+
+    // Construct globally refined reference mesh and setup reference space.
+    Space* ref_space = construct_refined_space(&space);
+
+    // Assemble the reference problem.
+    info("Solving on reference mesh.");
+    bool is_linear = true;
+    FeProblem* fep = new FeProblem(&wf, ref_space, is_linear);
+    SparseMatrix* matrix = create_matrix(matrix_solver);
+    Vector* rhs = create_vector(matrix_solver);
+    Solver* solver = create_linear_solver(matrix_solver, matrix, rhs);
+    fep->assemble(matrix, rhs);
+
+    // Time measurement.
+    cpu_time.tick();
+
+    // Solve the linear system of the reference problem. If successful, obtain the solution.
+    Solution ref_sln;
+    if(solver->solve()) Solution::vector_to_solution(solver->get_solution(), ref_space, &ref_sln);
+    else error ("Matrix solver failed.\n");
+
+    // Time measurement.
+    cpu_time.tick();
+
+    // Project the fine mesh solution onto the coarse mesh.
+    Solution sln;
+    info("Projecting reference solution on coarse mesh.");
+    project_global(&space, &ref_sln, &sln, matrix_solver);
+
+    // View the coarse mesh solution and polynomial orders.
+    sview.show(&sln);
+    oview.show(&space);
+
+    // Calculate element errors and total error estimate.
+    info("Calculating error estimate and exact error.");
+    Adapt* adaptivity = new Adapt(&space, HERMES_H1_NORM);
+    adaptivity->set_solutions(&sln, &ref_sln);
+    double err_est_rel = adaptivity->calc_err_est(HERMES_TOTAL_ERROR_REL | HERMES_ELEMENT_ERROR_REL) * 100;
+
+    // Calculate exact error.   
+    double err_exact_rel = adaptivity->calc_err_exact(HERMES_TOTAL_ERROR_REL, &exact) * 100;
+
+    // Report results.
+    info("ndof_coarse: %d, ndof_fine: %d",
+      get_num_dofs(&space), get_num_dofs(ref_space));
+    info("err_est_rel: %g%%, err_exact_rel: %g%%", err_est_rel, err_exact_rel);
+
+    // Time measurement.
+    cpu_time.tick();
+
+    // Add entry to DOF and CPU convergence graphs.
+    graph_dof.add_values(get_num_dofs(&space), err_est_rel);
+    graph_dof.save("conv_dof_est.dat");
+    graph_cpu.add_values(cpu_time.accumulated(), err_est_rel);
+    graph_cpu.save("conv_cpu_est.dat");
+    graph_dof_exact.add_values(get_num_dofs(&space), err_exact_rel);
+    graph_dof_exact.save("conv_dof_exact.dat");
+    graph_cpu_exact.add_values(cpu_time.accumulated(), err_exact_rel);
+    graph_cpu_exact.save("conv_cpu_exact.dat");
+
+    // If err_est too large, adapt the mesh.
+    if (err_est_rel < ERR_STOP) done = true;
+    else
+    {
+      info("Adapting coarse mesh.");
+      done = adaptivity->adapt(&selector, THRESHOLD, STRATEGY, MESH_REGULARITY);
+
+      // Increase the counter of performed adaptivity steps.
+      if (done == false)  as++;
+    }
+    if (get_num_dofs(&space) >= NDOF_STOP) done = true;
+
+    // Clean up.
+    delete solver;
+    delete matrix;
+    delete rhs;
+    delete adaptivity;
+    if(done == false) delete ref_space->mesh;
+    delete ref_space;
+    delete fep;
+
+  }
+  while (done == false);
+
+  verbose("Total running time: %g s", cpu_time.accumulated());
+
+  // Wait for all views to be closed.
+  View::wait();
+  return 0;
+}
