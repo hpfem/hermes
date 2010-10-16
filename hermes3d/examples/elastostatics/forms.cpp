d41a1641ad2f5b802bccd1dd52489aef4cc13e1d
Lukas Korous <lukas.korous@gmail.com>
2010-10-16 11:01:14 +0200

    H3D elastostatics example.

--- hermes3d/examples/elastostatics/forms.cpp
+++ hermes3d/examples/elastostatics/forms.cpp
@@ -1,96 +1,96 @@
 // Integrals
-template<typename real, typename scalar>
-scalar int_a_dx_b_dy_c_dz(double a, double b, double c, int n, double *wt, fn_t<real> *u, fn_t<real> *v, geom_t<real> *e) 
+template<typename Real, typename Scalar>
+Scalar int_a_dx_b_dy_c_dz(double a, double b, double c, int n, double *wt, Func<Real> *u, Func<Real> *v, Geom<Real> *e) 
 {
-  scalar Integral = 0.0;
+  Scalar Integral = 0.0;
   for (int i = 0; i < n; i++)
     Integral += wt[i] * (a * u->dx[i] * v->dx[i] + b * u->dy[i] * v->dy[i] + c * u->dz[i] * v->dz[i]);
   return Integral;
 }
 
-template<typename real, typename scalar>
-scalar int_a_dudx_dvdy_b_dudy_dvdx(double a, double b, int n, double *wt, fn_t<real> *u, fn_t<real> *v, geom_t<real> *e) 
+template<typename Real, typename Scalar>
+Scalar int_a_dudx_dvdy_b_dudy_dvdx(double a, double b, int n, double *wt, Func<Real> *u, Func<Real> *v, Geom<Real> *e) 
 {
-  scalar Integral = 0.0;
+  Scalar Integral = 0.0;
   for (int i = 0; i < n; i++)
     Integral += wt[i] * (a * u->dx[i] * v->dy[i] + b * u->dy[i] * v->dx[i]);
   return Integral;
 }
 
-template<typename real, typename scalar>
-scalar int_a_dudx_dvdz_b_dudz_dvdx(double a, double b, int n, double *wt, fn_t<real> *u, fn_t<real> *v, geom_t<real> *e) 
+template<typename Real, typename Scalar>
+Scalar int_a_dudx_dvdz_b_dudz_dvdx(double a, double b, int n, double *wt, Func<Real> *u, Func<Real> *v, Geom<Real> *e) 
 {
-  scalar Integral = 0.0;
+  Scalar Integral = 0.0;
   for (int i = 0; i < n; i++)
     Integral += wt[i] * (a * u->dx[i] * v->dz[i] + b * u->dz[i] * v->dx[i]);
   return Integral;
 }
 
-template<typename real, typename scalar>
-scalar int_a_dudy_dvdz_b_dudz_dvdy(double a, double b, int n, double *wt, fn_t<real> *u, fn_t<real> *v, geom_t<real> *e) 
+template<typename Real, typename Scalar>
+Scalar int_a_dudy_dvdz_b_dudz_dvdy(double a, double b, int n, double *wt, Func<Real> *u, Func<Real> *v, Geom<Real> *e) 
 {
-  scalar Integral = 0.0;
+  Scalar Integral = 0.0;
   for (int i = 0; i < n; i++)
     Integral += wt[i] * (a * u->dy[i] * v->dz[i] + b * u->dz[i] * v->dy[i]);
   return Integral;
 }
 
 // 1. equation
-template<typename real, typename scalar>
-scalar bilinear_form_0_0(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_0_0(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dx_b_dy_c_dz<real, scalar>(lambda + 2*mu, mu, mu, n, wt, u, v, e);
+  return int_a_dx_b_dy_c_dz<Real, Scalar>(lambda + 2*mu, mu, mu, n, wt, u, v, e);
 }
 
-template<typename real, typename scalar>
-scalar bilinear_form_0_1(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_0_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dudx_dvdy_b_dudy_dvdx<real, scalar>(lambda, mu, n, wt, v, u, e);
+  return int_a_dudx_dvdy_b_dudy_dvdx<Real, Scalar>(lambda, mu, n, wt, v, u, e);
 }
 
-template<typename real, typename scalar>
-scalar bilinear_form_0_2(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_0_2(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dudx_dvdz_b_dudz_dvdx<real, scalar>(lambda, mu, n, wt, v, u, e);
+  return int_a_dudx_dvdz_b_dudz_dvdx<Real, Scalar>(lambda, mu, n, wt, v, u, e);
 }
 
-template<typename real, typename scalar>
-scalar surf_linear_form_0(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar surf_linear_form_0(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
   return 0.0;
 }
 
 // 2. equation
-template<typename real, typename scalar>
-scalar bilinear_form_1_1(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_1_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dx_b_dy_c_dz<real, scalar>(mu, lambda + 2*mu, mu, n, wt, u, v, e);
+  return int_a_dx_b_dy_c_dz<Real, Scalar>(mu, lambda + 2*mu, mu, n, wt, u, v, e);
 }
 
-template<typename real, typename scalar>
-scalar bilinear_form_1_2(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_1_2(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dudy_dvdz_b_dudz_dvdy<real, scalar>(lambda, mu, n, wt, v, u, e);
+  return int_a_dudy_dvdz_b_dudz_dvdy<Real, Scalar>(lambda, mu, n, wt, v, u, e);
 }
 
-template<typename real, typename scalar>
-scalar surf_linear_form_1(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar surf_linear_form_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
   return 0.0;
 }
 
 // 3. equation
-template<typename real, typename scalar>
-scalar bilinear_form_2_2(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *u, fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar bilinear_form_2_2(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  return int_a_dx_b_dy_c_dz<real, scalar>(mu, mu, lambda + 2*mu, n, wt, u, v, e);
+  return int_a_dx_b_dy_c_dz<Real, Scalar>(mu, mu, lambda + 2*mu, n, wt, u, v, e);
 }
 
-template<typename real, typename scalar>
-scalar surf_linear_form_2(int n, double *wt, fn_t<scalar> *u_ext[], fn_t<real> *v, geom_t<real> *e, user_data_t<scalar> *data) 
+template<typename Real, typename Scalar>
+Scalar surf_linear_form_2(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *data) 
 {
-  scalar res = 0.0;
+  Scalar res = 0.0;
   for (int i = 0; i < n; i++)
-    res += wt[i] * (f * v->fn[i]);
+    res += wt[i] * (f * v->val[i]);
   return res;
 }
--- hermes3d/examples/elastostatics/main.cpp
+++ hermes3d/examples/elastostatics/main.cpp
@@ -1,8 +1,9 @@
+#define H3D_REPORT_WARN
+#define H3D_REPORT_INFO
+#define H3D_REPORT_VERBOSE
 #include "config.h"
+//#include <getopt.h>
 #include <hermes3d.h>
-#include <common/trace.h>
-#include <common/timer.h>
-#include <common/error.h>
 
 // This example shows how to solve linear elasticity problem on a 
 // L-shape beam. Since we are dealing with only the isotropic 
@@ -16,11 +17,20 @@
 //     du_x/dn = du_y/dn = du_z/dn = 0 elsewhere
 //
 // The following parameters can be changed:
-
-const int INIT_REF_NUM = 1;			    // Number of initial uniform mesh refinements.
-const int P_INIT = 4;				    // Initial polynomial degree of all mesh elements.
-MatrixSolverType matrix_solver = SOLVER_UMFPACK;    // Possibilities: SOLVER_AMESOS, SOLVER_MUMPS, SOLVER_NOX, 
-                                                    // SOLVER_PARDISO, SOLVER_PETSC, SOLVER_UMFPACK.
+const int INIT_REF_NUM = 2;                       // Number of initial uniform mesh refinements.
+const int P_INIT_X = 2,
+          P_INIT_Y = 2,
+          P_INIT_Z = 2;                           // Initial polynomial degree of all mesh elements.
+bool solution_output = true;                      // Generate output files (if true).
+MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESOS, SOLVER_MUMPS, SOLVER_NOX, 
+                                                  // SOLVER_PARDISO, SOLVER_PETSC, SOLVER_UMFPACK.
+const char* iterative_method = "bicgstab";        // Name of the iterative method employed by AztecOO (ignored
+                                                  // by the other solvers). 
+                                                  // Possibilities: gmres, cg, cgs, tfqmr, bicgstab.
+const char* preconditioner = "jacobi";            // Name of the preconditioner employed by AztecOO (ignored by
+                                                  // the other solvers). 
+                                                  // Possibilities: none, jacobi, neumann, least-squares, or a
+                                                  //  preconditioner from IFPACK (see solver/aztecoo.h)                                                  
 
 // Problem parameters. 
 const double E  = 200e9; 		// Young modulus for steel: 200 GPa.
@@ -45,80 +55,67 @@ BCType bc_types_z(int marker)
   return (marker == 3) ? BC_ESSENTIAL : BC_NATURAL;
 }
 
+// Essential (Dirichlet) boundary condition values. 
+scalar essential_bc_values(int ess_bdy_marker, double x, double y, double z)
+{
+  return 0;
+}
+
 // Output the solutions.
 void out_fn(MeshFunction *x, MeshFunction *y, MeshFunction *z, const char *name) 
 {
   char fname[1024];
   sprintf(fname, "%s.vtk", name);
-  FILE *ofile = fopen(fname, "w");
-  if (ofile != NULL) {
-    VtkOutputEngine output(ofile);
-    output.out(x, y, z, name);
-    fclose(ofile);
+  FILE *f = fopen(fname, "w");
+  if (f != NULL) {
+    VtkOutputEngine vtk(f);
+    vtk.out(x, y, z, name);
+    fclose(f);
   }
-  else 
-    warning("Can not open '%s' for writing.", fname);
+  else warning("Could not open file '%s' for writing.", fname);
 }
 
 #include "forms.cpp"
 
-/***********************************************************************************
- * main program                                                                    *
- ***********************************************************************************/
-int main(int argc, char **argv) {
-  // Load the initial mesh. 
+int main(int argc, char **args) 
+{
+  // Time measurement.
+  TimePeriod cpu_time;
+  cpu_time.tick();
+
+  // Load the mesh. 
   Mesh mesh;
-  Mesh3DReader mloader;
+  H3DReader mloader;
   mloader.load("l-beam.mesh3d", &mesh);
 
-  // Initial uniform mesh refinements. 
-  printf("Performing %d initial mesh refinements.\n", INIT_REF_NUM);
+  // Perform initial mesh refinement. 
   for (int i=0; i < INIT_REF_NUM; i++) mesh.refine_all_elements(H3D_H3D_H3D_REFT_HEX_XYZ);
-  Word_t (nelem) = mesh.get_num_elements();
-  printf("New number of elements is %d.\n", (int) nelem);
-
-  // Initialize the shapeset and the cache. 
-  H1ShapesetLobattoHex shapeset;
-
-
 
   // Create H1 spaces x-displacement component. 
-  H1Space xdisp(&mesh, &shapeset);
-  xdisp.set_bc_types(bc_types_x);
-  xdisp.set_uniform_order(order3_t(P_INIT, P_INIT, P_INIT));
-
+  H1Space xdisp(&mesh, bc_types_x, essential_bc_values, Ord3(P_INIT_X, P_INIT_Y, P_INIT_Z));
+  
   // Create H1 spaces y-displacement component. 
-  H1Space ydisp(&mesh, &shapeset);
-  ydisp.set_bc_types(bc_types_y);
-  ydisp.set_uniform_order(order3_t(P_INIT, P_INIT, P_INIT));
-
+  H1Space ydisp(&mesh, bc_types_y, essential_bc_values, Ord3(P_INIT_X, P_INIT_Y, P_INIT_Z));
+  
   // Create H1 spaces z-displacement component. 
-  H1Space zdisp(&mesh, &shapeset);
-  zdisp.set_bc_types(bc_types_z);
-  zdisp.set_uniform_order(order3_t(P_INIT, P_INIT, P_INIT));
-
-  // Assign DOF. 
-  int ndofs = 0;
-  ndofs += xdisp.assign_dofs(ndofs);
-  ndofs += ydisp.assign_dofs(ndofs);
-  ndofs += zdisp.assign_dofs(ndofs);
-  printf("  - Number of DOFs: %d\n", ndofs);
-
+  H1Space zdisp(&mesh, bc_types_z, essential_bc_values, Ord3(P_INIT_X, P_INIT_Y, P_INIT_Z));
+  
   // Initialized the Weak formulation.
   WeakForm wf(3);
-  wf.add_matrix_form(0, 0, bilinear_form_0_0<double, scalar>, bilinear_form_0_0<ord_t, ord_t>, SYM);
-  wf.add_matrix_form(0, 1, bilinear_form_0_1<double, scalar>, bilinear_form_0_1<ord_t, ord_t>, SYM);
-  wf.add_matrix_form(0, 2, bilinear_form_0_2<double, scalar>, bilinear_form_0_2<ord_t, ord_t>, SYM);
-  wf.add_vector_form_surf(0, surf_linear_form_0<double, scalar>, surf_linear_form_0<ord_t, ord_t>);
+  wf.add_matrix_form(0, 0, callback(bilinear_form_0_0), HERMES_SYM);
+  wf.add_matrix_form(0, 1, callback(bilinear_form_0_1), HERMES_SYM);
+  wf.add_matrix_form(0, 2, callback(bilinear_form_0_2), HERMES_SYM);
+  wf.add_vector_form_surf(0, callback(surf_linear_form_0));
 
-  wf.add_matrix_form(1, 1, bilinear_form_1_1<double, scalar>, bilinear_form_1_1<ord_t, ord_t>, SYM);
-  wf.add_matrix_form(1, 2, bilinear_form_1_2<double, scalar>, bilinear_form_1_2<ord_t, ord_t>, SYM);
-  wf.add_vector_form_surf(1, surf_linear_form_1<double, scalar>, surf_linear_form_1<ord_t, ord_t>);
+  wf.add_matrix_form(1, 1, callback(bilinear_form_1_1), HERMES_SYM);
+  wf.add_matrix_form(1, 2, callback(bilinear_form_1_2), HERMES_SYM);
+  wf.add_vector_form_surf(1, callback(surf_linear_form_1));
 
-  wf.add_matrix_form(2, 2, bilinear_form_2_2<double, scalar>, bilinear_form_2_2<ord_t, ord_t>, SYM);
-  wf.add_vector_form_surf(2, surf_linear_form_2<double, scalar>, surf_linear_form_2<ord_t, ord_t>, 5);
+  wf.add_matrix_form(2, 2, callback(bilinear_form_2_2), HERMES_SYM);
+  wf.add_vector_form_surf(2, callback(surf_linear_form_2), 5);
 
-  // Initialize the FE problem.
+  // Assemble the linear problem.
+  info("Assembling the linear problem (ndof: %d).", Space::get_num_dofs(Tuple<Space *>(&xdisp, &ydisp, &zdisp)));
   bool is_linear = true;
   DiscreteProblem dp(&wf, Tuple<Space *>(&xdisp, &ydisp, &zdisp), is_linear);
 
@@ -127,43 +124,38 @@ int main(int argc, char **argv) {
   Vector* rhs = create_vector(matrix_solver);
   Solver* solver = create_linear_solver(matrix_solver, matrix, rhs);
 
-  // Assemble stiffness matrix
-  printf("  - Assembling... "); fflush(stdout);
-  Timer tmr_assemble;
-  tmr_assemble.start();
-  dp.assemble(matrix, rhs);
-  tmr_assemble.stop();
-  printf("done in %s (%lf secs)\n", tmr_assemble.get_human_time(), tmr_assemble.get_seconds());
-
-  // Solve the stiffness matrix.
-  printf("  - Solving... "); fflush(stdout);
-  Timer tmr_solve;
-  tmr_solve.start();
-  bool solved = solver->solve();
-  tmr_solve.stop();
-  if (solved)
-    printf("done in %s (%lf secs)\n", tmr_solve.get_human_time(), tmr_solve.get_seconds());
-  else {
-    printf("failed\n");
+  if (matrix_solver == SOLVER_AZTECOO) 
+  {
+    ((AztecOOSolver*) solver)->set_solver(iterative_method);
+    ((AztecOOSolver*) solver)->set_precond(preconditioner);
+    // Using default iteration parameters (see solver/aztecoo.h).
   }
 
-  // Construct a solution. 
-  double *solution_vector = solver->get_solution();
-  Solution xsln(&mesh), ysln(&mesh), zsln(&mesh);
-  xsln.set_coeff_vector(&xdisp, solution_vector);
-  ysln.set_coeff_vector(&ydisp, solution_vector);
-  zsln.set_coeff_vector(&zdisp, solution_vector);
+  dp.assemble(matrix, rhs);
 
-  // Output the solutions. 
-  printf("  - Output... "); fflush(stdout);
-  out_fn(&xsln, &ysln, &zsln, "disp");
-  printf("done\n");
+  // Solve the linear system of the reference problem. If successful, obtain the solution.
+  info("Solving the linear problem.");
+  Solution xsln(xdisp.get_mesh());
+  Solution ysln(ydisp.get_mesh());
+  Solution zsln(zdisp.get_mesh());
+  if(solver->solve()) Solution::vector_to_solutions(solver->get_solution(), Tuple<Space *>(&xdisp, &ydisp, &zdisp), Tuple<Solution *>(&xsln, &ysln, &zsln));
+  else error ("Matrix solver failed.\n");
+
+  // Output solution and mesh.
+  if (solution_output) 
+    out_fn(&xsln, &ysln, &zsln, "sln");
+  
+  // Time measurement.
+  cpu_time.tick();
+
+  // Print timing information.
+  info("Solutions saved. Total running time: %g s", cpu_time.accumulated());
 
   // Clean up.
-  delete solution_vector;
-  delete solver;
   delete matrix;
   delete rhs;
+  delete solver;
+  finalize_solution_environment(matrix_solver);
 
   return 0;
 }
