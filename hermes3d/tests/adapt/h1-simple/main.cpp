93b03085405d236f5c6e4180b7ab0e92304b5ccd
Lukas Korous <lukas.korous@gmail.com>
2010-10-19 19:07:19 +0200

    tests/adapt/h1-simple

--- hermes3d/tests/adapt/h1-simple/main.cpp
+++ hermes3d/tests/adapt/h1-simple/main.cpp
@@ -1,3 +1,6 @@
+#define H3D_REPORT_WARN
+#define H3D_REPORT_INFO
+#define H3D_REPORT_VERBOSE
 // This file is part of Hermes3D
 //
 // Copyright (c) 2009 hp-FEM group at the University of Nevada, Reno (UNR).
@@ -23,19 +26,25 @@
 //
 
 #include "config.h"
-#ifdef WITH_PETSC
-#include <petsc.h>
-#endif
+//#include <getopt.h>
 #include <hermes3d.h>
-#include <common/trace.h>
-#include <common/timer.h>
-#include <common/error.h>
 
+int P_INIT_X, P_INIT_Y, P_INIT_Z;       // Initial polynomial degree of all mesh elements taken from the command line arguments passed.
+const double ERR_STOP = 1.0;            // Stopping criterion for adaptivity (rel. error tolerance between the
+                                        // fine mesh and coarse mesh solution in percent).
+const int NDOF_STOP = 100000;           // Adaptivity process stops when the number of degrees of freedom grows
+                                        // over this limit. This is to prevent h-adaptivity to go on forever.
 MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESOS, SOLVER_MUMPS, SOLVER_NOX, 
                                                   // SOLVER_PARDISO, SOLVER_PETSC, SOLVER_UMFPACK.
-
-const double TOLERANCE = 0.001;		// error tolerance in percent
-const double THRESHOLD = 0.5;		// error threshold for element refinement
+const char* iterative_method = "bicgstab";        // Name of the iterative method employed by AztecOO (ignored
+                                                  // by the other solvers). 
+                                                  // Possibilities: gmres, cg, cgs, tfqmr, bicgstab.
+const char* preconditioner = "jacobi";            // Name of the preconditioner employed by AztecOO (ignored by
+                                                  // the other solvers). 
+                                                  // Possibilities: none, jacobi, neumann, least-squares, or a
+                                                  // preconditioner from IFPACK (see solver/aztecoo.h).
+const double TOLERANCE = 0.001;		                // error tolerance in percent
+const double THRESHOLD = 0.5;		                  // error threshold for element refinement
 
 int m = 2;
 int n = 2;
@@ -78,174 +87,160 @@ scalar essential_bc_values(int ess_bdy_marker, double x, double y, double z) {
 }
 
 template<typename f_t, typename res_t>
-res_t bilinear_form(int n, double *wt, fn_t<res_t> *u_ext[], fn_t<f_t> *u, fn_t<f_t> *v, geom_t<f_t> *e, user_data_t<res_t> *data) {
+res_t bilinear_form(int n, double *wt, Func<res_t> *u_ext[], Func<f_t> *u, Func<f_t> *v, Geom<f_t> *e, ExtData<res_t> *data) {
 	return int_grad_u_grad_v<f_t, res_t>(n, wt, u, v, e);
 }
 
 template<typename f_t, typename res_t>
-res_t linear_form(int n, double *wt, fn_t<res_t> *u_ext[], fn_t<f_t> *u, geom_t<f_t> *e, user_data_t<res_t> *data) {
+res_t linear_form(int n, double *wt, Func<res_t> *u_ext[], Func<f_t> *u, Geom<f_t> *e, ExtData<res_t> *data) {
 	return int_F_v<f_t, res_t>(n, wt, dfnc, u, e);
 }
 
 
 // main ///////////////////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char **argv) {
+int main(int argc, char **args) 
+{
 	int res = ERR_SUCCESS;
-
-	set_verbose(false);
 
 	if (argc < 5) error("Not enough parameters.");
 
-	H1ShapesetLobattoHex shapeset;
-
-	printf("* Loading mesh '%s'\n", argv[1]);
+	printf("* Loading mesh '%s'\n", args[1]);
 	Mesh mesh;
-	Mesh3DReader mloader;
-	if (!mloader.load(argv[1], &mesh)) error("Loading mesh file '%s'\n", argv[1]);
-
-	printf("* Setting the space up\n");
-	H1Space space(&mesh, &shapeset);
-	space.set_bc_types(bc_types);
-	space.set_essential_bc_values(essential_bc_values);
-
-	int o[3] = { 0, 0, 0 };
-	sscanf(argv[2], "%d", o + 0);
-	sscanf(argv[3], "%d", o + 1);
-	sscanf(argv[4], "%d", o + 2);
-	order3_t order(o[0], o[1], o[2]);
-	printf("  - Setting uniform order to (%d, %d, %d)\n", order.x, order.y, order.z);
-	space.set_uniform_order(order);
+	H3DReader mloader;
+	if (!mloader.load(args[1], &mesh)) error("Loading mesh file '%s'\n", args[1]);
+  
+  P_INIT_X = atoi(args[2]);
+  P_INIT_Y = atoi(args[3]);
+  P_INIT_Z = atoi(args[4]);
+	printf("* Setting the space up with initial polynomial degrees %d, %d, %d.\n", P_INIT_X, P_INIT_Y, P_INIT_Z);
+	H1Space space(&mesh, bc_types, essential_bc_values, Ord3(P_INIT_X, P_INIT_Y, P_INIT_Z));
 
 	WeakForm wf;
-	wf.add_matrix_form(bilinear_form<double, scalar>, bilinear_form<ord_t, ord_t>, SYM, HERMES_ANY);
-	wf.add_vector_form(linear_form<double, scalar>, linear_form<ord_t, ord_t>, HERMES_ANY);
+	wf.add_matrix_form(bilinear_form<double, scalar>, bilinear_form<Ord, Ord>, HERMES_SYM, HERMES_ANY);
+	wf.add_vector_form(linear_form<double, scalar>, linear_form<Ord, Ord>, HERMES_ANY);
 
-        bool is_linear = true;
-	DiscreteProblem lp(&wf, &space, is_linear);
+	// Time measurement.
+	TimePeriod cpu_time;
+	cpu_time.tick();
+  
+	// Initialize the solver in the case of SOLVER_PETSC or SOLVER_MUMPS.
+	initialize_solution_environment(matrix_solver, argc, args);
 
+	// Adaptivity loop.
+  int as = 1; 
 	bool done = false;
-	int iter = 0;
 	do {
-		Timer assemble_timer("Assembling stiffness matrix");
-		Timer solve_timer("Solving stiffness matrix");
+    info("---- Adaptivity step %d:", as);
 
-		printf("\n=== Iter #%d ================================================================\n", iter);
+    // Construct globally refined reference mesh and setup reference space.
+  	Space* ref_space = construct_refined_space(&space,1 , H3D_H3D_H3D_REFT_HEX_XYZ);
+  
+    out_orders_vtk(ref_space, "space", as);
+	
+	  // Initialize discrete problem.
+	  bool is_linear = true;
+	  DiscreteProblem lp(&wf, ref_space, is_linear);
+		
+	  // Set up the solver, matrix, and rhs according to the solver selection.
+    SparseMatrix* matrix = create_matrix(matrix_solver);
+    Vector* rhs = create_vector(matrix_solver);
+    Solver* solver = create_linear_solver(matrix_solver, matrix, rhs);
 
-		printf("\nSolution\n");
+    // Initialize the preconditioner in the case of SOLVER_AZTECOO.
+    if (matrix_solver == SOLVER_AZTECOO) 
+    {
+      ((AztecOOSolver*) solver)->set_solver(iterative_method);
+      ((AztecOOSolver*) solver)->set_precond(preconditioner);
+      // Using default iteration parameters (see solver/aztecoo.h).
+    }
 
-                // Set up the solver, matrix, and rhs according to the solver selection.
-                SparseMatrix* matrix = create_matrix(matrix_solver);
-                Vector* rhs = create_vector(matrix_solver);
-                Solver* solver = create_linear_solver(matrix_solver, matrix, rhs);
+    // Assemble the reference problem.
+    info("Assembling on reference mesh (ndof: %d).", Space::get_num_dofs(ref_space));
+    lp.assemble(matrix, rhs);
 
-		int ndof = space.assign_dofs();
-		printf("  - Number of DOF: %d\n", ndof);
+    // Time measurement.
+    cpu_time.tick();
 
-		// assemble stiffness matrix
-		printf("  - Assembling... "); fflush(stdout);
-		assemble_timer.reset();
-		assemble_timer.start();
-		lp.assemble(matrix, rhs);
-		assemble_timer.stop();
-		printf("done in %s (%lf secs)\n", assemble_timer.get_human_time(), assemble_timer.get_seconds());
+    // Solve the linear system on reference mesh. If successful, obtain the solution.
+    info("Solving on reference mesh.");
+    Solution ref_sln(ref_space->get_mesh());
+    if(solver->solve()) Solution::vector_to_solution(solver->get_solution(), ref_space, &ref_sln);
+    else {
+		  printf("Matrix solver failed.\n");
+		  res = ERR_FAILURE;
+	  }
+    
+    // Time measurement.
+    cpu_time.tick();
 
-		// solve the stiffness matrix
-		printf("  - Solving... "); fflush(stdout);
-		solve_timer.reset();
-		solve_timer.start();
-		bool solved = solver->solve();
-		solve_timer.stop();
-		if (solved)
-			printf("done in %s (%lf secs)\n", solve_timer.get_human_time(), solve_timer.get_seconds());
-		else {
-			res = ERR_FAILURE;
-			printf("failed\n");
-			break;
-		}
+    // Project the reference solution on the coarse mesh.
+    Solution sln(space.get_mesh());
+    info("Projecting reference solution on coarse mesh.");
+    OGProjection::project_global(&space, &ref_sln, &sln, matrix_solver);
 
-		printf("Reference solution\n");
+    // Time measurement.
+    cpu_time.tick();
 
-		Mesh rmesh;
-		rmesh.copy(mesh);
-		rmesh.refine_all_elements(H3D_H3D_H3D_REFT_HEX_XYZ);
+	  // Calculate element errors and total error estimate.
+    info("Calculating error estimate and exact error.");
+    Adapt *adaptivity = new Adapt(&space, HERMES_H1_NORM);
+    bool solutions_for_adapt = true;
+    double err_est_rel = adaptivity->calc_err_est(&sln, &ref_sln, solutions_for_adapt) * 100;
 
-		Space *rspace = space.dup(&rmesh);
-		rspace->copy_orders(space, 1);
+    // Report results.
+    info("ndof_coarse: %d, ndof_fine: %d.", Space::get_num_dofs(&space), Space::get_num_dofs(ref_space));
+    info("err_est_rel: %g%%.", err_est_rel);
 
-		DiscreteProblem rlp(&wf, rspace, is_linear);
+	  // If err_est_rel is too large, adapt the mesh. 
+    if (err_est_rel < ERR_STOP) {
+		  done = true;
+      ExactSolution ex_sln(&mesh, exact_solution);
+		  
+      // Calculation of norms of the solution and the error.
+		  double h1_sln_norm = h1_norm(&sln);
+		  double h1_err_norm = h1_error(&sln, &ex_sln);
+		  printf(" - H1 solution norm: % le\n", h1_sln_norm);
+		  printf(" - H1 error norm: % le\n", h1_err_norm);
+			
+		  double l2_sln_norm = l2_norm(&sln);
+		  double l2_err_norm = l2_error(&sln, &ex_sln);
+		  printf(" - L2 solution norm: % le\n", l2_sln_norm);
+		  printf(" - L2 error norm: % le\n", l2_err_norm);
+			
+      // If the calculated solution is not precise enough, the returned flag will be failure.
+		  if (h1_err_norm > EPS || l2_err_norm > EPS)
+		    res = ERR_FAILURE;
+	 
+      break;
+	  }	
+    else {
+      info("Adapting coarse mesh.");
+      adaptivity->adapt(THRESHOLD);
+    }
 
-		int rndof = rspace->assign_dofs();
-		printf("  - Number of DOF: %d\n", rndof);
+    // If we reached the maximum allowed number of degrees of freedom, set the return flag to failure.
+    if (Space::get_num_dofs(&space) >= NDOF_STOP)
+    {
+      done = true;
+      res = ERR_FAILURE;
+    }
 
-                // Set up the solver, matrix, and rhs according to the solver selection.
-                SparseMatrix* rmatrix = create_matrix(matrix_solver);
-                Vector* rrhs = create_vector(matrix_solver);
-                Solver* rsolver = create_linear_solver(matrix_solver, matrix, rhs);
+	  // Clean up.
+    delete ref_space->get_mesh();
+    delete ref_space;
+    delete matrix;
+    delete rhs;
+    delete solver;
+    delete adaptivity;
 
-		printf("  - Assembling... "); fflush(stdout);
-		assemble_timer.reset();
-		assemble_timer.start();
-		rlp.assemble(rmatrix, rrhs);
-		assemble_timer.stop();
-		printf("done in %s (%lf secs)\n", assemble_timer.get_human_time(), assemble_timer.get_seconds());
-
-		printf("  - Solving... "); fflush(stdout);
-		solve_timer.reset();
-		solve_timer.start();
-		bool rsolved = rsolver->solve();
-		solve_timer.stop();
-		if (rsolved)
-			printf("done in %s (%lf secs)\n", solve_timer.get_human_time(), solve_timer.get_seconds());
-		else {
-			res = ERR_FAILURE;
-			printf("failed\n");
-			break;
-		}
-
-		Solution sln(&mesh);
-		sln.set_coeff_vector(&space, solver->get_solution());
-
-		Solution rsln(&rmesh);
-		rsln.set_coeff_vector(rspace, rsolver->get_solution());
-
-		printf("Adaptivity:\n");
-		H1Adapt hp(&space);
-		double tol = hp.calc_error(&sln, &rsln) * 100;
-		printf("  - tolerance: "); fflush(stdout);
-		printf("% lf\n", tol);
-		if (tol < TOLERANCE) {
-			printf("\nDone\n");
-			ExactSolution ex_sln(&mesh, exact_solution);
-			// norm
-			double h1_sln_norm = h1_norm(&sln);
-			double h1_err_norm = h1_error(&sln, &ex_sln);
-			printf("  - H1 solution norm:   % le\n", h1_sln_norm);
-			printf("  - H1 error norm:      % le\n", h1_err_norm);
-
-			double l2_sln_norm = l2_norm(&sln);
-			double l2_err_norm = l2_error(&sln, &ex_sln);
-			printf("  - L2 solution norm:   % le\n", l2_sln_norm);
-			printf("  - L2 error norm:      % le\n", l2_err_norm);
-
-			if (h1_err_norm > EPS || l2_err_norm > EPS) {
-				// calculated solution is not enough precise
-				res = ERR_FAILURE;
-			}
-
-			break;
-		}
-
-		Timer t("");
-		printf("  - adapting... "); fflush(stdout);
-		t.start();
-		hp.adapt(THRESHOLD);
-		t.stop();
-		printf("done in %lf secs (refined %d element(s))\n", t.get_seconds(), hp.get_num_refined_elements());
-
-		iter++;
+    // Increase the counter of performed adaptivity steps.
+    as++;
 	} while (!done);
 
-	return res;
+  // Properly terminate the solver in the case of SOLVER_PETSC or SOLVER_MUMPS.
+  finalize_solution_environment(matrix_solver);
+  
+  return res;
 }
 
