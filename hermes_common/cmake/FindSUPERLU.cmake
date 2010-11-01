69e8cf57c5762254c1002e6c4e09d181f0e2a4ef
Milan Hanus <mhanus@kma.zcu.cz>
2010-11-01 17:48:59 +0100

    Making FindSUPERLU.cmake search through system locations only if SuperLU has not been found in the user supplied path.

--- hermes_common/cmake/FindSUPERLU.cmake
+++ hermes_common/cmake/FindSUPERLU.cmake
@@ -9,16 +9,19 @@ ELSEIF(SUPERLU_MT)
 ENDIF(SUPERLU_MT AND WITH_OPENMP)
 
 IF(POST)
-  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h ${SUPERLU_ROOT}/include/SLU_MT /usr/include/SLU_MT /usr/include/superlu/SLU_MT /usr/local/include/superlu/SLU_MT)
+  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h ${SUPERLU_ROOT}/include/SLU_MT NO_DEFAULT_PATH)
+  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h /usr/include/SLU_MT /usr/include/superlu/SLU_MT /usr/local/include/superlu/SLU_MT)
 ELSE(POST)
-  FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h ${SUPERLU_ROOT}/include /usr/include /usr/include/superlu /usr/local/include/superlu)
+  FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h ${SUPERLU_ROOT}/include NO_DEFAULT_PATH)
+  FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h /usr/include /usr/include/superlu /usr/local/include/superlu)
 ENDIF(POST)
 
 IF(MSVC)
   SET(PRE lib)
 ENDIF(MSVC)
 
-FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} ${SUPERLU_ROOT}/lib /usr/lib /usr/local/lib/SUPERLU)
+FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} ${SUPERLU_ROOT}/lib NO_DEFAULT_PATH)
+FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} /usr/lib /usr/local/lib/superlu)
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SUPERLU DEFAULT_MSG SUPERLU_LIBRARY SUPERLU_INCLUDE_DIR)
