082528d33d971f7da91725dea424115d74193ef7
Milan Hanus <mhanus@kma.zcu.cz>
2010-11-02 18:24:13 +0100

    Minor correction to FindSUPERLU.cmake.

--- hermes_common/cmake/FindSUPERLU.cmake
+++ hermes_common/cmake/FindSUPERLU.cmake
@@ -9,10 +9,10 @@ ELSEIF(SUPERLU_MT)
 ENDIF(SUPERLU_MT AND WITH_OPENMP)
 
 IF(POST)
-  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h ${SUPERLU_ROOT}/include/SLU_MT NO_DEFAULT_PATH)
-  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h /usr/include/SLU_MT /usr/include/superlu/SLU_MT /usr/local/include/superlu/SLU_MT)
+  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h ${SUPERLU_ROOT}/include/superlu_mt NO_DEFAULT_PATH)
+  FIND_PATH(SUPERLU_INCLUDE_DIR pdsp_defs.h /usr/include/superlu_mt /usr/local/include/superlu_mt)
 ELSE(POST)
-  FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h ${SUPERLU_ROOT}/include NO_DEFAULT_PATH)
+  FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h ${SUPERLU_ROOT}/include/superlu NO_DEFAULT_PATH)
   FIND_PATH(SUPERLU_INCLUDE_DIR slu_ddefs.h /usr/include /usr/include/superlu /usr/local/include/superlu)
 ENDIF(POST)
 
@@ -21,7 +21,7 @@ IF(MSVC)
 ENDIF(MSVC)
 
 FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} ${SUPERLU_ROOT}/lib NO_DEFAULT_PATH)
-FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} /usr/lib /usr/local/lib/superlu)
+FIND_LIBRARY( SUPERLU_LIBRARY ${PRE}superlu${POST} /usr/lib /usr/lib/superlu /usr/local/lib/superlu)
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SUPERLU DEFAULT_MSG SUPERLU_LIBRARY SUPERLU_INCLUDE_DIR)
