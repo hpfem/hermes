24ffc9dd3a4b00b35767af01755f7cc4a8102fb7
Ondrej Certik <ondrej@certik.cz>
2010-11-04 19:56:47 -0700

    Remove gcc specific code
    MSVC nor clang support C++0x yet, so I have removed that code completely for
    now, it should be rewritten anyway, so that we get the same behavior in MSVC
    and gcc.

--- hermes2d/src/views/scalar_view.h
+++ hermes2d/src/views/scalar_view.h
@@ -200,15 +200,9 @@ protected:
   bool is_constant; ///< true if the function to be displayed is constant
 
   // Perspective projection parameters.
-#ifdef _MSC_VER
-  static const int fovy;        ///< Field of view in the vertical direction (in degrees).
-  static const double znear;  ///< Distance of the near clipping plane of the viewing frustum from the camera.
-  static const double zfar;     ///< Distance of the Far clipping plane of the viewing frustum from the camera.
-#else
-	static const int fovy = 50;        ///< Field of view in the vertical direction (in degrees).
-  static const double znear = 0.05;  ///< Distance of the near clipping plane of the viewing frustum from the camera.
-  static const double zfar = 10;     ///< Distance of the Far clipping plane of the viewing frustum from the camera.
-#endif
+  int fovy;        ///< Field of view in the vertical direction (in degrees).
+  double znear;  ///< Distance of the near clipping plane of the viewing frustum from the camera.
+  double zfar;     ///< Distance of the Far clipping plane of the viewing frustum from the camera.
 
   bool pmode, mode3d, panning;
   double xrot, yrot, xtrans, ytrans, ztrans;
