d0c758671ee482c1d141aae48c00259b1629cbe7
Ondrej Certik <ondrej@certik.cz>
2010-11-23 20:37:13 -0800

    Position the solution properly

--- hermes2d/hermes2d/plot.py
+++ hermes2d/hermes2d/plot.py
@@ -30,17 +30,20 @@ def plot_sln_mayavi(sln, offscreen=False, show_scale=True):
         mlab.options.offscreen = True
     mlab.clf()
     s = mlab.triangular_mesh(x, y, z, triangles, scalars=t)
-    #mlab.view(0, 0)
+    mlab.view(0, 0)
+    mlab.view(distance=4)
+    mlab.view(focalpoint=(.25,0,0))
     mlab.colorbar(title="Solution", orientation="vertical")
 
+    #mlab.move(right=-1.0, up=-10.0)
     # Below is a code that does exactly what the "View along the +Z axis"
     # button does:
-    scene = mlab.get_engine().current_scene.scene
-    scene.camera.focal_point = [0, 0, 0]
-    scene.camera.position = [0, 0, 1]
-    scene.camera.view_up = [0, 1, 0]
-    scene.renderer.reset_camera()
-    scene.render()
+    #scene = mlab.get_engine().current_scene.scene
+    #scene.camera.focal_point = [0, 0, 0]
+    #scene.camera.position = [0, 0, 1]
+    #scene.camera.view_up = [0, 1, 0]
+    #scene.renderer.reset_camera()
+    #scene.render()
     # the above looks ok, but there is still quite a large margin, so we prefer
     # to just call .view(0, 0), which seems to be working fine.
     return s
