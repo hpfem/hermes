78259a39f079168a98eb75d85d34bbfef9328a3c
moritz.braun <moritz.braun@gmail.com>
2010-12-18 16:02:25 +0200

    main.cpp in for tutorial eigenvalue-h2p was cleaned up and timing inserted for matrix assembly writing matrices to disk and actual calculation of one eigenpair

--- hermes2d/tutorial/52-eigenvalue-h2p/main.cpp
+++ hermes2d/tutorial/52-eigenvalue-h2p/main.cpp
@@ -1,5 +1,3 @@
-#define H2D_REPORT_INFO
-#define H2D_REPORT_VERBOSE
 #include "hermes2d.h"
 #include <stdio.h>
 #include <cmath>
@@ -102,7 +100,7 @@ int main(int argc, char* argv[])
   }
   if (vertex_id_nucleus == -1)
     {
-      verbose("nucleus is not a vertex! Slow convergence would result");
+      printf("nucleus is not a vertex! Slow convergence would result\n");
       exit(0);
     }
   printf("vertex_id_nucleus=%d\n",vertex_id_nucleus);
@@ -121,20 +119,15 @@ int main(int argc, char* argv[])
   // Create an H1 space with default shapeset.
   H1Space space(&mesh, &bc_types, &bc_values, P_INIT);
   int ndof = Space::get_num_dofs(&space);
-  info("ndof: %d.", ndof);
-  NUM_ELEMS=mesh.get_max_element_id();
-  info("ndof = %d", ndof);
-  info("nelems = %d", NUM_ELEMS);
+  printf("ndof = %d\n", ndof);
   // Initialize the weak formulation for the left hand side, i.e., H.
   WeakForm wfH, wfU;
   wfH.add_matrix_form(bilinear_form_H,bilinear_form_ord);
   wfU.add_matrix_form(bilinear_form_U,bilinear_form_ord);
 
-
   // Initialize matrices.
   SparseMatrix* Hmat = create_matrix(matrix_solver);
   SparseMatrix* Umat = create_matrix(matrix_solver);
-
 
   // Assemble the matrices.
   bool is_linear = true;
@@ -142,21 +135,24 @@ int main(int argc, char* argv[])
   dpH.assemble(Hmat);
   DiscreteProblem dpU(&wfU, &space, is_linear);
   dpU.assemble(Umat);
-
-
   cpu_time.tick();
-  verbose("Total running time for assembling matrices : %g s", cpu_time.accumulated());
+  printf("Total running time for assembling matrices : %g s\n", cpu_time.accumulated());
   cpu_time.reset();
   write_matrix_mm("mat_left.mtx" ,Hmat);
   write_matrix_mm("mat_right.mtx", Umat);
-  info("Calling Pysparse...");
+  cpu_time.tick();
+  printf("Total running time for writing matrices to disk : %g s\n", cpu_time.accumulated());
+  cpu_time.reset();
+  printf("Calling JDSYM...\n");
   char call_cmd[255];
   double TOL=1e-10;
   int MAX_ITER=1000;
   sprintf(call_cmd, "python solveGenEigenFromMtx.py mat_left.mtx mat_right.mtx %g %d %g %d", 
 	  TARGET_VALUE, NUMBER_OF_EIGENVALUES, TOL, MAX_ITER);
   system(call_cmd);
-  info("Pysparse finished.");
+  printf("JDSYM finished.\n");
+  cpu_time.tick();
+  printf("Total running time for solving generalized eigenvalue problem: %g s\n", cpu_time.accumulated());
   double* coeff_vec = new double[ndof];
   Solution sln;
   ScalarView view("Solution", new WinGeom(0, 0, 1024, 768));
@@ -198,28 +194,6 @@ int main(int argc, char* argv[])
 
   double E=eigenval[0];
   printf("E=%.16f   Delta E=%.16e\n",E ,E-E_LITERATURE);
-  cpu_time.tick();
-  verbose("Total running time for solving generalized eigenvalue problem: %g s", cpu_time.accumulated());
-  // Convert double array to vector and coefficient vector into a Solution.
-  // for (int i=0;i<ndof;i++) eivec->set(i,abs(evec[i]));// ensure that the plotted eigenvector is positive
-  /* Solution* sln = new Solution(&space, eivec);
-  ScalarView view("Solution", new WinGeom(0, 0, 1024, 768));
-  */
-  /*
-  // this code prints the values of the solution on a grid 
-  // of points along the z axis.
-  double xp;
-  for (int i=0;i<4000;i++){
-    xp=i*0.001;
-    printf("%24.15e    %24.15e\n",xp,sln->get_pt_value(0.0,xp,H2D_FN_VAL_0));
-  }
-  */
-  // Visualize the solution.
-  //view.set_3d_mode();
-  // view.show(sln);
-  // Wait for the view to be closed.
-  //View::wait(H2DV_WAIT_KEYPRESS);
-  //  }  
   return 0; 
 };
 
