c85ac4259a0ee50ec22d408d7cf0bcf65e3a8c3c
Ondrej Certik <ondrej@certik.cz>
2010-12-18 21:11:25 +0100

    Implement assemble() internals

--- hermes2d/modules/schroedinger/schroedinger.cpp
+++ hermes2d/modules/schroedinger/schroedinger.cpp
@@ -40,5 +40,27 @@ Scalar bilinear_form_right(int n, double *wt, Func<Scalar> *u_ext[],
 
 void ModuleSchroedinger::assemble(const Ptr<Matrix> &A, const Ptr<Matrix> &B)
 {
+    Mesh mesh;
+    H2DReader mloader;
+    mloader.load("domain.mesh", &mesh);
+
+    BCTypes bc_types;
+    bc_types.add_bc_neumann(Hermes::Tuple<int>(BDY_BOTTOM, BDY_RIGHT,
+                BDY_TOP, BDY_LEFT));
+    BCValues bc_values;
+    bc_values.add_zero(Hermes::Tuple<int>(BDY_BOTTOM, BDY_RIGHT,
+                BDY_TOP, BDY_LEFT));
+
+    H1Space space(&mesh, &bc_types, &bc_values, P_INIT);
+    int ndof = Space::get_num_dofs(&space);
+    info("ndof: %d.", ndof);
+
     global_potential = this->potential;
+    WeakForm wf_left, wf_right;
+    wf_left.add_matrix_form(bilinear_form_left, bilinear_form_left_ord);
+    wf_right.add_matrix_form(callback(bilinear_form_right));
+    DiscreteProblem dp_left(&wf_left, &space, true);
+    DiscreteProblem dp_right(&wf_right, &space, true);
+    dp_left.assemble(A);
+    dp_right.assemble(A);
 }
