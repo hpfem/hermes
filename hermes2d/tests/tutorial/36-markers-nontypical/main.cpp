b009804813d96e3663bef11261957021107fdcb0
Ma Zhonghua <mazhonghua83@gmail.com>
2010-12-23 15:29:23 +0800

    H2D: adding tests for tutorial example 35 and 36.

--- hermes2d/tests/tutorial/36-markers-nontypical/main.cpp
+++ hermes2d/tests/tutorial/36-markers-nontypical/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace RefinementSelectors;
 
-// This test makes sure that example 35-markers works correctly.
+// This test makes sure that example 36-markers-nontypical works correctly.
 
 int P_INIT = 2;                                   // Initial polynomial degree of all mesh elements.
 const double THRESHOLD = 0.3;                     // This is a quantitative parameter of the adapt(...) function and
@@ -188,5 +188,4 @@ int main(int argc, char* argv[])
     printf("Failure!\n");
     return ERR_FAILURE;
   }
-
 }
