2be4dadfb1e3fdecfeef3c82467457de213375fd
Ma Zhonghua <mazhonghua83@gmail.com>
2010-12-24 10:26:21 +0800

    H2D: adding tests for tutorial 37 and 38.

---
+++ hermes2d/tests/tutorial/37-nurbs/forms.cpp
@@ -0,0 +1,13 @@
+template<typename Real, typename Scalar>
+Scalar bilinear_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, 
+                     Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext)
+{
+  return int_grad_u_grad_v<Real, Scalar>(n, wt, u, v);
+}
+
+template<typename Real, typename Scalar>
+Scalar linear_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, 
+                   Geom<Real> *e, ExtData<Scalar> *ext)
+{
+  return CONST_F * int_v<Real, Scalar>(n, wt, v);
+}
