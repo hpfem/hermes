d62bad1e8f95986e673872386aed9dda6eddac00
Milan Hanus <mhanus@kma.zcu.cz>
2011-01-06 21:34:07 +0100

    Stabilized-advection-reaction: Update of the exact solution.

--- hermes2d/benchmarks/stabilized-advection-reaction/exact/plotu.m
+++ hermes2d/benchmarks/stabilized-advection-reaction/exact/plotu.m
@@ -3,9 +3,17 @@ load sol_100x100.map
 [x,y,z]=deal(out(:,1),out(:,2),out(:,3));
 [X,Y]=meshgrid(0:0.01:1,0:0.01:1);
 Z = griddata(x,y,z,X,Y,'cubic');
-surf(X,Y,Z,'FaceColor','interp')
+
+figure;
+surf(X,Y,Z,'FaceColor','interp','LineStyle', 'none')
 lighting phong
 xlabel('x')
 ylabel('y')
 zlabel('u(x,y)')
-axis equal
+axis square
+
+figure;
+contourf(X,Y,Z)
+xlabel('x')
+ylabel('y')
+axis square
