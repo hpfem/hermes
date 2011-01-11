5ba8b7f4bbd29d8d6b26081fd79c0c7192e2d813
Lukas Korous <lukas.korous@gmail.com>
2011-01-11 12:35:15 +0100

    Unified long int types handling of H2D and H3D.
    Conflicts:
    
    	hermes_common/common.h

--- hermes_common/common.h
+++ hermes_common/common.h
@@ -438,5 +438,22 @@ class HERMES_API CommandLineArgs
 /** \param[in] text A text (a cause) of the exception. */
 extern HERMES_API void throw_exception(char *text);
 
+
+/// Int types handling.
+#ifdef JU_WIN
+typedef __int8           int8_t;
+typedef __int16          int16_t;
+typedef __int32          int32_t;
+typedef __int64          int64_t;
+
+typedef unsigned __int8  uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+
+#else
+#include <inttypes.h>
+#endif
+
 #endif
 
