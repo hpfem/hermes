30fbd805539b357b6fb111083e8bbad321d3cfa8
Ondrej Certik <ondrej@certik.cz>
2011-01-25 11:33:31 -0800

    Refactor the matrix creation

--- hermes2d/modules/schroedinger/eigensolver.cpp
+++ hermes2d/modules/schroedinger/eigensolver.cpp
@@ -5,20 +5,28 @@ namespace Schroedinger {
 using Teuchos::RCP;
 using Teuchos::Ptr;
 using Teuchos::rcp;
+using Teuchos::ptr;
 using Teuchos::null;
 using Teuchos::rcp_dynamic_cast;
+
+void wrap_CSC(const Ptr<Python> p, const std::string name,
+        const Ptr<CSCMatrix> A)
+{
+    p->push_numpy_int_inplace("_IA", A->get_Ai(), A->get_nnz());
+    p->push_numpy_int_inplace("_JA", A->get_Ap(), A->get_size()+1);
+    p->push_numpy_double_inplace("_A", A->get_Ax(), A->get_nnz());
+    p->push_int("n", A->get_size());
+    p->exec("from scipy.sparse import csc_matrix\n");
+    p->exec(name + " = csc_matrix((_A, _IA, _JA), shape=(n, n))");
+}
 
 void EigenSolver::solve() {
     // Support CSCMatrix only for now:
     RCP<CSCMatrix> A = rcp_dynamic_cast<CSCMatrix>(this->A);
     RCP<CSCMatrix> B = rcp_dynamic_cast<CSCMatrix>(this->B);
     Python p;
-    p.push_numpy_int_inplace("_IA", A->get_Ai(), A->get_nnz());
-    p.push_numpy_int_inplace("_JA", A->get_Ap(), A->get_size()+1);
-    p.push_numpy_double_inplace("_A", A->get_Ax(), A->get_nnz());
-    p.push_int("n", A->get_size());
-    p.exec("from scipy.sparse import csc_matrix\n");
-    p.exec("A = csc_matrix((_A, _IA, _JA), shape=(n, n))");
+    wrap_CSC(ptr(&p), "A", A);
+    wrap_CSC(ptr(&p), "B", B);
 
     printf("Solving the system A * x = lambda * B * x\n");
 }
