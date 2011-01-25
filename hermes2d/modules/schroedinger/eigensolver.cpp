5f48d8baffd0de2492315fd18120dc7fd4f00a53
Ondrej Certik <ondrej@certik.cz>
2011-01-25 11:16:20 -0800

    Cast to CSCMatrix

--- hermes2d/modules/schroedinger/eigensolver.cpp
+++ hermes2d/modules/schroedinger/eigensolver.cpp
@@ -6,8 +6,13 @@ using Teuchos::RCP;
 using Teuchos::Ptr;
 using Teuchos::rcp;
 using Teuchos::null;
+using Teuchos::rcp_dynamic_cast;
 
 void EigenSolver::solve() {
+    // Support CSCMatrix only for now:
+    RCP<CSCMatrix> A = rcp_dynamic_cast<CSCMatrix>(this->A);
+    RCP<CSCMatrix> B = rcp_dynamic_cast<CSCMatrix>(this->B);
+
     printf("Solving the system A * x = lambda * B * x\n");
 }
 
