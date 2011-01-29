5ee1404c0a9178dd30b205ff1703a897077797dd
moritz.braun <moritz.braun@gmail.com>
2011-01-29 07:26:24 +0200

    This commit adds a directory hartree-fock-neweigen to help debug the reason for a double free / corruption core dump

---
+++ hermes3d/examples/hartree-fock-neweigen/CMakeFiles/hartree-fock-neweigen.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/braunm/hermes/hermes3d/examples/hartree-fock-neweigen/main.cpp" "/home/braunm/hermes/hermes3d/examples/hartree-fock-neweigen/CMakeFiles/hartree-fock-neweigen.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "WITH_BLAS"
+  "NOGLUT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/braunm/hermes/hermes3d/src/CMakeFiles/hermes3d-debug.dir/DependInfo.cmake"
+  )
