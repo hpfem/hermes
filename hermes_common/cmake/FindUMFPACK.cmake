9e3ea70bd37554ecdc929e67b4b3349fdbbdd7d9
Sascha Schnepp <saschaschnepp@Pavel-Solins-Mac-Pro.local>
2011-02-05 13:54:56 -0800

    cmake finds UMFPACK on Mac.

--- hermes_common/cmake/FindUMFPACK.cmake
+++ hermes_common/cmake/FindUMFPACK.cmake
@@ -2,8 +2,8 @@
 # UMFPACK
 #
 
-FIND_PATH(UMFPACK_INCLUDE_DIR umfpack.h ${UMFPACK_ROOT}/include ${UMFPACK_ROOT}/Include /usr/include /usr/include/umfpack /usr/local/include/UMFPACK /usr/include/suitesparse)
-FIND_PATH(AMD_INCLUDE_DIR amd.h ${AMD_ROOT}/include ${AMD_ROOT}/Include /usr/include /usr/local/include/AMD /usr/include/suitesparse)
+FIND_PATH(UMFPACK_INCLUDE_DIR umfpack.h ${UMFPACK_ROOT}/include ${UMFPACK_ROOT}/Include /usr/include /usr/include/umfpack /usr/local/include/UMFPACK /usr/include/suitesparse /opt/local/include/ufsparse)
+FIND_PATH(AMD_INCLUDE_DIR amd.h ${AMD_ROOT}/include ${AMD_ROOT}/Include /usr/include /usr/local/include/AMD /usr/include/suitesparse /opt/local/include/ufsparse)
 
 FIND_LIBRARY(UMFPACK_LIBRARY NAMES libumfpack umfpack PATHS ${UMFPACK_ROOT}/bin ${UMFPACK_ROOT}/Lib /usr/lib /usr/local/lib/UMFPACK) 
 FIND_LIBRARY(AMD_LIBRARY NAMES libamd amd PATHS ${AMD_ROOT}/bin ${AMD_ROOT}/Lib /usr/lib /usr/local/lib/AMD)
