a07332259ecb4ed218eedbec1d1c890879565afb
Ma Zhonghua <mazhonghua83@gmail.com>
2011-02-09 02:55:22 +0800

    H2D: creating test for tutorial 09-timedep-basic.

--- hermes2d/tests/tutorial/09-timedep-basic/main.cpp
+++ hermes2d/tests/tutorial/09-timedep-basic/main.cpp
@@ -3,7 +3,7 @@
 #include "hermes2d.h"
 #include "runge_kutta.h"
 
-// This test makes sure that example 09-timedep-rk works correctly.
+// This test makes sure that example 09-timedep-basic works correctly.
 
 
 const int P_INIT = 2;                             // Polynomial degree of all mesh elements.
@@ -42,6 +42,7 @@ const double LAMBDA = 1e5;         // Thermal conductivity of the material.
 const double HEATCAP = 1e6;        // Heat capacity.
 const double RHO = 3000;           // Material density.
 const double T_FINAL = time_step*5;// Length of time interval (24 hours) in seconds.
+double current_time = time_step;
 
 // Time-dependent exterior temperature.
 template<typename Real>
@@ -89,56 +90,62 @@ int main(int argc, char* argv[])
   info("ndof = %d.", ndof);
  
   // Previous time level solution (initialized by the external temperature).
-  Solution* sln = new Solution(&mesh, TEMP_INIT);
+  Solution tsln(&mesh, TEMP_INIT);
 
   // Initialize weak formulation.
   WeakForm wf;
-  wf.add_matrix_form(callback(stac_jacobian_vol));
-  wf.add_vector_form(callback(stac_residual_vol), HERMES_ANY, sln);
-  wf.add_matrix_form_surf(callback(stac_jacobian_surf), BDY_AIR, sln);
-  wf.add_vector_form_surf(callback(stac_residual_surf), BDY_AIR, sln);
+  wf.add_matrix_form(callback(bilinear_form));
+  wf.add_matrix_form_surf(callback(bilinear_form_surf), BDY_AIR);
+  wf.add_vector_form(callback(linear_form), HERMES_ANY, &tsln);
+  wf.add_vector_form_surf(callback(linear_form_surf), BDY_AIR);
 
   // Initialize the FE problem.
-  bool is_linear = false;
+  bool is_linear = true;
   DiscreteProblem dp(&wf, &space, is_linear);
 
-  // Time stepping loop:
-  double current_time = time_step; int ts = 1;
+  // Set up the solver, matrix, and rhs according to the solver selection.
+  SparseMatrix* matrix = create_matrix(matrix_solver);
+  Vector* rhs = create_vector(matrix_solver);
+  Solver* solver = create_linear_solver(matrix_solver, matrix, rhs);
+  solver->set_factorization_scheme(HERMES_REUSE_FACTORIZATION_COMPLETELY);
+
+  // Time stepping:
+  int ts = 1; bool rhs_only = false;
   do 
   {
-    // Perform one Runge-Kutta time step according to the selected Butcher's table.
-    info("Runge-Kutta time step (t = %g s, tau = %g s, stages: %d).", 
-         current_time, time_step, bt.get_size());
-    bool verbose = true;
-    bool is_linear = true;
-    if (!rk_time_step(current_time, time_step, &bt, sln, &space, &dp, matrix_solver,
-		      verbose, is_linear)) {
-      error("Runge-Kutta time step failed, try to decrease time step size.");
-    }
+    info("---- Time step %d, time %3.5f s, ext_temp %g C", ts, current_time, temp_ext(current_time));
 
-    // Convert coeff_vec into a new time level solution.
-    //Solution::vector_to_solution(coeff_vec, &space, &u_prev_time);
+    // First time assemble both the stiffness matrix and right-hand side vector,
+    // then just the right-hand side vector.
+    if (rhs_only == false) info("Assembling the stiffness matrix and right-hand side vector.");
+    else info("Assembling the right-hand side vector (only).");
+    dp.assemble(matrix, rhs, rhs_only);
+    rhs_only = true;
+
+    // Solve the linear system and if successful, obtain the solution.
+    info("Solving the matrix problem.");
+    if(solver->solve()) Solution::vector_to_solution(solver->get_solution(), &space, &tsln);
+    else error ("Matrix solver failed.\n");
 
     // Increase current time and time step counter.
     current_time += time_step;
     ts++;
-  } 
+  }
   while (current_time < T_FINAL);
 
-  info("Coordinate (-2.0, 2.0) value = %lf", sln->get_pt_value(-2.0, 2.0));
-  info("Coordinate (-1.0, 2.0) value = %lf", sln->get_pt_value(-1.0, 2.0));
-  info("Coordinate ( 0.0, 2.0) value = %lf", sln->get_pt_value(0.0, 2.0));
-  info("Coordinate ( 1.0, 2.0) value = %lf", sln->get_pt_value(1.0, 2.0));
-  info("Coordinate ( 2.0, 2.0) value = %lf", sln->get_pt_value(2.0, 2.0));
+  info("Coordinate (-2.0, 2.0) value = %lf", tsln.get_pt_value(-2.0, 2.0));
+  info("Coordinate (-1.0, 2.0) value = %lf", tsln.get_pt_value(-1.0, 2.0));
+  info("Coordinate ( 0.0, 2.0) value = %lf", tsln.get_pt_value(0.0, 2.0));
+  info("Coordinate ( 1.0, 2.0) value = %lf", tsln.get_pt_value(1.0, 2.0));
+  info("Coordinate ( 2.0, 2.0) value = %lf", tsln.get_pt_value(2.0, 2.0));
 
   bool success = true;
 
-  if (fabs(sln->get_pt_value(-2.0, 2.0) - 10.000033) > 1E-6) success = false;
-  if (fabs(sln->get_pt_value(-1.0, 2.0) - 9.999999) > 1E-6) success = false;
-  if (fabs(sln->get_pt_value(0.0, 2.0) - 10.000005) > 1E-6) success = false;
-  if (fabs(sln->get_pt_value(1.0, 2.0) - 9.999999) > 1E-6) success = false;
-  if (fabs(sln->get_pt_value(2.0, 2.0) - 10.000033) > 1E-6) success = false;
-
+  if (fabs(tsln.get_pt_value(-2.0, 2.0) - 10.000122) > 1E-6) success = false;
+  if (fabs(tsln.get_pt_value(-1.0, 2.0) - 9.999985) > 1E-6) success = false;
+  if (fabs(tsln.get_pt_value(0.0, 2.0) - 10.000029) > 1E-6) success = false;
+  if (fabs(tsln.get_pt_value(1.0, 2.0) - 9.999985) > 1E-6) success = false;
+  if (fabs(tsln.get_pt_value(2.0, 2.0) - 10.000122) > 1E-6) success = false;
 
   if (success) {
     printf("Success!\n");
