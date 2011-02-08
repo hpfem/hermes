b056429104af8b172caaa18d88487e8ac68c0f93
Ma Zhonghua <mazhonghua83@gmail.com>
2011-02-09 02:41:34 +0800

    H2D: creating test for tutorial 09-timedep-rk.

--- hermes2d/tests/tutorial/09-timedep-rk/forms.cpp
+++ hermes2d/tests/tutorial/09-timedep-rk/forms.cpp
@@ -1,6 +1,6 @@
 template<typename Real, typename Scalar>
-Scalar stac_jacobian(int n, double *wt, Func<Real> *u_ext[], Func<Real> *u, Func<Real> *v, 
-                     Geom<Real> *e, ExtData<Scalar> *ext)
+Scalar stac_jacobian_vol(int n, double *wt, Func<Real> *u_ext[], Func<Real> *u, Func<Real> *v, 
+                         Geom<Real> *e, ExtData<Scalar> *ext)
 {
   Scalar result = 0;
   for (int i = 0; i < n; i++) {
@@ -11,10 +11,11 @@ Scalar stac_jacobian(int n, double *wt, Func<Real> *u_ext[], Func<Real> *u, Func
 }
 
 template<typename Real, typename Scalar>
-Scalar stac_residual(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v, 
-                   Geom<Real> *e, ExtData<Scalar> *ext)
+Scalar stac_residual_vol(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v, 
+                         Geom<Real> *e, ExtData<Scalar> *ext)
 {
-  Func<Scalar>* u_prev = u_ext[0];
+  Func<Scalar>* K_sln = u_ext[0];
+  Func<Scalar>* sln_prev_time = ext->fn[0];
 
   // This is a temporary workaround. The stage time t_n + h * c_i
   // can be accessed via u_stage_time->val[0];
@@ -25,8 +26,9 @@ Scalar stac_residual(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v,
 
   Scalar result = 0;
   for (int i = 0; i < n; i++) {
-    result += -wt[i] * (u_prev->dx[i] * v->dx[i] + u_prev->dy[i] * v->dy[i]);
-    result += wt[i] * heat_src(e->x[i], e->y[i]) * v->val[i];	       
+    Scalar sln_dx_i = sln_prev_time->dx[i] + K_sln->dx[i];
+    Scalar sln_dy_i = sln_prev_time->dy[i] + K_sln->dy[i];
+    result += -wt[i] * ( sln_dx_i * v->dx[i] +  sln_dy_i * v->dy[i]);	       
   }
 
   return result * LAMBDA / HEATCAP / RHO;
@@ -34,28 +36,30 @@ Scalar stac_residual(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v,
 
 
 template<typename Real, typename Scalar>
-Scalar bilinear_form_surf(int n, double *wt, Func<Real> *u_ext[], Func<Real> *u, Func<Real> *v, 
+Scalar stac_jacobian_surf(int n, double *wt, Func<Real> *u_ext[], Func<Real> *u, Func<Real> *v, 
                           Geom<Real> *e, ExtData<Scalar> *ext)
 {
   return - LAMBDA / HEATCAP / RHO * ALPHA * int_u_v<Real, Scalar>(n, wt, u, v);
 }
 
 template<typename Real, typename Scalar>
-Scalar linear_form_surf(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v, 
-                        Geom<Real> *e, ExtData<Scalar> *ext)
+Scalar stac_residual_surf(int n, double *wt, Func<Real> *u_ext[], Func<Real> *v, 
+                          Geom<Real> *e, ExtData<Scalar> *ext)
 {
-  Func<Scalar>* u_prev = u_ext[0];
+  Func<Scalar>* K_sln = u_ext[0];
+  Func<Scalar>* sln_prev_time = ext->fn[0];
 
   // This is a temporary workaround. The stage time t_n + h * c_i
   // can be accessed via u_stage_time->val[0];
-  Func<Scalar>* u_stage_time = ext->fn[0]; 
+  Func<Scalar>* u_stage_time = ext->fn[1]; 
   
   Scalar stage_time = u_stage_time->val[0];
   Real stage_ext_temp = temp_ext<Real>(stage_time);
 
   Scalar result = 0;
   for (int i = 0; i < n; i++) {
-    result += wt[i] * (stage_ext_temp - u_prev->val[i]) * v->val[i];		       
+    Scalar sln_val_i = sln_prev_time->val[i] + K_sln->val[i];
+    result += wt[i] * (stage_ext_temp - sln_val_i) * v->val[i];		       
   }
 
   return LAMBDA / HEATCAP / RHO * ALPHA * result;
--- hermes2d/tests/tutorial/09-timedep-rk/main.cpp
+++ hermes2d/tests/tutorial/09-timedep-rk/main.cpp
@@ -15,12 +15,20 @@ const int NEWTON_MAX_ITER = 100;                  // Maximum allowed number of N
 MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESOS, SOLVER_AZTECOO, SOLVER_MUMPS,
                                                   // SOLVER_PARDISO, SOLVER_PETSC, SOLVER_SUPERLU, SOLVER_UMFPACK.
 
-// Time integration. Choose one of the following methods, or define your own Butcher's table:
-// Explicit_RK_1, Implicit_RK_1, Explicit_RK_2, Implicit_Crank_Nicolson_2_2, Implicit_SIRK_2_2, 
-// Implicit_ESIRK_2_2, Implicit_SDIRK_2_2, Implicit_Lobatto_IIIA_2_2, Implicit_Lobatto_IIIB_2_2, 
-// Implicit_Lobatto_IIIC_2_2, Explicit_RK_3, Explicit_RK_4, Implicit_Lobatto_IIIA_3_4, 
-// Implicit_Lobatto_IIIB_3_4, Implicit_Lobatto_IIIC_3_4, Implicit_Radau_IIA_3_5, Implicit_SDIRK_4_5. 
-
+// Choose one of the following time-integration methods, or define your own Butcher's table. The last number 
+// in the name of each method is its order. The one before last, if present, is the number of stages.
+// Explicit methods:
+//   Explicit_RK_1, Explicit_RK_2, Explicit_RK_3, Explicit_RK_4.
+// Implicit methods: 
+//   Implicit_RK_1, Implicit_Crank_Nicolson_2_2, Implicit_SIRK_2_2, Implicit_ESIRK_2_2, Implicit_SDIRK_2_2, 
+//   Implicit_Lobatto_IIIA_2_2, Implicit_Lobatto_IIIB_2_2, Implicit_Lobatto_IIIC_2_2, Implicit_Lobatto_IIIA_3_4, 
+//   Implicit_Lobatto_IIIB_3_4, Implicit_Lobatto_IIIC_3_4, Implicit_Radau_IIA_3_5, Implicit_SDIRK_4_5.
+// Embedded explicit methods:
+//   Explicit_HEUN_EULER_2_12_embedded, Explicit_BOGACKI_SHAMPINE_4_23_embedded, Explicit_FEHLBERG_6_45_embedded,
+//   Explicit_CASH_KARP_6_45_embedded, Explicit_DORMAND_PRINCE_7_45_embedded.
+// Embedded implicit methods:
+//   Implicit_SDIRK_CASH_3_23_embedded, Implicit_ESDIRK_TRBDF2_3_23_embedded, Implicit_ESDIRK_TRX2_3_23_embedded, 
+//   Implicit_SDIRK_CASH_5_24_embedded, Implicit_SDIRK_CASH_5_34_embedded, Implicit_DIRK_7_45_embedded. 
 ButcherTableType butcher_table_type = Implicit_SDIRK_2_2;
 
 // Boundary markers.
@@ -33,7 +41,7 @@ const double ALPHA = 10;           // Heat flux coefficient for Newton's boundar
 const double LAMBDA = 1e5;         // Thermal conductivity of the material.
 const double HEATCAP = 1e6;        // Heat capacity.
 const double RHO = 3000;           // Material density.
-const double T_FINAL = 86400;      // Length of time interval (24 hours) in seconds.
+const double T_FINAL = time_step*5;// Length of time interval (24 hours) in seconds.
 
 // Time-dependent exterior temperature.
 template<typename Real>
@@ -52,6 +60,9 @@ int main(int argc, char* argv[])
 {
   // Choose a Butcher's table or define your own.
   ButcherTable bt(butcher_table_type);
+  if (bt.is_explicit()) info("Using a %d-stage explicit R-K method.", bt.get_size());
+  if (bt.is_diagonally_implicit()) info("Using a %d-stage diagonally implicit R-K method.", bt.get_size());
+  if (bt.is_fully_implicit()) info("Using a %d-stage fully implicit R-K method.", bt.get_size());
 
   // Load the mesh.
   Mesh mesh;
@@ -78,61 +89,58 @@ int main(int argc, char* argv[])
   info("ndof = %d.", ndof);
  
   // Previous time level solution (initialized by the external temperature).
-  Solution u_prev_time(&mesh, TEMP_INIT);
+  Solution* sln = new Solution(&mesh, TEMP_INIT);
 
   // Initialize weak formulation.
   WeakForm wf;
-  wf.add_matrix_form(callback(stac_jacobian));
-  wf.add_vector_form(callback(stac_residual));
-  wf.add_matrix_form_surf(callback(bilinear_form_surf), BDY_AIR);
-  wf.add_vector_form_surf(callback(linear_form_surf), BDY_AIR);
-
-  // Project the initial condition on the FE space to obtain initial solution coefficient vector.
-  info("Projecting initial condition to translate initial condition into a vector.");
-  scalar* coeff_vec = new scalar[ndof];
-  OGProjection::project_global(&space, &u_prev_time, coeff_vec, matrix_solver);
+  wf.add_matrix_form(callback(stac_jacobian_vol));
+  wf.add_vector_form(callback(stac_residual_vol), HERMES_ANY, sln);
+  wf.add_matrix_form_surf(callback(stac_jacobian_surf), BDY_AIR, sln);
+  wf.add_vector_form_surf(callback(stac_residual_surf), BDY_AIR, sln);
 
   // Initialize the FE problem.
   bool is_linear = false;
   DiscreteProblem dp(&wf, &space, is_linear);
 
   // Time stepping loop:
-  double current_time = 0.0; int ts = 1;
+  double current_time = time_step; int ts = 1;
   do 
   {
     // Perform one Runge-Kutta time step according to the selected Butcher's table.
-    info("Runge-Kutta time step (t = %g, tau = %g, stages: %d).", 
+    info("Runge-Kutta time step (t = %g s, tau = %g s, stages: %d).", 
          current_time, time_step, bt.get_size());
     bool verbose = true;
-    if (!rk_time_step(current_time, time_step, &bt, coeff_vec, &dp, matrix_solver,
-		      verbose, NEWTON_TOL, NEWTON_MAX_ITER)) {
+    bool is_linear = true;
+    if (!rk_time_step(current_time, time_step, &bt, sln, &space, &dp, matrix_solver,
+		      verbose, is_linear)) {
       error("Runge-Kutta time step failed, try to decrease time step size.");
     }
 
     // Convert coeff_vec into a new time level solution.
-    Solution::vector_to_solution(coeff_vec, &space, &u_prev_time);
+    //Solution::vector_to_solution(coeff_vec, &space, &u_prev_time);
 
-    // Update time.
+    // Increase current time and time step counter.
     current_time += time_step;
-
-    // Increase counter of time steps.
     ts++;
   } 
-  while (current_time < time_step*5);
+  while (current_time < T_FINAL);
 
-  double sum = 0;
-  for (int i = 0; i < ndof; i++) 
-    sum += coeff_vec[i];
+  info("Coordinate (-2.0, 2.0) value = %lf", sln->get_pt_value(-2.0, 2.0));
+  info("Coordinate (-1.0, 2.0) value = %lf", sln->get_pt_value(-1.0, 2.0));
+  info("Coordinate ( 0.0, 2.0) value = %lf", sln->get_pt_value(0.0, 2.0));
+  info("Coordinate ( 1.0, 2.0) value = %lf", sln->get_pt_value(1.0, 2.0));
+  info("Coordinate ( 2.0, 2.0) value = %lf", sln->get_pt_value(2.0, 2.0));
 
-  printf("sum = %g\n", sum);
+  bool success = true;
 
-  int success = 1;
-  if (fabs(sum - 3617.55) > 1e-1) success = 0;
+  if (fabs(sln->get_pt_value(-2.0, 2.0) - 10.000033) > 1E-6) success = false;
+  if (fabs(sln->get_pt_value(-1.0, 2.0) - 9.999999) > 1E-6) success = false;
+  if (fabs(sln->get_pt_value(0.0, 2.0) - 10.000005) > 1E-6) success = false;
+  if (fabs(sln->get_pt_value(1.0, 2.0) - 9.999999) > 1E-6) success = false;
+  if (fabs(sln->get_pt_value(2.0, 2.0) - 10.000033) > 1E-6) success = false;
 
-  // Cleanup.
-  delete [] coeff_vec;
 
-  if (success == 1) {
+  if (success) {
     printf("Success!\n");
     return ERR_SUCCESS;
   }
