56837669084e95e13aab9670bbb400ef44eb7912
Pavel Solin <solin.pavel@gmail.com>
2011-02-18 11:27:06 -0800

    Removing modules.

--- hermes2d/hermes2d/modules/schroedinger/__init__.py
+++
@@ -1,1 +0,0 @@
-from schroedinger import ModuleSchroedinger, PotentialHarmonicOscillator
--- hermes2d/hermes2d/modules/schroedinger/tests/test_schroedinger.py
+++
@@ -1,8 +0,0 @@
-from hermes2d.modules.schroedinger import (ModuleSchroedinger,
-        PotentialHarmonicOscillator)
-
-def test_oscillator():
-    m = ModuleSchroedinger()
-    p = PotentialHarmonicOscillator()
-    p.set_omega(5)
-    m.set_potential(p)
