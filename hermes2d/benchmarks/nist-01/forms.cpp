c4d57106cef0b5cebfe72f1dea1931045dd4ed75
Lukas Korous <lukas.korous@gmail.com>
2011-03-02 23:41:21 +0100

    NIST - 01 & test.

--- hermes2d/benchmarks/nist-01/forms.cpp
+++ hermes2d/benchmarks/nist-01/forms.cpp
@@ -1,25 +1,103 @@
-template<typename Real, typename Scalar>
-Scalar bilinear_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext)
-{
-  return int_grad_u_grad_v<Real, Scalar>(n, wt, u, v);
-}
+#include "weakform/weakform.h"
+#include "integrals/integrals_h1.h"
+#include "boundaryconditions/boundaryconditions.h"
 
-template<typename Real>
-Real rhs(Real x, Real y)
-{
-  Real a = pow(2.0, 4.0*EXACT_SOL_P);
-  Real b = pow(x-1.0, 8.0);
-  Real c = (38.0*pow(x, 2.0) - 38.0*x + 9.0);
-  Real d = pow(y-1.0, EXACT_SOL_P);
-  Real e = pow(y-1.0, 8.0);
-  Real f = (38.0*pow(y, 2.0) - 38.0*y + 9.0);
-  Real g = pow(x-1.0, EXACT_SOL_P);
+// Exact solution.
+#include "exact_solution.cpp"
 
-  return EXACT_SOL_P*a*pow(x, 8.0)*b*c*pow(y, EXACT_SOL_P)*d + EXACT_SOL_P*a*pow(y, 8.0)*e*f*pow(x,EXACT_SOL_P)*g;
-}
-
-template<typename Real, typename Scalar>
-Scalar linear_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext)
+class WeakFormPoisson : public WeakForm
 {
-  return -int_F_v<Real, Scalar>(n, wt, rhs, v, e);
-}
+public:
+  WeakFormPoisson(double EXACT_SOL_P) : WeakForm(1)
+  {
+    add_matrix_form(new MatrixFormVolPoisson(0, 0));
+    
+    VectorFormVolPoisson* wfp= new VectorFormVolPoisson(0);
+    wfp->EXACT_SOL_P = EXACT_SOL_P;
+    add_vector_form(wfp);
+  };
+
+private:
+  class MatrixFormVolPoisson : public WeakForm::MatrixFormVol
+  {
+  public:
+    MatrixFormVolPoisson(int i, int j) : WeakForm::MatrixFormVol(i, j) { }
+
+    template<typename Real, typename Scalar>
+    Scalar matrix_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext)
+    {
+      return int_grad_u_grad_v<Real, Scalar>(n, wt, u, v);
+    }
+
+    scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *u, Func<double> *v, Geom<double> *e, ExtData<scalar> *ext)
+    {
+      return matrix_form<scalar, scalar>(n, wt, u_ext, u, v, e, ext);
+    }
+
+    Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext)
+    {
+      return matrix_form<Ord, Ord>(n, wt, u_ext, u, v, e, ext);
+    }
+  };
+
+  class VectorFormVolPoisson : public WeakForm::VectorFormVol
+  {
+  public:
+    VectorFormVolPoisson(int i) : WeakForm::VectorFormVol(i) { }
+
+    template<typename Real, typename Scalar>
+    Scalar vector_form(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e, ExtData<Scalar> *ext)
+    {
+      Scalar result = 0;
+      for (int i = 0; i < n; i++)
+        result += wt[i] * (rhs<Real>(e->x[i], e->y[i]) * v->val[i]);
+      return result;
+    }
+
+    scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v, Geom<double> *e, ExtData<scalar> *ext)
+    {
+      return vector_form<scalar, scalar>(n, wt, u_ext, v, e, ext);
+    }
+
+    Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, Geom<Ord> *e, ExtData<Ord> *ext)
+    {
+      return vector_form<Ord, Ord>(n, wt, u_ext, v, e, ext);
+    }
+
+    template<typename Real>
+    Real rhs(Real x, Real y)
+    {
+      Real a = pow(2.0, 4.0*EXACT_SOL_P);
+      Real b = pow(x-1.0, 8.0);
+      Real c = (38.0*pow(x, 2.0) - 38.0*x + 9.0);
+      Real d = pow(y-1.0, EXACT_SOL_P);
+      Real e = pow(y-1.0, 8.0);
+      Real f = (38.0*pow(y, 2.0) - 38.0*y + 9.0);
+      Real g = pow(x-1.0, EXACT_SOL_P);
+
+      return EXACT_SOL_P*a*pow(x, 8.0)*b*c*pow(y, EXACT_SOL_P)*d + EXACT_SOL_P*a*pow(y, 8.0)*e*f*pow(x,EXACT_SOL_P)*g;
+    };
+    
+    // Members.
+    double EXACT_SOL_P;
+  };
+};
+
+class DirichletFunctionBoundaryCondition : public DirichletBoundaryCondition
+{
+public:
+  DirichletFunctionBoundaryCondition(std::string marker) : 
+        DirichletBoundaryCondition(Hermes::vector<std::string>()){
+    markers.push_back(marker);
+  };
+  
+  ~DirichletFunctionBoundaryCondition() {};
+
+  virtual BoundaryConditionValueType get_value_type() const { 
+    return BC_FUNCTION; 
+  };
+
+  virtual scalar function(double x, double y) const {
+    return fn(x, y);
+  };
+};
--- hermes2d/benchmarks/nist-01/tests/main.cpp
+++ hermes2d/benchmarks/nist-01/tests/main.cpp
@@ -59,14 +59,9 @@ MatrixSolverType matrix_solver = SOLVER_UMFPACK;  // Possibilities: SOLVER_AMESO
 // Problem parameters.
 double EXACT_SOL_P = 10;                     // The exact solution is a polynomial of degree 2*EXACT_SOL_P in the x-direction
                                              // as well as in the y-direction. 
-// Exact solution.
-#include "exact_solution.cpp"
 
 // Boundary markers.
-const int BDY_DIRICHLET = 1;
-
-// Essential (Dirichlet) boundary condition values.
-scalar essential_bc_values(double x, double y) { return fn(x, y);}
+const std::string BDY_DIRICHLET = "1";
 
 // Weak forms.
 #include "forms.cpp"
@@ -82,21 +77,14 @@ int main(int argc, char* argv[])
   // Perform initial mesh refinements.
   for (int i=0; i < INIT_REF_NUM; i++) mesh.refine_all_elements();
 
-  // Enter boundary markers.
-  BCTypes bc_types;
-  bc_types.add_bc_dirichlet(BDY_DIRICHLET);
-
-  // Enter Dirichlet boudnary values.
-  BCValues bc_values;
-  bc_values.add_function(BDY_DIRICHLET, essential_bc_values);
+  // Initialize the weak formulation.
+  WeakFormPoisson wf(EXACT_SOL_P);
+  // Initialize boundary conditions
+  DirichletFunctionBoundaryCondition bc(BDY_DIRICHLET);
+  BoundaryConditions bcs(&bc);
 
   // Create an H1 space with default shapeset.
-  H1Space space(&mesh, &bc_types, &bc_values, P_INIT);
-
-  // Initialize the weak formulation.
-  WeakForm wf;
-  wf.add_matrix_form(callback(bilinear_form), HERMES_SYM);
-  wf.add_vector_form(callback(linear_form));
+  H1Space space(&mesh, &bcs, P_INIT);
 
   // Initialize refinement selector.
   H1ProjBasedSelector selector(CAND_LIST, CONV_EXP, H2DRS_DEFAULT_ORDER);
