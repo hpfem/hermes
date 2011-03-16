d437f94c8e04b59dc25011cad90f780e3b77c90c
Lukas Korous <lukas.korous@gmail.com>
2011-03-16 19:52:16 +0100

    euler-forward-step example.

--- hermes2d/examples/euler/euler-forward-step/main.cpp
+++ hermes2d/examples/euler/euler-forward-step/main.cpp
@@ -20,7 +20,7 @@
 // Setting this option to false saves the computation time.
 const bool CALC_TIME_DER = true;
 
-const Ord2 P_INIT = Ord2(0,0);                    // Initial polynomial degree.                      
+const int P_INIT = 0;                             // Initial polynomial degree.                      
 const int INIT_REF_NUM = 2;                       // Number of initial uniform mesh refinements.                       
 double CFL = 0.8;                                 // CFL value.
 double TAU = 1E-4;                                // Time step.
@@ -33,54 +33,16 @@ const double RHO_EXT = 1.4;       // Inlet density (dimensionless).
 const double V1_EXT = 3.0;        // Inlet x-velocity (dimensionless).
 const double V2_EXT = 0.0;        // Inlet y-velocity (dimensionless).
 const double KAPPA = 1.4;         // Kappa.
-// Numerical flux.
-// For numerical fluxes, please see hermes2d/src/numerical_flux.h
-NumericalFlux num_flux(KAPPA);
-
-// Utility functions for the Euler equations.
-#include "../euler-util.cpp"
-
-// Calculated exterior energy.
-double ENERGY_EXT = calc_energy(RHO_EXT, RHO_EXT*V1_EXT, RHO_EXT*V2_EXT, P_EXT);
 
 // Boundary markers.
-const int BDY_SOLID_WALL = 1;
-const int BDY_INLET_OUTLET = 2;
-
-// Constant initial state (matching the supersonic inlet state).
-double ic_density(double x, double y, scalar& dx, scalar& dy)
-{
-  return RHO_EXT;
-}
-double ic_density_vel_x(double x, double y, scalar& dx, scalar& dy)
-{
-  return RHO_EXT * V1_EXT;
-}
-double ic_density_vel_y(double x, double y, scalar& dx, scalar& dy)
-{
-  return RHO_EXT * V2_EXT;
-}
-double ic_energy(double x, double y, scalar& dx, scalar& dy)
-{
-  return calc_energy(RHO_EXT, RHO_EXT*V1_EXT, RHO_EXT*V2_EXT, P_EXT);
-}
+const std::string BDY_SOLID_WALL = "1";
+const std::string BDY_INLET_OUTLET = "2";
 
 // Weak forms.
-#include "forms.cpp"
+#include "../forms_explicit.cpp"
 
-// Filters.
-#include "filters.cpp"
-
-// Filter for entropy which uses the constants defined above.
-static void calc_entropy_estimate_func(int n, Hermes::vector<scalar*> scalars, scalar* result)
-{
-  for (int i = 0; i < n; i++)
-    result[i] = std::log((calc_pressure(scalars.at(0)[i], scalars.at(1)[i], scalars.at(2)[i], scalars.at(3)[i]) / P_EXT)
-    / pow((scalars.at(0)[i] / RHO_EXT), KAPPA));
-};
-
-// Time is zero at the beginning.
-double t = 0;
+// Initial condition.
+#include "../constant_initial_condition.cpp"
 
 int main(int argc, char* argv[])
 {
@@ -94,155 +56,50 @@ int main(int argc, char* argv[])
     mesh.refine_all_elements();
 
   // Boundary condition types;
-  BCTypes bc_types;
+  NaturalBoundaryCondition bc(Hermes::vector<std::string>(BDY_SOLID_WALL, BDY_INLET_OUTLET));
+  BoundaryConditions bcs(&bc);
 
   // Initialize boundary condition types and spaces with default shapesets.
-  bc_types.add_bc_neumann(Hermes::vector<int>(BDY_SOLID_WALL, BDY_INLET_OUTLET));
-  L2Space space_rho(&mesh, &bc_types, P_INIT);
-  L2Space space_rho_v_x(&mesh, &bc_types, P_INIT);
-  L2Space space_rho_v_y(&mesh, &bc_types, P_INIT);
-  L2Space space_e(&mesh, &bc_types, P_INIT);
+  L2Space space_rho(&mesh, &bcs, P_INIT);
+  L2Space space_rho_v_x(&mesh, &bcs, P_INIT);
+  L2Space space_rho_v_y(&mesh, &bcs, P_INIT);
+  L2Space space_e(&mesh, &bcs, P_INIT);
 
   // Initialize solutions, set initial conditions.
-  Solution sln_rho, sln_rho_v_x, sln_rho_v_y, sln_e, prev_rho, prev_rho_v_x, prev_rho_v_y, prev_e;
-  sln_rho.set_exact(&mesh, ic_density);
-  sln_rho_v_x.set_exact(&mesh, ic_density_vel_x);
-  sln_rho_v_y.set_exact(&mesh, ic_density_vel_y);
-  sln_e.set_exact(&mesh, ic_energy);
-  prev_rho.set_exact(&mesh, ic_density);
-  prev_rho_v_x.set_exact(&mesh, ic_density_vel_x);
-  prev_rho_v_y.set_exact(&mesh, ic_density_vel_y);
-  prev_e.set_exact(&mesh, ic_energy);
+  InitialSolutionEulerDensity sln_rho(&mesh, RHO_EXT);
+  InitialSolutionEulerDensityVelX sln_rho_v_x(&mesh, RHO_EXT * V1_EXT);
+  InitialSolutionEulerDensityVelY sln_rho_v_y(&mesh, RHO_EXT * V2_EXT);
+  InitialSolutionEulerDensityEnergy sln_e(&mesh, calc_energy(RHO_EXT, RHO_EXT * V1_EXT, RHO_EXT * V2_EXT, P_EXT, KAPPA));
+  
+  InitialSolutionEulerDensity prev_rho(&mesh, RHO_EXT);
+  InitialSolutionEulerDensityVelX prev_rho_v_x(&mesh, RHO_EXT * V1_EXT);
+  InitialSolutionEulerDensityVelY prev_rho_v_y(&mesh, RHO_EXT * V2_EXT);
+  InitialSolutionEulerDensityEnergy prev_e(&mesh, calc_energy(RHO_EXT, RHO_EXT * V1_EXT, RHO_EXT * V2_EXT, P_EXT, KAPPA));
 
   // Initialize weak formulation.
-  WeakForm wf(4);
-
-  // Bilinear forms coming from time discretization by explicit Euler's method.
-  wf.add_matrix_form(0, 0, callback(bilinear_form_time));
-  wf.add_matrix_form(1, 1, callback(bilinear_form_time));
-  wf.add_matrix_form(2, 2, callback(bilinear_form_time));
-  wf.add_matrix_form(3, 3, callback(bilinear_form_time));
-
-  // Volumetric linear forms.
-  // Linear forms coming from the linearization by taking the Eulerian fluxes' Jacobian matrices 
-  // from the previous time step.
-  // Unnecessary for FVM.
-  if(P_INIT.order_h > 0 || P_INIT.order_v > 0) {
-    // First flux.
-    wf.add_vector_form(0, callback(linear_form_0_1), HERMES_ANY, Hermes::vector<MeshFunction*>(&prev_rho_v_x));
-
-    wf.add_vector_form(1, callback(linear_form_1_0_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_1_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_2_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_3_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(2, callback(linear_form_2_0_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_1_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_2_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_3_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_0_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_1_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_2_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_3_first_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    // Second flux.
-    wf.add_vector_form(0, callback(linear_form_0_2), HERMES_ANY, Hermes::vector<MeshFunction*>(&prev_rho_v_y));
-    
-    wf.add_vector_form(1, callback(linear_form_1_0_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_1_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_2_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(1, callback(linear_form_1_3_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(2, callback(linear_form_2_0_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_1_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_2_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y));
-    wf.add_vector_form(2, callback(linear_form_2_3_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_0_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_1_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_2_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-    wf.add_vector_form(3, callback(linear_form_3_3_second_flux), HERMES_ANY, 
-                         Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  }
-
-  // Volumetric linear forms coming from the time discretization.
-  wf.add_vector_form(0, linear_form_time, linear_form_order, HERMES_ANY, &prev_rho);
-  wf.add_vector_form(1, linear_form_time, linear_form_order, HERMES_ANY, &prev_rho_v_x);
-  wf.add_vector_form(2, linear_form_time, linear_form_order, HERMES_ANY, &prev_rho_v_y);
-  wf.add_vector_form(3, linear_form_time, linear_form_order, HERMES_ANY, &prev_e);
-
-  // Surface linear forms - inner edges coming from the DG formulation.
-  wf.add_vector_form_surf(0, linear_form_interface_0, linear_form_order, H2D_DG_INNER_EDGE, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(1, linear_form_interface_1, linear_form_order, H2D_DG_INNER_EDGE, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(2, linear_form_interface_2, linear_form_order, H2D_DG_INNER_EDGE, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(3, linear_form_interface_3, linear_form_order, H2D_DG_INNER_EDGE, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-
-  // Surface linear forms - inlet / outlet edges.
-  wf.add_vector_form_surf(0, bdy_flux_inlet_outlet_comp_0, linear_form_order, BDY_INLET_OUTLET, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(1, bdy_flux_inlet_outlet_comp_1, linear_form_order, BDY_INLET_OUTLET, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(2, bdy_flux_inlet_outlet_comp_2, linear_form_order, BDY_INLET_OUTLET, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(3, bdy_flux_inlet_outlet_comp_3, linear_form_order, BDY_INLET_OUTLET, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-
-  // Surface linear forms - Solid wall edges.
-  wf.add_vector_form_surf(0, bdy_flux_solid_wall_comp_0, linear_form_order, BDY_SOLID_WALL, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(1, bdy_flux_solid_wall_comp_1, linear_form_order, BDY_SOLID_WALL, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(2, bdy_flux_solid_wall_comp_2, linear_form_order, BDY_SOLID_WALL, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
-  wf.add_vector_form_surf(3, bdy_flux_solid_wall_comp_3, linear_form_order, BDY_SOLID_WALL, 
-                          Hermes::vector<MeshFunction*>(&prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e));
+  EulerEquationsWeakFormExplicit wf(KAPPA, RHO_EXT, V1_EXT, V2_EXT, P_EXT, BDY_SOLID_WALL, BDY_SOLID_WALL, 
+    BDY_INLET_OUTLET, BDY_INLET_OUTLET, &prev_rho, &prev_rho_v_x, &prev_rho_v_y, &prev_e);
 
   // Initialize the FE problem.
   bool is_linear = true;
   
   DiscreteProblem dp(&wf, Hermes::vector<Space*>(&space_rho, &space_rho_v_x, &space_rho_v_y, &space_e), is_linear);
   
-  // If the FE problem is in fact a FV problem.
-  if(P_INIT.order_h == 0 && P_INIT.order_v == 0)
-    dp.set_fvm();
-  
-  // Filters for visualization of pressure and the two components of velocity.
-  SimpleFilter pressure(calc_pressure_func, Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e));
-  SimpleFilter u(calc_u_func, Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e));
-  SimpleFilter w(calc_w_func, Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e));
-  SimpleFilter Mach_number(calc_Mach_func, Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e));
-  SimpleFilter entropy_estimate(calc_entropy_estimate_func, Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e));
+  // Filters for visualization of Mach number, pressure and entropy.
+  MachNumberFilter Mach_number(Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e), KAPPA);
+  PressureFilter pressure(Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e), KAPPA);
+  EntropyFilter entropy(Hermes::vector<MeshFunction*>(&sln_rho, &sln_rho_v_x, &sln_rho_v_y, &sln_e), KAPPA, RHO_EXT, P_EXT);
 
   ScalarView pressure_view("Pressure", new WinGeom(0, 0, 600, 300));
   ScalarView Mach_number_view("Mach number", new WinGeom(700, 0, 600, 300));
   ScalarView entropy_production_view("Entropy estimate", new WinGeom(0, 400, 600, 300));
-  VectorView vview("Velocity", new WinGeom(700, 400, 600, 300));
 
-  // Iteration number.
-  int iteration = 0;
+  /*
+  ScalarView s1("1", new WinGeom(0, 0, 600, 300));
+  ScalarView s2("2", new WinGeom(700, 0, 600, 300));
+  ScalarView s3("3", new WinGeom(0, 400, 600, 300));
+  ScalarView s4("4", new WinGeom(700, 400, 600, 300));
+  */
   
   // Set up the solver, matrix, and rhs according to the solver selection.
   SparseMatrix* matrix = create_matrix(matrix_solver);
@@ -252,6 +109,7 @@ int main(int argc, char* argv[])
   // Output of the approximate time derivative.
   std::ofstream time_der_out("time_der");
 
+  int iteration = 0; double t = 0;
   for(t = 0.0; t < 10.0; t += TAU) {
     info("---- Time step %d, time %3.5f.", iteration++, t);
     
@@ -306,7 +164,7 @@ int main(int argc, char* argv[])
       space_e.get_element_assembly_list(e, &al);
       double energy = solution_vector[al.dof[0]];
       
-      double condition = e->get_area() / (std::sqrt(v1*v1 + v2*v2) + calc_sound_speed(rho, rho*v1, rho*v2, energy));
+      double condition = e->get_area() / (std::sqrt(v1*v1 + v2*v2) + calc_sound_speed(rho, rho*v1, rho*v2, energy, KAPPA));
       
       if(condition < min_condition || min_condition == 0.)
         min_condition = condition;
@@ -323,21 +181,31 @@ int main(int argc, char* argv[])
     prev_e.copy(&sln_e);
 
     // Visualization.
-    pressure.reinit();
-    u.reinit();
-    w.reinit();
     Mach_number.reinit();
-    entropy_estimate.reinit();
+    pressure.reinit();
+    entropy.reinit();
     pressure_view.show(&pressure);
-    entropy_production_view.show(&entropy_estimate);
+    entropy_production_view.show(&entropy);
     Mach_number_view.show(&Mach_number);
-    vview.show(&u, &w);
+   
+    /*
+    s1.show(&prev_rho);
+    s2.show(&prev_rho_v_x);
+    s3.show(&prev_rho_v_y);
+    s4.show(&prev_e);
+    */
   }
   
   pressure_view.close();
   entropy_production_view.close();
   Mach_number_view.close();
-  vview.close();
+
+  /*
+  s1.close();
+  s2.close();
+  s3.close();
+  s4.close();
+  */
 
   time_der_out.close();
   return 0;
