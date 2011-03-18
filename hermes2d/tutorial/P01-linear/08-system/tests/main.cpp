f910b1ab29d1ba94fd1539720fadcd666a8053bc
Lukas Korous <lukas.korous@gmail.com>
2011-03-18 18:11:01 +0100

    Tutorial P01-08 & test.

--- hermes2d/tutorial/P01-linear/08-system/tests/main.cpp
+++ hermes2d/tutorial/P01-linear/08-system/tests/main.cpp
@@ -3,90 +3,43 @@
 // This test makes sure that example 08-system works correctly.
 // CAUTION: This test will fail when any changes to the shapeset
 // are made, but it is easy to fix (see below).
-
-// problem constants
-const double E  = 200e9;                                   // Young modulus (steel)
-const double nu = 0.3;                                     // Poisson ratio
-const double f_0  = 0;                                     // external force in x-direction
-const double f_1  = 1e4;                                   // external force in y-direction
-const double lambda = (E * nu) / ((1 + nu) * (1 - 2*nu));  // first Lame constant
-const double mu = E / (2*(1 + nu));                        // second Lame constant
 const int P_INIT = 8;                                      // Initial polynomial degree of all elements.
 MatrixSolverType matrix_solver = SOLVER_UMFPACK;           // Possibilities: SOLVER_AMESOS, SOLVER_AZTECOO, SOLVER_MUMPS,
                                                            // SOLVER_PETSC, SOLVER_SUPERLU, SOLVER_UMFPACK.
 
-
 // Boundary markers.
-const int BDY_1 = 1, BDY_2 = 2, BDY_3 = 3, BDY_4 = 4, BDY_5 = 5;
+const std::string BDY_1 = "1", BDY_2 = "2", BDY_3 = "3", BDY_4 = "4", BDY_5 = "5";
 
-// Bilinear forms.
-template<typename Real, typename Scalar>
-Scalar bilinear_form_0_0(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v,
-                         Geom<Real> *e, ExtData<Scalar> *ext)
-{
-  return (lambda + 2*mu) * int_dudx_dvdx<Real, Scalar>(n, wt, u, v) +
-                      mu * int_dudy_dvdy<Real, Scalar>(n, wt, u, v);
-}
+// Problem parameters.
+const double E  = 200e9;                                   // Young modulus (steel).
+const double nu = 0.3;                                     // Poisson ratio.
+const double f_0  = 0;                                     // External force in x-direction.
+const double f_1  = 1e4;                                   // External force in y-direction.
+const double lambda = (E * nu) / ((1 + nu) * (1 - 2*nu));  // First Lame constant.
+const double mu = E / (2*(1 + nu));                        // Second Lame constant.
 
-template<typename Real, typename Scalar>
-Scalar bilinear_form_0_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v,
-                         Geom<Real> *e, ExtData<Scalar> *ext)
-{
-  return lambda * int_dudy_dvdx<Real, Scalar>(n, wt, u, v) +
-             mu * int_dudx_dvdy<Real, Scalar>(n, wt, u, v);
-}
-
-template<typename Real, typename Scalar>
-Scalar bilinear_form_1_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *u, Func<Real> *v,
-                         Geom<Real> *e, ExtData<Scalar> *ext)
-{
-  return              mu * int_dudx_dvdx<Real, Scalar>(n, wt, u, v) +
-         (lambda + 2*mu) * int_dudy_dvdy<Real, Scalar>(n, wt, u, v);
-}
-
-// Linear forms.
-template<typename Real, typename Scalar>
-Scalar linear_form_surf_0(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e,
-                          ExtData<Scalar> *ext)
-{
-  return f_0 * int_v<Real, Scalar>(n, wt, v);
-}
-
-template<typename Real, typename Scalar>
-Scalar linear_form_surf_1(int n, double *wt, Func<Scalar> *u_ext[], Func<Real> *v, Geom<Real> *e,
-                          ExtData<Scalar> *ext)
-{
-  return f_1 * int_v<Real, Scalar>(n, wt, v);
-}
+// Weak forms.
+#include "forms.cpp"
 
 int main(int argc, char* argv[])
 {
-  // Load the mesh file.
+  // Load the mesh.
   Mesh mesh;
   H2DReader mloader;
   mloader.load("sample.mesh", &mesh);
 
   // Initialize boundary conditions.
-  BCTypes bc_types;
-  bc_types.add_bc_dirichlet(BDY_1);
-  bc_types.add_bc_neumann(Hermes::vector<int>(BDY_2, BDY_3, BDY_4, BDY_5));
-
-  // Enter Dirichlet boundary values;
-  BCValues bc_values;
-  bc_values.add_zero(BDY_1);
+  DirichletConstantBoundaryCondition bc_dirichlet(BDY_1, 0.0);
+  NaturalBoundaryCondition bc_natural(Hermes::vector<std::string>(BDY_2, BDY_3, BDY_4, BDY_5));
+  BoundaryConditions bcs(Hermes::vector<BoundaryCondition*>(&bc_dirichlet, &bc_natural));
 
   // Create x- and y- displacement space using the default H1 shapeset.
-  H1Space u_space(&mesh, &bc_types, &bc_values, P_INIT);
-  H1Space v_space(&mesh, &bc_types, &bc_values, P_INIT);
+  H1Space u_space(&mesh, &bcs, P_INIT);
+  H1Space v_space(&mesh, &bcs, P_INIT);
   info("ndof = %d.", Space::get_num_dofs(Hermes::vector<Space *>(&u_space, &v_space)));
 
   // Initialize the weak formulation.
-  WeakForm wf(2);
-  wf.add_matrix_form(0, 0, callback(bilinear_form_0_0), HERMES_SYM);  // Note that only one symmetric part is
-  wf.add_matrix_form(0, 1, callback(bilinear_form_0_1), HERMES_SYM);  // added in the case of symmetric bilinear
-  wf.add_matrix_form(1, 1, callback(bilinear_form_1_1), HERMES_SYM);  // forms.
-  wf.add_vector_form_surf(0, callback(linear_form_surf_0), BDY_3);
-  wf.add_vector_form_surf(1, callback(linear_form_surf_1), BDY_3);
+  WeakFormLameEquations wf(mu, lambda, BDY_3, f_0, f_1);
 
   // Testing n_dof and correctness of solution vector
   // for p_init = 1, 2, ..., 10
