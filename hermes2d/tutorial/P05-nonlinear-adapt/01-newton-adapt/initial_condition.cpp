9044464b149e56bddd7f0a03eeb54aea55166bc3
Lukas Korous <lukas.korous@gmail.com>
2011-03-20 22:22:00 +0100

    Add forgotten file.

---
+++ hermes2d/tutorial/P05-nonlinear-adapt/01-newton-adapt/initial_condition.cpp
@@ -0,0 +1,12 @@
+class InitialSolutionHeatTransfer : public ExactSolutionScalar
+{
+public:
+  InitialSolutionHeatTransfer(Mesh* mesh) : ExactSolutionScalar(mesh) {};
+
+  // Function representing an exact one-dimension valued solution.
+  virtual scalar exact_function (double x, double y, scalar& dx, scalar& dy) {
+    dx = (y+10)/10.;
+    dy = (x+10)/10.;
+    return (x+10)*(y+10)/100. + 2.;
+  };
+};
