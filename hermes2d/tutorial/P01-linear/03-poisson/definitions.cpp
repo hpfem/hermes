10e6b0b925a32345d4fb8ef4b64e00aab5e3bb55
Pavel Solin <solin.pavel@gmail.com>
2011-03-26 16:57:52 -0700

    Fixing namespace in example P01/03.

--- hermes2d/tutorial/P01-linear/03-poisson/definitions.cpp
+++ hermes2d/tutorial/P01-linear/03-poisson/definitions.cpp
@@ -3,13 +3,16 @@
 #include "boundaryconditions/essential_bcs.h"
 #include "weakform_library/laplace.h"
 
+using namespace Laplace::VolumetricMatrixForms;
+using namespace Laplace::VolumetricVectorForms;
+
 class CustomWeakFormPoisson : public WeakForm
 {
 public:
   CustomWeakFormPoisson(double const_f) : WeakForm(1)
   {
-    add_matrix_form(new Laplace::DefaultVolumetricMatrixForms::MatrixFormStiffness(0, 0));
-    add_vector_form(new Laplace::DefaultVolumetricVectorForms::VectorFormConst(0, const_f));
+    add_matrix_form(new DefaultMatrixFormStiffness(0, 0));
+    add_vector_form(new DefaultVectorFormConst(0, const_f));
   };
 };
 
