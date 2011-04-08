b6fa5d8b7a47990bdba8c3d348a918f571d81101
Pavel Solin <solin.pavel@gmail.com>
2011-04-07 20:56:10 -0700

    Cleaning tutorial examples P01/01 - P01/06.

--- hermes2d/tutorial/P01-linear/06-bc-newton/definitions.cpp
+++ hermes2d/tutorial/P01-linear/06-bc-newton/definitions.cpp
@@ -3,17 +3,49 @@
 #include "boundaryconditions/essential_bcs.h"
 #include "weakform_library/h1.h"
 
-using namespace WeakFormsH1;
+using namespace WeakFormsH1::VolumetricMatrixForms;
+using namespace WeakFormsH1::VolumetricVectorForms;
+using namespace WeakFormsH1::SurfaceMatrixForms;
+using namespace WeakFormsH1::SurfaceVectorForms;
 
-class CustomWeakFormPoissonNewton : public WeakForm
+/* Weak forms */
+
+class CustomWeakFormPoissonNeumann : public WeakForm
 {
 public:
-  // Problem parameters.
-  CustomWeakFormPoissonNewton(double h, double T0, std::string natural_bc_bnd_part) : WeakForm(1)
+  CustomWeakFormPoissonNeumann(std::string mat_al, double lambda_al, 
+                        std::string mat_cu, double lambda_cu, 
+                        double vol_heat_src, std::string bdy_heat_flux, 
+			double alpha, double t_exterior) : WeakForm(1)
   {
-    add_matrix_form(new VolumetricMatrixForms::DefaultLinearDiffusion(0, 0));
-    add_matrix_form_surf(new SurfaceMatrixForms::DefaultMatrixFormSurf(0, 0, natural_bc_bnd_part, h));
-    add_vector_form_surf(new SurfaceVectorForms::DefaultVectorFormSurf(0, natural_bc_bnd_part, h * T0));
+    // Volumetric integrals.
+    add_matrix_form(new DefaultLinearDiffusion(0, 0, mat_al, lambda_al));
+    add_matrix_form(new DefaultLinearDiffusion(0, 0, mat_cu, lambda_cu));
+    add_vector_form(new DefaultVectorFormConst(0, vol_heat_src));
+    
+    // Surface integrals due to the Newton condition.
+    add_matrix_form_surf(new DefaultMatrixFormSurf(0, 0, bdy_heat_flux, alpha));
+    add_vector_form_surf(new DefaultVectorFormSurf(0, bdy_heat_flux, alpha * t_exterior));
   };
+};
+
+/* Custom non-constant Dirichlet condition */
+
+class CustomDirichletCondition : public EssentialBoundaryCondition {
+public:
+  CustomDirichletCondition(Hermes::vector<std::string> markers, double A, double B, double C) 
+    : EssentialBoundaryCondition(markers), A(A), B(B), C(C) { }
+
+  ~CustomDirichletCondition() {};
+
+  virtual EssentialBCValueType get_value_type() const
+         { return EssentialBoundaryCondition::BC_FUNCTION; }
+
+  virtual scalar value(double x, double y, double n_x, double n_y, double t_x, double t_y) const {
+    return A*x + B*y + C;
+  }
+
+  protected:
+    double A, B, C;
 };
 
