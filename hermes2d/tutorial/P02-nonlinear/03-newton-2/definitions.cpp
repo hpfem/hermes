3acf0f2757b85de3c4b7744a22277bbd540b7706
František Mach <mach.frantisek@gmail.com>
2011-04-08 23:55:20 -0700

    Cleaning up.

--- hermes2d/tutorial/P02-nonlinear/03-newton-2/definitions.cpp
+++ hermes2d/tutorial/P02-nonlinear/03-newton-2/definitions.cpp
@@ -36,7 +36,7 @@ private:
 
     virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *u, 
                  Func<double> *v, Geom<double> *e, ExtData<scalar> *ext) const {
-      return matrix_form<scalar, scalar>(n, wt, u_ext, u, v, e, ext);
+      return matrix_form<double, scalar>(n, wt, u_ext, u, v, e, ext);
     }
 
     virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v, 
@@ -77,7 +77,7 @@ private:
 
     virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v, 
                  Geom<double> *e, ExtData<scalar> *ext) const {
-      return vector_form<scalar, scalar>(n, wt, u_ext, v, e, ext);
+      return vector_form<double, scalar>(n, wt, u_ext, v, e, ext);
     }
 
     virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v, 
