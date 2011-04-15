b476998a01591c734624ef11fea148a29dac35a2
František Mach <mach.frantisek@gmail.com>
2011-04-14 22:12:40 -0700

    Added weak forms DefaultLinearMagnetostaticsVelocity and DefaultLinearMagnetostaticsRemanence.

--- hermes2d/src/weakform_library/maxwell.h
+++ hermes2d/src/weakform_library/maxwell.h
@@ -31,7 +31,7 @@ namespace WeakFormsMaxwell {
     public:
       // The optional order_increase takes into account the axisymmetric part.
       DefaultLinearMagnetostatics(int i, int j, scalar coeff = 1.0,
-                                  SymFlag sym = HERMES_SYM, GeomType gt = HERMES_PLANAR, 
+                                  SymFlag sym = HERMES_SYM, GeomType gt = HERMES_PLANAR,
                                   int order_increase = 3)
              : WeakForm::MatrixFormVol(i, j, sym), coeff(coeff), gt(gt), order_increase(order_increase) { }
       DefaultLinearMagnetostatics(int i, int j, std::string area, scalar coeff = 1.0,
@@ -78,21 +78,21 @@ namespace WeakFormsMaxwell {
     class DefaultJacobianNonlinearMagnetostatics : public WeakForm::MatrixFormVol
     {
     public:
-      DefaultJacobianNonlinearMagnetostatics(int i, int j, CubicSpline* spline_coeff, 
+      DefaultJacobianNonlinearMagnetostatics(int i, int j, CubicSpline* spline_coeff,
                                              scalar const_coeff = 1.0,
-                                             SymFlag sym = HERMES_NONSYM, 
+                                             SymFlag sym = HERMES_NONSYM,
                                              GeomType gt = HERMES_PLANAR,
                                              int order_increase = 3)
-             : WeakForm::MatrixFormVol(i, j, sym), spline_coeff(spline_coeff), 
+             : WeakForm::MatrixFormVol(i, j, sym), spline_coeff(spline_coeff),
                                        const_coeff(const_coeff), gt(gt),
                                        order_increase(order_increase) { }
       DefaultJacobianNonlinearMagnetostatics(int i, int j, std::string area,
-                                             CubicSpline* spline_coeff, 
-                                             scalar const_coeff = 1.0, 
+                                             CubicSpline* spline_coeff,
+                                             scalar const_coeff = 1.0,
                                              SymFlag sym = HERMES_NONSYM,
-                                             GeomType gt = HERMES_PLANAR, 
+                                             GeomType gt = HERMES_PLANAR,
                                              int order_increase = 3)
-             : WeakForm::MatrixFormVol(i, j, sym, area), spline_coeff(spline_coeff), 
+             : WeakForm::MatrixFormVol(i, j, sym, area), spline_coeff(spline_coeff),
                                        const_coeff(const_coeff), gt(gt),
                                        order_increase(order_increase) { }
 
@@ -108,12 +108,12 @@ namespace WeakFormsMaxwell {
                                  * (u_ext[0]->dx[i] * v->dx[i] + u_ext[0]->dy[i] * v->dy[i]);
             if (gt == HERMES_AXISYM_X) {
               axisym_part += wt[i] * const_coeff*spline_coeff->get_derivative(B_i) / B_i / e->y[i]
-       		                   * (u_ext[0]->dx[i] * u->dx[i] + u_ext[0]->dy[i] * u->dy[i])
+                             * (u_ext[0]->dx[i] * u->dx[i] + u_ext[0]->dy[i] * u->dy[i])
                                    * u_ext[0]->val[i] * v->dy[i];
             }
             else if (gt == HERMES_AXISYM_Y) {
               axisym_part += wt[i] * const_coeff*spline_coeff->get_derivative(B_i) / B_i / e->x[i]
- 		                   * (u_ext[0]->dx[i] * u->dx[i] + u_ext[0]->dy[i] * u->dy[i])
+                       * (u_ext[0]->dx[i] * u->dx[i] + u_ext[0]->dy[i] * u->dy[i])
                                    * u_ext[0]->val[i] * v->dx[i];
             }
           }
@@ -162,6 +162,49 @@ namespace WeakFormsMaxwell {
         int order_increase;
     };
   }
+
+  /* Default volumetric matrix form \int_{area} u->val[i] * ((vel_x - e->y[i] * vel_ang) *
+     v->dx[i] + (vel_y + e->x[i] * vel_ang) * v->dy[i])
+     vel_x, vel_y... velocity components
+     vel_ang... velocity angle
+  */
+
+  class DefaultLinearMagnetostaticsVelocity : public WeakForm::MatrixFormVol
+  {
+  public:
+      DefaultLinearMagnetostaticsVelocity(int i, int j, double gamma, double vel_x, double vel_y, double vel_ang = 0.0)
+          : WeakForm::MatrixFormVol(i, j), gamma(gamma), vel_x(vel_x), vel_y(vel_y), vel_ang(vel_ang) { }
+
+      DefaultLinearMagnetostaticsVelocity(int i, int j, std::string area, double gamma, double vel_x, double vel_y, double vel_ang = 0.0)
+          : WeakForm::MatrixFormVol(i, j, HERMES_NONSYM, area), gamma(gamma), vel_x(vel_x), vel_y(vel_y), vel_ang(vel_ang) { }
+
+      virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *u, Func<double> *v,
+                           Geom<double> *e, ExtData<scalar> *ext) const {
+          scalar result = 0;
+          for (int i = 0; i < n; i++)
+              result += wt[i] * u->val[i] * ((vel_x - e->y[i] * vel_ang) * v->dx[i] +
+                                             (vel_y + e->x[i] * vel_ang) * v->dy[i]);
+
+          return -gamma * result;
+      }
+
+      virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *u, Func<Ord> *v,
+                      Geom<Ord> *e, ExtData<Ord> *ext) const {
+          Ord result = 0;
+          for (int i = 0; i < n; i++)
+              result += wt[i] * u->val[i] * (v->dx[i] + v->dy[i]);
+
+          return result;
+      }
+
+      // This is to make the form usable in rk_time_step().
+      virtual WeakForm::MatrixFormVol* clone() {
+          return new DefaultLinearMagnetostaticsVelocity(*this);
+      }
+
+  private:
+      double gamma, vel_x, vel_y, vel_ang;
+  };
 
   namespace VolumetricVectorForms {
 
@@ -217,16 +260,16 @@ namespace WeakFormsMaxwell {
     class DefaultResidualNonlinearMagnetostatics : public WeakForm::VectorFormVol
     {
     public:
-      DefaultResidualNonlinearMagnetostatics(int i, CubicSpline* spline_coeff, 
-                                             scalar const_coeff = 1.0, 
+      DefaultResidualNonlinearMagnetostatics(int i, CubicSpline* spline_coeff,
+                                             scalar const_coeff = 1.0,
                                              GeomType gt = HERMES_PLANAR,
                                              int order_increase = 3)
-             : WeakForm::VectorFormVol(i), spline_coeff(spline_coeff), const_coeff(const_coeff), 
+             : WeakForm::VectorFormVol(i), spline_coeff(spline_coeff), const_coeff(const_coeff),
                                            gt(gt), order_increase(order_increase) { }
-      DefaultResidualNonlinearMagnetostatics(int i, std::string area, CubicSpline* spline_coeff, 
+      DefaultResidualNonlinearMagnetostatics(int i, std::string area, CubicSpline* spline_coeff,
                                              scalar const_coeff = 1.0,
                                              GeomType gt = HERMES_PLANAR, int order_increase = 3)
-             : WeakForm::VectorFormVol(i, area), spline_coeff(spline_coeff), const_coeff(const_coeff), gt(gt), 
+             : WeakForm::VectorFormVol(i, area), spline_coeff(spline_coeff), const_coeff(const_coeff), gt(gt),
                                        order_increase(order_increase) { }
 
       virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v,
@@ -267,6 +310,51 @@ namespace WeakFormsMaxwell {
         scalar const_coeff;
         GeomType gt;
         int order_increase;
+    };
+
+    /* Default volumetric vector form \int_{area} rem/perm * (- sin(rem_ang / 180.0 * M_PI) *
+       v->dx[i + cos(rem_ang / 180.0 * M_PI) * v->dy[i])
+       rem... remanent induction
+       rem_ang... remanent induction angle
+       per... permeability
+    */
+
+    class DefaultLinearMagnetostaticsRemanence : public WeakForm::VectorFormVol
+    {
+    public:
+        DefaultLinearMagnetostaticsRemanence(int i, double perm, double rem, double rem_ang, GeomType gt = HERMES_PLANAR)
+            : WeakForm::VectorFormVol(i), perm(perm), rem(rem), rem_ang(rem_ang), gt(gt) { }
+
+        DefaultLinearMagnetostaticsRemanence(int i, std::string area, double perm, double rem, double rem_ang, GeomType gt = HERMES_PLANAR)
+            : WeakForm::VectorFormVol(i, area), perm(perm), rem(rem), rem_ang(rem_ang), gt(gt) { }
+
+        virtual scalar value(int n, double *wt, Func<scalar> *u_ext[], Func<double> *v,
+                             Geom<double> *e, ExtData<scalar> *ext) const {
+            scalar result = 0;
+            for (int i = 0; i < n; i++)
+                result += wt[i] * rem/perm * (- sin(rem_ang / 180.0 * M_PI) * v->dx[i]
+                                              + cos(rem_ang / 180.0 * M_PI) * v->dy[i]);
+
+            return (gt == HERMES_PLANAR ? -1.0 : 1.0) * result;
+        }
+
+        virtual Ord ord(int n, double *wt, Func<Ord> *u_ext[], Func<Ord> *v,
+                        Geom<Ord> *e, ExtData<Ord> *ext) const {
+            Ord result = 0;
+            for (int i = 0; i < n; i++)
+                result += wt[i] * (v->dx[i] + v->dy[i]);
+
+            return result;
+        }
+
+        // This is to make the form usable in rk_time_step().
+        virtual WeakForm::VectorFormVol* clone() {
+            return new DefaultLinearMagnetostaticsRemanence(*this);
+        }
+
+    private:
+        double perm, rem, rem_ang;
+        GeomType gt;
     };
   }
 }
