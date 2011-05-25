436586fcaf2016917257141369f5c86c5e2d8fcb
Vladimir Cerny <blackvladimir@gmail.com>
2011-05-25 14:53:41 +0200

    taking back dpinterface change

--- hermes_common/solver/dpinterface.h
+++ hermes_common/solver/dpinterface.h
@@ -27,7 +27,7 @@ public:
   /// forms do not exist. This is useful if the matrix is later to be merged with
   /// a matrix that has nonzeros in these blocks. The Table serves for optional
   /// weighting of matrix blocks in systems.
-  virtual void create_sparse_structure(SparseMatrix<Scalar>* mat, Vector<Scalar>* rhs = NULL,bool rhsonly = false,
+  virtual void create_sparse_structure(SparseMatrix<Scalar>* mat, Vector<Scalar>* rhs = NULL,
                                        bool force_diagonal_blocks = false, Table* block_weights = NULL) = 0;
 
   /// Assembling.
@@ -39,7 +39,7 @@ public:
   /// weighting of matrix blocks in systems. The parameter add_dir_lift decides 
   /// whether Dirichlet lift will be added while coeff_vec is converted into 
   /// Solutions.
-  virtual void assemble(Scalar* coeff_vec, SparseMatrix<Scalar>* mat, Vector<Scalar>* rhs = NULL, bool rhsonly = false,
+  virtual void assemble(Scalar* coeff_vec, SparseMatrix<Scalar>* mat, Vector<Scalar>* rhs = NULL,
              bool force_diagonal_blocks = false, bool add_dir_lift = true, Table* block_weights = NULL) = 0; 
 
   virtual void invalidate_matrix() = 0;
