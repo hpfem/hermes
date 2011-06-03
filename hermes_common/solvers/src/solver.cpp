8d5b2e575498a103c2d419dfe96f4612d84946f1
Lukas Korous <lukas.korous@gmail.com>
2011-06-03 11:51:43 +0200

    Reorganizing hermes_common.

---
+++ hermes_common/solvers/src/solver.cpp
@@ -0,0 +1,23 @@
+// This file is part of Hermes2D
+//
+// Copyright (c) 2009 hp-FEM group at the University of Nevada, Reno (UNR).
+// Email: hpfem-group@unr.edu, home page: http://hpfem.org/.
+//
+// Hermes2D is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; either version 2 of the License,
+// or (at your option) any later version.
+//
+// Hermes3D is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Hermes2D; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef __HERMES_COMMON_SOLVER_H_
+#define __HERMES_COMMON_SOLVER_H_
+
+#endif
