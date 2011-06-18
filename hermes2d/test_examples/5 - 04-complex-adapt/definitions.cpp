2197ee6b41800894b62b3af027be0681e6f3b09f
Vladimir Cerny <blackvladimir@gmail.com>
2011-06-18 16:43:25 +0200

    example 5 works

--- hermes2d/test_examples/5 - 04-complex-adapt/definitions.cpp
+++ hermes2d/test_examples/5 - 04-complex-adapt/definitions.cpp
@@ -2,20 +2,20 @@
   
 CustomWeakForm::CustomWeakForm(std::string mat_air,  double mu_air,
                                std::string mat_iron, double mu_iron, double gamma_iron,
-                               std::string mat_wire, double mu_wire, scalar j_ext, double omega) : WeakForm(1) 
+                               std::string mat_wire, double mu_wire, std::complex<double> j_ext, double omega) : WeakForm(1) 
 {
-  scalar ii =  cplx(0.0, 1.0);
+  std::complex<double> ii =  std::complex<double>(0.0, 1.0);
 
   // Jacobian.
-  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion(0, 0, mat_air,  new HermesFunction(1.0/mu_air)));
-  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion(0, 0, mat_iron, new HermesFunction(1.0/mu_iron)));
-  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion(0, 0, mat_wire, new HermesFunction(1.0/mu_wire)));
-  add_matrix_form(new WeakFormsH1::DefaultMatrixFormVol(0, 0, mat_iron, new HermesFunction(ii * omega * gamma_iron)));
+  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<std::complex<double> >(0, 0, mat_air,  new HermesFunction<std::complex<double> >(1.0/mu_air)));
+  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<std::complex<double> >(0, 0, mat_iron, new HermesFunction<std::complex<double> >(1.0/mu_iron)));
+  add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<std::complex<double> >(0, 0, mat_wire, new HermesFunction<std::complex<double> >(1.0/mu_wire)));
+  add_matrix_form(new WeakFormsH1::DefaultMatrixFormVol<std::complex<double> >(0, 0, mat_iron, new HermesFunction<std::complex<double> >(ii * omega * gamma_iron)));
 
   // Residual.
-  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion(0, mat_air, new HermesFunction(1.0/mu_air)));
-  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion(0, mat_iron, new HermesFunction(1.0/mu_iron)));
-  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion(0, mat_wire, new HermesFunction(1.0/mu_wire)));
-  add_vector_form(new WeakFormsH1::DefaultVectorFormVol(0, mat_wire, new HermesFunction(-j_ext)));
-  add_vector_form(new WeakFormsH1::DefaultResidualVol(0, mat_iron, new HermesFunction(ii * omega * gamma_iron)));
+  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion<std::complex<double> >(0, mat_air, new HermesFunction<std::complex<double> >(1.0/mu_air)));
+  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion<std::complex<double> >(0, mat_iron, new HermesFunction<std::complex<double> >(1.0/mu_iron)));
+  add_vector_form(new WeakFormsH1::DefaultResidualDiffusion<std::complex<double> >(0, mat_wire, new HermesFunction<std::complex<double> >(1.0/mu_wire)));
+  add_vector_form(new WeakFormsH1::DefaultVectorFormVol<std::complex<double> >(0, mat_wire, new HermesFunction<std::complex<double> >(-j_ext)));
+  add_vector_form(new WeakFormsH1::DefaultResidualVol<std::complex<double> >(0, mat_iron, new HermesFunction<std::complex<double> >(ii * omega * gamma_iron)));
 }
--- hermes2d/test_examples/5 - 04-complex-adapt/definitions.h
+++ hermes2d/test_examples/5 - 04-complex-adapt/definitions.h
@@ -1,12 +1,11 @@
 #include "hermes2d.h"
-#include "boundaryconditions/essential_bcs.h"
 
 /* Weak forms */
 
-class CustomWeakForm : public WeakForm
+class CustomWeakForm : public WeakForm<std::complex<double> >
 { 
 public:
   CustomWeakForm(std::string mat_air,  double mu_air,
                  std::string mat_iron, double mu_iron, double gamma_iron,
-                 std::string mat_wire, double mu_wire, scalar j_ext, double omega);
+                 std::string mat_wire, double mu_wire, std::complex<double> j_ext, double omega);
 };
