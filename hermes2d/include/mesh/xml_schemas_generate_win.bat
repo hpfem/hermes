842226b955328cc3124a7c7a5323da2795e23f6e
Lukas Korous <lukas.korous@gmail.com>
2011-08-20 22:27:16 +0200

    Update scripts generating C++ sources from XML schemas in .xsd.

---
+++ hermes2d/include/mesh/xml_schemas_generate_win.bat
@@ -0,0 +1,3 @@
+xsd cxx-tree --generate-doxygen --generate-ostream --hxx-suffix .h --cxx-suffix .cpp mesh_h2d_xml.xsd &
+xsd cxx-tree --generate-doxygen --generate-ostream --hxx-suffix .h --cxx-suffix .cpp subdomains_h2d_xml.xsd &
+mv -f *.cpp ../../src/mesh
