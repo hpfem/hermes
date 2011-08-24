2ca0ce6c5c96ffe133f685bd33859eedcc1fe0f6
Lukas Korous <lukas.korous@gmail.com>
2011-08-24 14:10:26 +0200

    Work on subdomains-advanced.

--- hermes2d/tests/meshes/subdomains-advanced/definitions.cpp
+++ hermes2d/tests/meshes/subdomains-advanced/definitions.cpp
@@ -1,17 +1,54 @@
 #include "definitions.h"
 
-using namespace Hermes;
-using namespace Hermes::Hermes2D;
-using namespace Hermes::Hermes2D::WeakFormsH1;
+CustomWeakFormHeatAndFlow::CustomWeakFormHeatAndFlow(bool Stokes, double Reynolds, double time_step, Solution<double>* x_vel_previous_time, Solution<double>* y_vel_previous_time, Solution<double>* T_prev_time) : WeakForm<double>(4), Stokes(Stokes), 
+    Reynolds(Reynolds), time_step(time_step), x_vel_previous_time(x_vel_previous_time), y_vel_previous_time(y_vel_previous_time)
+  {
+    BilinearFormSymVel* sym_form_0 = new BilinearFormSymVel(0, 0, Stokes, Reynolds, time_step);
+    add_matrix_form(sym_form_0);
+    BilinearFormSymVel* sym_form_1 = new BilinearFormSymVel(1, 1, Stokes, Reynolds, time_step);
+    add_matrix_form(sym_form_1);
 
-CustomWeakFormPoisson::CustomWeakFormPoisson() : Hermes::Hermes2D::WeakForm<double>(2)
-{
-  this->add_matrix_form(new DefaultJacobianDiffusion<double>(0, 0));
-  this->add_matrix_form(new DefaultJacobianDiffusion<double>(1, 1));
+    BilinearFormUnSymVel_0_0* unsym_vel_form_0_0 = new BilinearFormUnSymVel_0_0(0, 0, Stokes);
+    add_matrix_form(unsym_vel_form_0_0);
+    BilinearFormUnSymVel_0_1* unsym_vel_form_0_1 = new BilinearFormUnSymVel_0_1(0, 1, Stokes);
+    add_matrix_form(unsym_vel_form_0_1);
+    BilinearFormUnSymVel_1_0* unsym_vel_form_1_0 = new BilinearFormUnSymVel_1_0(1, 0, Stokes);
+    add_matrix_form(unsym_vel_form_1_0);
+    BilinearFormUnSymVel_1_1* unsym_vel_form_1_1 = new BilinearFormUnSymVel_1_1(1, 1, Stokes);
+    add_matrix_form(unsym_vel_form_1_1);
 
-  // Residual.
-  this->add_vector_form(new DefaultResidualDiffusion<double>(0));
-  this->add_vector_form(new DefaultVectorFormVol<double>(0));
-  this->add_vector_form(new DefaultResidualDiffusion<double>(1));
-  this->add_vector_form(new DefaultVectorFormVol<double>(1));
-};
+    BilinearFormUnSymXVelPressure* unsym_velx_pressure_form = new BilinearFormUnSymXVelPressure(0, 2);
+    add_matrix_form(unsym_velx_pressure_form);
+
+    BilinearFormUnSymYVelPressure* unsym_vely_pressure_form = new BilinearFormUnSymYVelPressure(1, 2);
+    add_matrix_form(unsym_vely_pressure_form);
+
+    VectorFormNS_0* F_0 = new VectorFormNS_0(0, Stokes, Reynolds, time_step);
+    F_0->ext = Hermes::vector<MeshFunction<double>*>(x_vel_previous_time, y_vel_previous_time);
+    add_vector_form(F_0);
+    VectorFormNS_1* F_1 = new VectorFormNS_1(1, Stokes, Reynolds, time_step);
+    F_1->ext = Hermes::vector<MeshFunction<double>*>(x_vel_previous_time, y_vel_previous_time);
+    add_vector_form(F_1);
+    VectorFormNS_2* F_2 = new VectorFormNS_2(2);
+    add_vector_form(F_2);
+
+    // Jacobian volumetric part.
+    add_matrix_form(new BilinearFormTime(3, 3, time_step));
+
+    add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<double>(3, 3, "Outside", new Hermes1DFunction<double>(0.01)));
+    add_matrix_form(new WeakFormsH1::DefaultJacobianDiffusion<double>(3, 3, "Inner Circle", new Hermes1DFunction<double>(0.03)));
+    add_matrix_form(new CustomJacobianAdvection(3, 3, "Outside"));
+
+    // Residual - volumetric.
+    VectorFormTime *vft = new VectorFormTime(3, time_step);
+    Hermes::vector<MeshFunction<double>*> mesh_function_vector;
+    mesh_function_vector.push_back(T_prev_time);
+    vft->ext = mesh_function_vector;
+    add_vector_form(vft);
+
+    add_vector_form(new WeakFormsH1::DefaultResidualDiffusion<double>(3, "Outside", new Hermes1DFunction<double>(0.01)));
+    add_vector_form(new WeakFormsH1::DefaultResidualDiffusion<double>(3, "Inner Circle", new Hermes1DFunction<double>(0.03)));
+    add_vector_form(new CustomResidualAdvection(3, "Outside"));
+    
+    add_vector_form(new WeakFormsH1::DefaultVectorFormVol<double>(3, "Inner Circle", new Hermes::Hermes2DFunction<double>(-0.00001)));
+  };
--- hermes2d/tests/meshes/subdomains-advanced/main.cpp
+++ hermes2d/tests/meshes/subdomains-advanced/main.cpp
@@ -6,13 +6,47 @@ using namespace Hermes::Hermes2D;
 
 // This test makes sure that subdomains work correctly.
 
+const bool STOKES = false;
+
+#define PRESSURE_IN_L2                      
+
+// Initial polynomial degree for velocity components.
+const int P_INIT_VEL = 2;                         
+
+// Initial polynomial degree for pressure.
+// Note: P_INIT_VEL should always be greater than
+// P_INIT_PRESSURE because of the inf-sup condition.
+const int P_INIT_PRESSURE = 1;
+
+// Reynolds number.
+const double RE = 200.0;
+
+// Inlet velocity (reached after STARTUP_TIME).
+const double VEL_INLET = 1.0;
+
+// During this time, inlet velocity increases gradually
+// from 0 to VEL_INLET, then it stays constant.
+const double STARTUP_TIME = 1.0;
+
+const double TAU = 0.1;                           // Time step.
+const double T_FINAL = 30000.0;                   // Time interval length.
+const double NEWTON_TOL = 1e-4;                   // Stopping criterion for the Newton's method.
+const int NEWTON_MAX_ITER = 10;                   // Maximum allowed number of Newton iterations.
+const double H = 6;                               // Domain height (necessary to define the parabolic
+// velocity profile at inlet).
+
 // Uniform polynomial degree of mesh elements.
 const int P_INIT = 2;
 // Number of initial uniform mesh refinements.
-const int INIT_REF_NUM = 2;
+const int INIT_REF_NUM = 3;
 // Possibilities: SOLVER_AMESOS, SOLVER_AZTECOO, SOLVER_MUMPS,
 // SOLVER_PETSC, SOLVER_SUPERLU, SOLVER_UMFPACK.
 Hermes::MatrixSolverType matrix_solver_type = Hermes::SOLVER_UMFPACK;  
+
+// Boundary markers.
+const std::string BDY_INLET = "Inlet";
+const std::string BDY_OUTER = "Outer";
+const std::string BDY_INNER = "Inner";
 
 int main(int argc, char* argv[])
 {
@@ -27,52 +61,121 @@ int main(int argc, char* argv[])
     for(unsigned int meshes_i = 0; meshes_i < meshes.size(); meshes_i++)
       meshes[meshes_i]->refine_all_elements();
 
-  // Initialize the weak formulation.
-  CustomWeakFormPoisson wf;
 
-  // Initialize essential boundary conditions.
-  DefaultEssentialBCConst<double> bc_essential_whole_domain("b1", 0.0);
-  EssentialBCs<double> bcs_whole_domain(&bc_essential_whole_domain);
+  // Initialize boundary conditions.
+  // Flow.
+  EssentialBCNonConst bc_inlet_vel_x(BDY_INLET, VEL_INLET, H, STARTUP_TIME);
+  Hermes::Hermes2D::DefaultEssentialBCConst<double> bc_other_vel_x(Hermes::vector<std::string>(BDY_OUTER, BDY_INNER), 0.0);
+  Hermes::Hermes2D::EssentialBCs<double> bcs_vel_x(Hermes::vector<EssentialBoundaryCondition<double> *>(&bc_inlet_vel_x, &bc_other_vel_x));
+  Hermes::Hermes2D::DefaultEssentialBCConst<double> bc_vel_y(Hermes::vector<std::string>(BDY_INLET, BDY_OUTER, BDY_INNER), 0.0);
+  Hermes::Hermes2D::EssentialBCs<double> bcs_vel_y(&bc_vel_y);
+  Hermes::Hermes2D::EssentialBCs<double> bcs_pressure;
 
-  DefaultEssentialBCConst<double> bc_essential_without_hole(Hermes::vector<std::string>("b1", "b2"), 0.0);
-  EssentialBCs<double> bcs_without_hole(&bc_essential_without_hole);
+  // Temperature.
+  Hermes::Hermes2D::DefaultEssentialBCConst<double> bc_temperature(Hermes::vector<std::string>(BDY_INLET, BDY_OUTER), 0.0);
+  EssentialBCs<double> bcs_temperature(&bc_temperature);
+
+  // Spaces for velocity components and pressure.
+  H1Space<double> xvel_space(&mesh_without_hole, &bcs_vel_x, P_INIT_VEL);
+  H1Space<double> yvel_space(&mesh_without_hole, &bcs_vel_y, P_INIT_VEL);
+#ifdef PRESSURE_IN_L2
+  L2Space<double> p_space(&mesh_without_hole, P_INIT_PRESSURE);
+#else
+  H1Space<double> p_space(&mesh_without_hole, &bcs_pressure, P_INIT_PRESSURE);
+#endif
+  // Space for temperature.
+  H1Space<double> temperature_space(&mesh_whole_domain, &bcs_temperature, P_INIT);
+
+  // Calculate and report the number of degrees of freedom.
+  int ndof = Space<double>::get_num_dofs(Hermes::vector<Space<double> *>(&xvel_space, &yvel_space, &p_space, &temperature_space));
+  info("ndof = %d.", ndof);
+
+  // Define projection norms.
+  ProjNormType vel_proj_norm = HERMES_H1_NORM;
+#ifdef PRESSURE_IN_L2
+  ProjNormType p_proj_norm = HERMES_L2_NORM;
+#else
+  ProjNormType p_proj_norm = HERMES_H1_NORM;
+#endif
+  ProjNormType temperature_proj_norm = HERMES_H1_NORM;
+
+  // Solutions for the Newton's iteration and time stepping.
+  info("Setting initial conditions.");
+  ZeroInitialCondition xvel_prev_time(&mesh_without_hole), yvel_prev_time(&mesh_without_hole), p_prev_time(&mesh_without_hole), temperature_prev_time(&mesh_whole_domain); 
+
+  // Initialize weak formulation.
+  CustomWeakFormHeatAndFlow wf(STOKES, RE, TAU, &xvel_prev_time, &yvel_prev_time, &temperature_prev_time);
   
-  // Create H1 spaces with default shapeset.
-  H1Space<double> space_whole_domain(&mesh_whole_domain, &bcs_whole_domain, P_INIT);
-  int ndof_whole_domain = space_whole_domain.get_num_dofs();
-  info("Space whole domain ndof = %d", ndof_whole_domain);
-
-  H1Space<double> space_without_hole(&mesh_without_hole, &bcs_without_hole, P_INIT);
-  int ndof_without_hole = space_without_hole.get_num_dofs();
-  info("Space bottom left corner ndof = %d", ndof_without_hole);
-
-  Views::BaseView<double> b;
-  b.show(&space_whole_domain);
-  b.wait_for_keypress();
-  b.show(&space_without_hole);
-  b.wait_for_close();
-
   // Initialize the FE problem.
-  Hermes::Hermes2D::DiscreteProblem<double> dp(&wf, Hermes::vector<Space<double>*>(&space_whole_domain, &space_without_hole));
+  DiscreteProblem<double> dp(&wf, Hermes::vector<Space<double> *>(&xvel_space, &yvel_space, &p_space, &temperature_space));
 
-  // Initial coefficient vector for the Newton's method.  
-  double* coeff_vec = new double[ndof_whole_domain + ndof_without_hole];
-  memset(coeff_vec, 0, (ndof_whole_domain + ndof_without_hole)*sizeof(double));
-
-  // Perform Newton's iteration and translate the resulting coefficient vector into a Solution.
-  Hermes::Hermes2D::Solution<double> sln_whole_domain, sln_without_hole;
+  // Initialize the Newton solver.
   Hermes::Hermes2D::NewtonSolver<double> newton(&dp, matrix_solver_type);
-  if (!newton.solve(coeff_vec)) 
-    error("Newton's iteration failed.");
-  else
-    Hermes::Hermes2D::Solution<double>::vector_to_solutions(newton.get_sln_vector(), Hermes::vector<Space<double>*>(&space_whole_domain, &space_without_hole), Hermes::vector<Solution<double>*>(&sln_whole_domain, &sln_without_hole));
 
-  Views::ScalarView<double> s("Solution on the whole domain");
-  s.show(&sln_whole_domain);
-  s.wait_for_keypress();
-  s.set_title("Solution on the complement of the hole");
-  s.show(&sln_without_hole);
-  s.wait_for_close();
+  // Initialize views.
+  Views::VectorView<double> vview("velocity [m/s]", new Views::WinGeom(0, 0, 500, 300));
+  Views::ScalarView<double> pview("pressure [Pa]", new Views::WinGeom(0, 310, 500, 300));
+  Views::ScalarView<double> tempview("temperature [Pa]", new Views::WinGeom(510, 0, 500, 300));
+  vview.set_min_max_range(0, 1.6);
+  vview.fix_scale_width(80);
+  //pview.set_min_max_range(-0.9, 1.0);
+  pview.fix_scale_width(80);
+  pview.show_mesh(true);
 
+  // Project the initial condition on the FE space to obtain initial
+  // coefficient vector for the Newton's method.
+  double* coeff_vec = new double[ndof];
+  info("Projecting initial condition to obtain initial vector for the Newton's method.");
+  OGProjection<double>::project_global(Hermes::vector<Space<double> *>(&xvel_space, &yvel_space, &p_space, &temperature_space), 
+    Hermes::vector<MeshFunction<double> *>(&xvel_prev_time, &yvel_prev_time, &p_prev_time, &temperature_prev_time), 
+    coeff_vec, matrix_solver_type, 
+    Hermes::vector<ProjNormType>(vel_proj_norm, vel_proj_norm, p_proj_norm, temperature_proj_norm));
+
+  // Time-stepping loop:
+  char title[100];
+  int num_time_steps = T_FINAL / TAU;
+  double current_time = 0.0;
+  for (int ts = 1; ts <= num_time_steps; ts++)
+  {
+    current_time += TAU;
+    info("---- Time step %d, time = %g:", ts, current_time);
+
+    // Update time-dependent essential BCs.
+    if (current_time <= STARTUP_TIME) 
+    {
+      info("Updating time-dependent essential BC.");
+      Space<double>::update_essential_bc_values(Hermes::vector<Space<double> *>(&xvel_space, &yvel_space, &p_space, &temperature_space), current_time);
+    }
+
+    // Perform Newton's iteration.
+    info("Solving nonlinear problem:");
+    bool verbose = true;
+    // Perform Newton's iteration and translate the resulting coefficient vector into previous time level solutions.
+    newton.set_verbose_output(verbose);
+    if (!newton.solve(coeff_vec, NEWTON_TOL, NEWTON_MAX_ITER)) 
+      error("Newton's iteration failed.");
+    else
+    {
+      Hermes::vector<Solution<double> *> tmp(&xvel_prev_time, &yvel_prev_time, &p_prev_time, &temperature_prev_time);
+      Hermes::Hermes2D::Solution<double>::vector_to_solutions(newton.get_sln_vector(), Hermes::vector<Space<double> *>(&xvel_space, &yvel_space, &p_space, &temperature_space), tmp);
+    }
+    
+    // Show the solution at the end of time step.
+    sprintf(title, "Velocity, time %g", current_time);
+    vview.set_title(title);
+    vview.show(&xvel_prev_time, &yvel_prev_time, Views::HERMES_EPS_LOW);
+    sprintf(title, "Pressure, time %g", current_time);
+    pview.set_title(title);
+    pview.show(&p_prev_time);
+    tempview.show(&temperature_prev_time);
+    vview.save_numbered_screenshot("Velocity %i.bmp", ts);
+    pview.save_numbered_screenshot("Pressure %i.bmp", ts);
+    tempview.save_numbered_screenshot("Temperature %i.bmp", ts);
+  }
+
+  delete [] coeff_vec;
+
+  // Wait for all views to be closed.
+  Views::View::wait();
   return 0;
 }
