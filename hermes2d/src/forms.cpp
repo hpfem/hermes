3b3e71527b00a23aae6a1ca9d857863eeaecf80d
Lukas Korous <lukas.korous@gmail.com>
2011-10-18 22:14:15 +0200

    Fix init_geom_surf().

--- hermes2d/src/forms.cpp
+++ hermes2d/src/forms.cpp
@@ -364,7 +364,7 @@ namespace Hermes
       e->elem_marker = rm->get_active_element()->marker;
       e->diam = rm->get_active_element()->get_diameter();
       e->area = rm->get_active_element()->get_area();
-      e->id = rm->get_active_element()->en[surf_pos->surf_num]->id;
+      e->id = rm->get_active_element()->id;
       e->x = rm->get_phys_x(order);
       e->y = rm->get_phys_y(order);
       double3 *tan;
