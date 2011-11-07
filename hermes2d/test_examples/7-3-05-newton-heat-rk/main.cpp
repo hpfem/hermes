502edaed1600d465cdfd67a41b6a27d7993e6f52
Lukas Korous <lukas.korous@gmail.com>
2011-11-07 10:42:38 +0100

    Fix the example R-K.

--- hermes2d/test_examples/7-3-05-newton-heat-rk/main.cpp
+++ hermes2d/test_examples/7-3-05-newton-heat-rk/main.cpp
@@ -127,7 +127,7 @@ int main(int argc, char* argv[])
     try
     {
       runge_kutta.rk_time_step_newton(current_time, time_step, sln_time_prev,
-                                  sln_time_new, freeze_jacobian, false, verbose);
+                                  sln_time_new, freeze_jacobian, true, verbose);
     }
     catch(Exceptions::Exception& e){
       e.printMsg();
