dee3c54fc1242733c233c4b18ea482d98d455800
Lukas Korous <lukas.korous@gmail.com>
2012-06-01 14:00:29 +0200

    Fix in an example.

--- hermes2d/test_examples/3-07-navier-stokes/tests/main.cpp
+++ hermes2d/test_examples/3-07-navier-stokes/tests/main.cpp
@@ -136,7 +136,6 @@ int main(int argc, char* argv[])
     }
 
     // Perform Newton's iteration.
-    info("Solving nonlinear problem using %i threads:", Global<double>::Hermes_omp_get_max_threads());
     bool verbose = true;
     // Perform Newton's iteration and translate the resulting coefficient vector into previous time level solutions.
     newton.set_verbose_output(verbose);
