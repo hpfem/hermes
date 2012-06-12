ca344c6225cadd50c714e71bb5628dc58bdd45fc
Lukas Korous <lukas.korous@gmail.com>
2012-06-12 09:18:23 +0200

    Merge SuperLU files.

--- hermes_common/src/solvers/superlu_solver.cpp
+++ hermes_common/src/solvers/superlu_solver.cpp
@@ -25,6 +25,8 @@
 #include "trace.h"
 #include "error.h"
 #include "callstack.h"
+#include <slu_ddefs.h>
+#include <slu_zdefs.h>
 
 using namespace Hermes::Error;
 
@@ -970,6 +972,53 @@ namespace Hermes
         SLU_DESTROY_U(&U);
         inited = false;
       }
+    }
+
+    template <>
+    void  SuperLUSolver<double>::solver_driver (superlu_options_t *options, SuperMatrix *A, int *perm_c, int *perm_r, int *etree, char *equed, double *R,
+      double *C, SuperMatrix *L, SuperMatrix *U, void *work, int lwork, SuperMatrix *B, SuperMatrix *X,
+      double *recip_pivot_growth, double *rcond, double *ferr, double *berr, slu_memusage_t *mem_usage, SuperLUStat_t *stat,
+      int *info)
+    {
+      dgssvx(options, A, perm_c, perm_r, etree, equed, R, C, L, U, work, lwork, B, X, recip_pivot_growth, rcond, ferr, berr, (mem_usage_t*) mem_usage, stat, info);
+    }
+
+    template <>
+    void SuperLUSolver<double>::create_csc_matrix (SuperMatrix *A, int m, int n, int nnz, SuperLuType<double>::Scalar *nzval,
+      int *rowind, int *colptr, Stype_t stype, Dtype_t dtype, Mtype_t mtype)
+    {
+      dCreate_CompCol_Matrix (A, m, n, nnz, nzval, rowind, colptr, stype, dtype, mtype);
+    }
+
+    template<>
+    void SuperLUSolver<double>::create_dense_matrix (SuperMatrix *X, int m, int n, SuperLuType<double>::Scalar *x,
+      int ldx, Stype_t stype, Dtype_t dtype, Mtype_t mtype)
+    {
+      dCreate_Dense_Matrix (X, m, n, (double*) x, ldx, stype, dtype, mtype);
+    }
+
+    template <>
+    void SuperLUSolver<std::complex<double> >::create_csc_matrix (SuperMatrix *A, int m, int n, int nnz,
+      SuperLuType<std::complex<double> >::Scalar *nzval,
+      int *rowind, int *colptr, Stype_t stype, Dtype_t dtype, Mtype_t mtype)
+    {
+      zCreate_CompCol_Matrix (A, m, n, nnz, (doublecomplex*) nzval, rowind, colptr, stype, dtype, mtype);
+    }
+
+    template<>
+    void SuperLUSolver<std::complex<double> >::create_dense_matrix (SuperMatrix *X, int m, int n, SuperLuType<std::complex<double> >::Scalar *x,
+      int ldx, Stype_t stype, Dtype_t dtype, Mtype_t mtype)
+    {
+      zCreate_Dense_Matrix (X, m, n, (doublecomplex *) x, ldx, stype, dtype, mtype);
+    }
+
+    template <>
+    void  SuperLUSolver<std::complex<double> >::solver_driver (superlu_options_t *options, SuperMatrix *A, int *perm_c, int *perm_r, int *etree, char *equed, double *R,
+      double *C, SuperMatrix *L, SuperMatrix *U, void *work, int lwork, SuperMatrix *B, SuperMatrix *X,
+      double *recip_pivot_growth, double *rcond, double *ferr, double *berr, slu_memusage_t *mem_usage, SuperLUStat_t *stat,
+      int *info)
+    {
+      zgssvx(options, A, perm_c, perm_r, etree, equed, R, C, L, U, work, lwork, B, X, recip_pivot_growth, rcond, ferr, berr, (mem_usage_t*) mem_usage, stat, info);
     }
 
 #ifdef SLU_MT
