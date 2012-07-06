bb2db80f2e8b40dce26891997594b587d2334dd8
Lukas Korous <lukas.korous@gmail.com>
2012-07-06 14:42:04 +0200

    Fix of looking for execinfo.

--- cmake/FindEXECINFO.cmake
+++ cmake/FindEXECINFO.cmake
@@ -9,40 +9,9 @@ MESSAGE(STATUS "Looking for execinfo")
 	                /opt/local/include
 	                NO_DEFAULT_PATH)
 	
-FIND_LIBRARY(EXECINFO_LIBRARY_PATH
-	        NAMES
-	                execinfo
-	        PATHS
-	                /lib64
-	                /usr/lib64
-	                /usr/local/lib64
-	                /lib/x86_64-linux-gnu
-                /usr/lib/x86_64-linux-gnu
-	                /opt/local/lib64
-	                /sw/lib64
-	                /lib
-	                /usr/lib
-                /usr/local/lib
-	                /lib/i386-linux-gnu
-	                /usr/lib/i386-linux-gnu
-	                /opt/local/lib
-	                /sw/lib
-	                NO_DEFAULT_PATH)
-	
 	IF(EXECINFO_INCLUDE_PATH)
 	        SET(EXECINFO_FOUND 1 CACHE STRING "Set to 1 if execinfo is found, 0 otherwise")
 	        MESSAGE(STATUS "Looking for execinfo headers - found")
-	ELSE(EXECINFO_INCLUDE_PATH)
-	        SET(EXECINFO_FOUND 0 CACHE STRING "Set to 1 if execinfo is found, 0 otherwise")
-	        MESSAGE(FATAL_ERROR "Looking for execinfo headers - not found")
 	ENDIF(EXECINFO_INCLUDE_PATH)
-	
-	IF(EXECINFO_LIBRARY_PATH)
-	        SET(EXECINFO_FOUND 1 CACHE STRING "Set to 1 if execinfo is found, 0 otherwise")
-	        MESSAGE(STATUS "Looking for execinfo library - found")
-	ELSE(EXECINFO_LIBRARY_PATH)
-	        SET(EXECINFO_FOUND 0 CACHE STRING "Set to 1 if execinfo is found, 0 otherwise")
-	        MESSAGE(FATAL_ERROR "Looking for execinfo library - not found")
-	ENDIF(EXECINFO_LIBRARY_PATH)
 	
 	MARK_AS_ADVANCED(EXECINFO_FOUND)
