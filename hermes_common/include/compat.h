c6cf67c50d6ad497c0b036073344f4b8da95b763
Lukas Korous <lukas.korous@gmail.com>
2012-09-18 17:21:11 +0200

    Take out old EXPORT_HERMES_MODULE stuff.

--- hermes_common/include/compat.h
+++ hermes_common/include/compat.h
@@ -43,20 +43,6 @@ FILE *fmemopen (void *buf, size_t size, const char *opentype);
   #define HERMES_API
 #endif
 
-// Windows DLL export/import definitions
-#if defined(WIN32) || defined(_WINDOWS)
-  // Visual Studio 2010.
-  #if defined(EXPORT_HERMES_MODULE)
-    // when building DLL (target project defines this macro)
-    #define HERMES_MODULE_API __declspec(dllexport)
-  #else  
-    // when using the DLL by a client project
-    #define HERMES_MODULE_API __declspec(dllimport)
-  #endif
-#else 
-  #define HERMES_MODULE_API
-#endif
-
 #ifndef HAVE_STRCASECMP
 #define strcasecmp strcmp
 #endif
