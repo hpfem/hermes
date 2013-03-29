8c931cf9d84ef111f924c647048b3846c98193df
Lukas Korous <lukas.korous@gmail.com>
2013-03-29 12:10:08 +0100

    Add TCMALLOC to cmake.

---
+++ cmake/FindTCMALLOC.cmake
@@ -0,0 +1,16 @@
+#
+# TCMALLOC
+# FROM http://code.google.com/p/google-perftools/
+#
+
+SET(MY_TCMALLOC_LIB_DIRS ${TCMALLOC_ROOT}/lib) 
+SET(MY_TCMALLOC_INC_DIRS ${TCMALLOC_ROOT}/include)
+
+FIND_PATH(TCMALLOC_INCLUDE_DIR tcmalloc.h ${MY_TCMALLOC_INC_DIRS})
+
+FIND_LIBRARY(TCMALLOC_LIBRARY  NAMES tcmalloc_minimal tcmalloc_minimal_debug libtcmalloc_minimal libtcmalloc_minimal-debug PATHS ${MY_TCMALLOC_LIB_DIRS}) 
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(  TCMALLOC "TCMALLOC could not be found." 
+   TCMALLOC_LIBRARY TCMALLOC_INCLUDE_DIR
+)
