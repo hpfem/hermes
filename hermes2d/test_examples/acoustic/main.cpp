3196569b8bd5c514ec0400430a1e43414a5743bb
Lukas Korous <lukas.korous@gmail.com>
2013-04-30 18:05:41 +0200

    Work on acoustics.

--- hermes2d/test_examples/acoustic/main.cpp
+++ hermes2d/test_examples/acoustic/main.cpp
@@ -8,11 +8,12 @@ using namespace Hermes::Hermes2D::RefinementSelectors;
 // Initial polynomial degree of mesh elements.
 const int P_INIT = 1;
 // Tiem step.
-const double time_step = 3e-5;
+const double time_step = 5e-5;
 // Initial number of uniform refinements.
-const int INIT_REF_NUM = 2;
+const int INIT_REF_NUM = 4;
+const int INIT_REF_NUM_BDY = 5;
 // This is a quantitative parameter of Adaptivity.
-const double THRESHOLD = 0.666;
+const double THRESHOLD = 0.75;
 // This is a stopping criterion for Adaptivity.
 const AdaptivityStoppingCriterion stoppingCriterion = AdaptStoppingCriterionSingleElement;
 // Predefined list of element refinement candidates.
@@ -25,7 +26,8 @@ const CalculatedErrorType errorType = RelativeErrorToGlobalNorm;
 // Stopping criterion for adaptivity.
 double REFINEMENT_THRESHOLD(double time, double norm)
 {
-  double toReturn = 5.0;
+  return 10050.0;
+  double minimum = 0.5;
   /*
     if(time > time_step * 10)
     {
@@ -33,17 +35,49 @@ double REFINEMENT_THRESHOLD(double time, double norm)
         toReturn = toReturn / (std::log10(norm / 1e10) + 1);
     }
 
-  */
   
-  toReturn = toReturn / std::max(std::log10(time / time_step) / 3, 1.0);
+  toReturn = (toReturn - minimum) * std::min(10 * time_step / time, 1.0) + minimum;
 
   std::cout << "\tCurrent refinement threshold: " << toReturn << std::endl;
 
   return toReturn;
+  */
 }
 
 double AMPLITUDE = 1.0;
 double FREQUENCY = 1000;
+
+// Derefinement time_step
+const int derefinement_period = 10000;
+const int adaptivity_offset = 10;
+
+Hermes::Hermes2D::Views::ScalarView viewS("Solution");
+Views::OrderView oview("Polynomial orders");
+Linearizer lin;
+Orderizer ord;
+ // DOF and CPU convergence graphs.
+SimpleGraph graph_dof_est;
+
+void output(MeshFunctionSharedPtr<double> rsln_value, SpaceSharedPtr<double> rspace_value)
+{
+  viewS.set_min_max_range(-AMPLITUDE, AMPLITUDE);
+  //viewS.show(rsln_value);
+  //oview.show(rspace_value);
+
+  // Add entry to DOF and CPU convergence graphs.
+  /*
+  graph_dof_est.add_values(rspace_value->get_num_dofs(), err_est_rel);
+  graph_dof_est.save("conv_dof_est.dat");
+
+  char* filename = new char[1000];
+  sprintf(filename, "Solution-%i.vtk", iteration);
+  lin.save_solution_vtk(rsln_value, filename, "sln", true);
+  sprintf(filename, "Orders-%i.vtk", iteration);
+  ord.save_orders_vtk(rspace_value, filename);
+  sprintf(filename, "Mesh-%i.vtk", iteration);
+  ord.save_mesh_vtk(rspace_value, filename);
+  */
+}
 
 class MySelector : public H1ProjBasedSelector<double>
 {
@@ -65,7 +99,7 @@ private:
       if(cand.error < unrefined.error)
       {
         double delta_dof = cand.dofs - unrefined.dofs;
-        candidates[i].score = (log10(unrefined.error) - log10(cand.error)) / delta_dof;
+        candidates[i].score = std::log(unrefined.error / cand.error) * std::pow((double)unrefined.dofs / (double)cand.dofs, 2.0);
       }
       else
         candidates[i].score = 0;
@@ -104,10 +138,9 @@ int main(int argc, char* argv[])
   */
 
   for(int i = 0; i < INIT_REF_NUM; i++)
-  {
     mesh->refine_all_elements(0, true);
-    mesh->refine_towards_boundary("Source", 1, true, true);
-  }
+
+  mesh->refine_towards_boundary("Source", INIT_REF_NUM_BDY, false);
   
   std::string matched_boundary("Wall");
   double matched_boundaries_values(345*1.25);
@@ -132,17 +165,13 @@ int main(int argc, char* argv[])
   Hermes::vector<MeshFunctionSharedPtr<double> > rslns(rsln_value, rsln_derivative);
   Hermes::vector<MeshFunctionSharedPtr<double> > prevslns(prev_value, prev_derivative);
 
-  Hermes::Hermes2D::Views::ScalarView viewS("Solution");
-  viewS.set_min_max_range(-AMPLITUDE, AMPLITUDE);
-  Views::OrderView oview("Polynomial orders");
-
   MyWeakForm wf(matched_boundary, matched_boundaries_values, prevslns);
   wf.set_current_time_step(time_step);
 
   // Initialize linear solver.
   Hermes::Hermes2D::LinearSolver<double> linear_solver(&wf, spaces);
-  linear_solver.set_do_not_use_cache();
-  linear_solver.set_jacobian_constant();
+  // linear_solver.set_do_not_use_cache();
+  // linear_solver.set_jacobian_constant();
 
   // Adaptivity.
   DefaultErrorCalculator<double, HERMES_H1_NORM> error_calculator(errorType, 2);
@@ -152,31 +181,26 @@ int main(int argc, char* argv[])
   MySelector selector(CAND_LIST);
   Hermes::vector<Selector<double>*> selectors(&selector, &selector);
 
-  // DOF and CPU convergence graphs.
-  SimpleGraph graph_dof_est;
-  
-  // Solve the linear problem.
+ // Solve the linear problem.
   unsigned int iteration = 0;
 
   // Unrefine when derefinement_threshold is reached.
-  bool derefinement_threshold_met = false;
+  bool refined = false;
 
   for(double time = time_step; time < 1.0; time += time_step)
   {
     Hermes::Mixins::Loggable::Static::info("\n\nIteration: %u, Time: %g.", iteration, time);
+    
     Space<double>::update_essential_bc_values(spaces, time);
-
     // Periodic global derefinement.
-    bool derefined_in_this_step = false;
-    if (derefinement_threshold_met)
+    if (iteration > 1 && iteration % derefinement_period == 1 && refined)
     {
       Hermes::Mixins::Loggable::Static::info("\t! Global mesh derefinement.");
       mesh->unrefine_all_elements();
       space_value->adjust_element_order(-1, -1, P_INIT, P_INIT);
       space_derivative->adjust_element_order(-1, -1, P_INIT, P_INIT);
       Space<double>::assign_dofs(Hermes::vector<SpaceSharedPtr<double> >(space_value, space_derivative));
-      derefinement_threshold_met = false;
-      derefined_in_this_step = true;
+      refined = false;
     }
 
     int as = 1;
@@ -200,68 +224,59 @@ int main(int argc, char* argv[])
       
       Solution<double>::vector_to_solutions(linear_solver.get_sln_vector(), rspaces, rslns);
       OGProjection<double>::project_global(spaces, rslns, slns);
+
+      output(rslns[0], rspaces[0]);
+
       error_calculator.calculate_errors(slns, rslns);
-      double err_est_rel = error_calculator.get_error_squared(0) * 100.0;
+      double err_est_rel = error_calculator.get_total_error_squared() * 100.0;
 
       // Report results.
       Hermes::Mixins::Loggable::Static::info("\tTotal error: %g%%.", err_est_rel);
       Hermes::Mixins::Loggable::Static::info("\tTotal norm: %g.", error_calculator.get_total_norm_squared());
 
-      // View the coarse mesh solution and polynomial orders.
-      viewS.show(rsln_value);
-      oview.show(rspace_value);
-
-      // Add entry to DOF and CPU convergence graphs.
-      /*
-      graph_dof_est.add_values(rspace_value->get_num_dofs(), err_est_rel);
-      graph_dof_est.save("conv_dof_est.dat");
-
-      Linearizer lin;
-      char* filename = new char[1000];
-      sprintf(filename, "Solution-%i.vtk", iteration);
-      lin.save_solution_vtk(rsln_value, filename, "sln", true);
-      Orderizer ord;
-      sprintf(filename, "Orders-%i.vtk", iteration);
-      ord.save_orders_vtk(rspace_value, filename);
-      sprintf(filename, "Mesh-%i.vtk", iteration);
-      ord.save_mesh_vtk(rspace_value, filename);
-      */
-
-      if(derefinement_threshold_met && err_est_rel < REFINEMENT_THRESHOLD(time, error_calculator.get_total_norm_squared()))
-        break;
-
+      bool derefine_step_reached = false;
       while(err_est_rel < REFINEMENT_THRESHOLD(time, error_calculator.get_total_norm_squared()))
       {
+        if((iteration + 1) % derefinement_period == 1)
+        {
+          derefine_step_reached = true;
+          break;
+        }
+
         prev_value->copy(rsln_value);
         prev_derivative->copy(rsln_derivative);
         time += time_step;
-        derefined_in_this_step = false;
         as = 1;
         Hermes::Mixins::Loggable::Static::info("\n\nIteration - inner loop: %u, Time: %g.", ++iteration, time);
         Hermes::Mixins::Loggable::Static::info("\tCoarse DOF: %d,  Fine DOF: %d.", Space<double>::get_num_dofs(spaces), Space<double>::get_num_dofs(rspaces));
+        Space<double>::update_essential_bc_values(spaces, time);
         Space<double>::update_essential_bc_values(rspaces, time);
         
         linear_solver.solve();
         
         Solution<double>::vector_to_solutions(linear_solver.get_sln_vector(), rspaces, rslns);
         OGProjection<double>::project_global(spaces, rslns, slns);
+
+        output(rslns[0], rspaces[0]);
+        
         error_calculator.calculate_errors(slns, rslns);
-        err_est_rel = error_calculator.get_error_squared(0) * 100.0;
+        err_est_rel = error_calculator.get_total_error_squared() * 100.0;
         
         // Report results.
         Hermes::Mixins::Loggable::Static::info("\tTotal error: %g%%.", err_est_rel);
         Hermes::Mixins::Loggable::Static::info("\tTotal norm: %g.", error_calculator.get_total_norm_squared());
-
-        viewS.show(rsln_value);
-        oview.show(rspace_value);
       }
-      
+      if(derefine_step_reached)
+        break;
+
       // We are above the error.
+      if(iteration > adaptivity_offset)
       {
         adaptivity.adapt(selectors);
-        if(!derefined_in_this_step)
-          derefinement_threshold_met = true;
+        refined = true;
       }
+      else
+        break;
     }
 
     prev_value->copy(rsln_value);
