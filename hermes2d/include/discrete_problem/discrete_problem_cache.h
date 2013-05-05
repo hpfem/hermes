38a17a55919111ae890a57635cbd0000f941562a
Lukas Korous <lukas.korous@gmail.com>
2013-05-05 17:09:41 +0200

    Take back some non-wanted changes.
    Conflicts:
    	hermes2d/include/discrete_problem/discrete_problem_cache.h

--- hermes2d/include/discrete_problem/discrete_problem_cache.h
+++ hermes2d/include/discrete_problem/discrete_problem_cache.h
@@ -123,9 +123,9 @@ namespace Hermes
       int recordCount;
 
       /// Starting size of the recordTable.
-      static const int DEFAULT_SIZE = 1e5;
+      static const int DEFAULT_SIZE = 2e5;
       /// Average number of subelements.
-      static const int GUESS_NUMBER_OF_SUBELEMENTS = 16;
+      static const int GUESS_NUMBER_OF_SUBELEMENTS = 32;
       /// Starting size of the hashTable.
       static const int DEFAULT_HASH_TABLE_SIZE = DEFAULT_SIZE * GUESS_NUMBER_OF_SUBELEMENTS;
 #pragma endregion
