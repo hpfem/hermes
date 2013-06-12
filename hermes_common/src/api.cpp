45b6d2e3862bac9512754a3bfb1aaaeb2066cee5
Lukas Korous <lukas.korous@gmail.com>
2013-06-12 18:54:02 +0200

    A fix when PARALUTION is missing.

--- hermes_common/src/api.cpp
+++ hermes_common/src/api.cpp
@@ -49,8 +49,10 @@ namespace Hermes
     this->parameters.insert(std::pair<HermesCommonApiParam, Parameter*> (Hermes::directMatrixSolverType, new Parameter(SOLVER_UMFPACK)));
 
     // Set handlers.
+#ifdef WITH_PARALUTION
     this->setter_handlers.insert(std::pair<std::pair<HermesCommonApiParam, int>, typename Api::SetterHandler>(std::pair<HermesCommonApiParam, int>(Hermes::matrixSolverType, SOLVER_PARALUTION), &ParalutionInitialization::init_paralution));
     this->change_handlers.insert(std::pair<std::pair<HermesCommonApiParam, int>, typename Api::SetterHandler>(std::pair<HermesCommonApiParam, int>(Hermes::matrixSolverType, SOLVER_PARALUTION), &ParalutionInitialization::deinit_paralution));
+#endif
 
     // Initialize TCMalloc (this line also serves for TCMalloc not to be linker-optimized out).
 #ifdef WITH_TC_MALLOC
