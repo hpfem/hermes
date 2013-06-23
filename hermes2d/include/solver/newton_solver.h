d95e9bab7c2db9fa5148741b0e6e3a2db4992164
Lukas Korous <lukas.korous@gmail.com>
2013-06-23 16:07:49 +0200

    Add Newton: p_iteration_with_recalculated_jacobian.

--- hermes2d/include/solver/newton_solver.h
+++ hermes2d/include/solver/newton_solver.h
@@ -271,6 +271,7 @@ namespace Hermes
       const OutputParameterDoubleVector& damping_coefficients() const { return this->p_damping_coefficients; };
       const OutputParameterBool& residual_norm_drop() const { return this->p_residual_norm_drop; };
       const OutputParameterUnsignedInt& iteration() const { return this->p_iteration; };
+      const OutputParameterUnsignedInt& iteration_with_recalculated_jacobian() const { return this->p_iteration_with_recalculated_jacobian; };
 
     private:
       // Parameters for OutputAttachable mixin.
@@ -282,6 +283,7 @@ namespace Hermes
       OutputParameterDoubleVector p_damping_coefficients;
       OutputParameterBool p_residual_norm_drop;
       OutputParameterUnsignedInt p_iteration;
+      OutputParameterUnsignedInt p_iteration_with_recalculated_jacobian;
 #pragma endregion
 
     private:
--- hermes2d/src/solver/newton_solver.cpp
+++ hermes2d/src/solver/newton_solver.cpp
@@ -383,7 +383,7 @@ namespace Hermes
     {
       memcpy(this->sln_vector, coeff_vec, ndof * sizeof(Scalar));
       this->tick();
-      this->info("\tNewton: solution duration: %f s.\n", this->last());
+      this->info("\tNewton: solution duration: %f s, iterations: %i.\n", this->last(), this->get_parameter_value(this->p_iteration), this->get_parameter_value(this->p_iteration_with_recalculated_jacobian));
       this->on_finish();
       this->deinit_solving(coeff_vec);
     }
@@ -427,6 +427,7 @@ namespace Hermes
       {
         this->matrix_solver->set_factorization_scheme(HERMES_FACTORIZE_FROM_SCRATCH);
         this->dp->assemble(coeff_vec, this->jacobian, this->residual);
+        this->get_parameter_value(this->p_iteration_with_recalculated_jacobian)++;
         this->jacobian_reusable = true;
       }
       if(this->report_cache_hits_and_misses)
@@ -531,6 +532,7 @@ namespace Hermes
 #pragma region parameter_setup
       // The Newton's loop.
       unsigned int it = 1;
+      unsigned int it_with_recalculated_jacobian = 0;
       unsigned int successful_steps_damping = 0;
       unsigned int successful_steps_jacobian = 0;
       Hermes::vector<double> residual_norms;
@@ -549,6 +551,7 @@ namespace Hermes
       this->set_parameter_value(this->p_successful_steps_damping, &successful_steps_damping);
       this->set_parameter_value(this->p_successful_steps_jacobian, &successful_steps_jacobian);
       this->set_parameter_value(this->p_iteration, &it);
+      this->set_parameter_value(this->p_iteration_with_recalculated_jacobian, &it_with_recalculated_jacobian);
       this->set_parameter_value(this->p_residual_norm_drop, &residual_norm_drop);
       this->set_parameter_value(this->p_damping_coefficients, &damping_coefficients);
 #pragma endregion
@@ -658,6 +661,7 @@ namespace Hermes
 
         // Reassemble the jacobian once not reusable anymore.
         this->info("\tNewton: re-calculating Jacobian.");
+        this->get_parameter_value(this->p_iteration_with_recalculated_jacobian)++;
         this->dp->assemble(coeff_vec, this->jacobian);
         if(this->report_cache_hits_and_misses)
           this->add_cache_hits_and_misses(this->dp);
