c6b079a16d7d746d6666280f95e33a1eb7fa6664
Lukas Korous <lukas.korous@gmail.com>
2013-07-10 10:21:27 +0200

    Fix FindBSON.cmake for Win.

--- cmake/FindBSON.cmake
+++ cmake/FindBSON.cmake
@@ -3,12 +3,12 @@
 # FROM https://github.com/mongodb/mongo-c-driver
 #
 
-FIND_PATH(BSON_INCLUDE_DIR bson.h ${BSON_ROOT}/src /usr/local/include /usr/include)
+FIND_PATH(BSON_INCLUDE_DIR bson.h ${BSON_ROOT}/src ${BSON_ROOT}/include /usr/local/include /usr/include)
 
 if(WIN64)
   FIND_LIBRARY(BSON_LIBRARY bson ${BSON_ROOT}/lib/x64 ${BSON_ROOT})
 else(WIN64)  
-  FIND_LIBRARY(BSON_LIBRARY bson ${BSON_ROOT} /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
+  FIND_LIBRARY(BSON_LIBRARY bson ${BSON_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
 endif(WIN64)
 
 # Report the found libraries, quit with fatal error if any required library has not been found.
