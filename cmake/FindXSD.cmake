0dcab13f9fb136ad55ae38bf32ee08d704215b04
Lukas Korous <lukas.korous@gmail.com>
2013-07-31 08:41:24 +0200

    CMake fixes.

--- cmake/FindXSD.cmake
+++ cmake/FindXSD.cmake
@@ -6,7 +6,7 @@ SET(XSD_INCLUDE_SEARCH_PATH
 
 FIND_PATH(XSD_INCLUDE_DIR xsd/cxx/pre.hxx xsd/cxx/xml/dom/parsing-source.hxx xsd/cxx/post.hxx xsd/cxx/xml/sax/std-input-source.hxx xsd/cxx/tree/error-handler.hxx ${XSD_INCLUDE_SEARCH_PATH})
 
-FIND_PROGRAM(XSD_BIN xsd xsdcxx)
+FIND_PROGRAM(XSD_BIN NAMES xsd xsdcxx)
 
 # Report the found libraries, quit with fatal error if any required library has not been found.
 INCLUDE(FindPackageHandleStandardArgs)
