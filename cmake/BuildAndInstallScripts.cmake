b7d110dd664b0d4ac9aaf6a36e825dd2f8ef6044
Lukas Korous <lukas.korous@gmail.com>
2013-08-05 09:20:53 +0200

    Some fixes in CMake.

--- cmake/BuildAndInstallScripts.cmake
+++ cmake/BuildAndInstallScripts.cmake
@@ -1,26 +1,27 @@
 # Macro for generating classes for XML mesh parsing according to XSD
 macro(GENERATE_XSD_FILES PROJECT_NAME HEADER_XML_FILE_OUTPUT SOURCE_XML_FILE_OUTPUT XSD_FILE TARGET_DIR)
-  add_custom_target(${PROJECT_NAME} ALL DEPENDS ${HEADER_XML_FILE_OUTPUT} ${SOURCE_XML_FILE_OUTPUT})
+  add_custom_target(${PROJECT_NAME} ALL DEPENDS ${HEADER_XML_FILE_OUTPUT} "src/${SOURCE_XML_FILE_OUTPUT}")
 
 IF(WIN32)
+  MAKE_PATH(PATH_FOR_MOVE "${PROJECT_SOURCE_DIR}/include/${SOURCE_XML_FILE_OUTPUT}")
   ADD_CUSTOM_COMMAND(
         SOURCE    ${XSD_FILE}
         COMMAND   ${XSD_BIN} ARGS cxx-tree --generate-doxygen --generate-ostream --hxx-suffix .h --cxx-suffix .cpp --root-element-first --generate-polymorphic --generate-serialization --output-dir include/${TARGET_DIR} ${XSD_FILE}
-        COMMAND   move ARGS "/Y" "${PROJECT_SOURCE_DIR}\\include\\${TARGET_DIR}\\*.cpp" "${PROJECT_SOURCE_DIR}\\src\\${TARGET_DIR}"
+        COMMAND   move ARGS "/Y" "${PATH_FOR_MOVE}" "${PROJECT_SOURCE_DIR}\\src\\${TARGET_DIR}"
         TARGET    ${PROJECT_NAME}
-        OUTPUTS   ${HEADER_XML_FILE_OUTPUT} ${SOURCE_XML_FILE_OUTPUT})
+        OUTPUTS   ${HEADER_XML_FILE_OUTPUT} "src/${SOURCE_XML_FILE_OUTPUT}")
 ELSE()
   ADD_CUSTOM_COMMAND(
         SOURCE    ${XSD_FILE}
         COMMAND   ${XSD_BIN} ARGS cxx-tree --generate-doxygen --generate-ostream --hxx-suffix .h --cxx-suffix .cpp --root-element-first --generate-polymorphic --generate-serialization --output-dir include/${TARGET_DIR} ${XSD_FILE}
-        COMMAND   mv ARGS "-f" "${PROJECT_SOURCE_DIR}/include/${TARGET_DIR}/*.cpp" "${PROJECT_SOURCE_DIR}/src/${TARGET_DIR}/"
+        COMMAND   mv ARGS "-f" "${PROJECT_SOURCE_DIR}/include/${SOURCE_XML_FILE_OUTPUT}" "${PROJECT_SOURCE_DIR}/src/${TARGET_DIR}/"
         TARGET    ${PROJECT_NAME}
-        OUTPUTS   ${HEADER_XML_FILE_OUTPUT} ${SOURCE_XML_FILE_OUTPUT})
+        OUTPUTS   ${HEADER_XML_FILE_OUTPUT} "src/${SOURCE_XML_FILE_OUTPUT}")
 ENDIF()
 ADD_CUSTOM_COMMAND(
       SOURCE    ${PROJECT_NAME}
       TARGET    ${PROJECT_NAME}
-      DEPENDS   ${HEADER_XML_FILE_OUTPUT} ${SOURCE_XML_FILE_OUTPUT})
+      DEPENDS   ${HEADER_XML_FILE_OUTPUT} "src/${SOURCE_XML_FILE_OUTPUT}")
 
 endmacro(GENERATE_XSD_FILES)
 
--- cmake/FindMATIO.cmake
+++ cmake/FindMATIO.cmake
@@ -6,11 +6,11 @@
 FIND_PATH(MATIO_INCLUDE_DIR matio.h ${MATIO_ROOT}/src ${MATIO_ROOT}/include /usr/local/include /usr/include)
 
 if(WIN64)
-  FIND_LIBRARY(MATIO_LIBRARY NAMES agros2d_3rd_party_matio matio PATHS ${MATIO_ROOT} ${MATIO_ROOT}/lib/x64 ${MATIO_ROOT})
+  FIND_LIBRARY(MATIO_LIBRARY NAMES agros2d_3rd_party_matio matio libmatio PATHS ${MATIO_ROOT} ${MATIO_ROOT}/lib/x64)
 else(WIN64)  
-  FIND_LIBRARY(MATIO_LIBRARY NAMES agros2d_3rd_party_matio matio PATHS ${MATIO_ROOT} ${MATIO_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
+  FIND_LIBRARY(MATIO_LIBRARY NAMES agros2d_3rd_party_matio matio libmatio PATHS ${MATIO_ROOT} ${MATIO_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
 endif(WIN64)
 
 # Report the found libraries, quit with fatal error if any required library has not been found.
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(BSON DEFAULT_MSG MATIO_LIBRARY MATIO_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MATIO DEFAULT_MSG MATIO_LIBRARY MATIO_INCLUDE_DIR)
