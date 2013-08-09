1bc0432f96705f3b00b715c4c417ee6eaef65b39
Lukas Korous <lukas.korous@gmail.com>
2013-08-09 13:49:22 +0200

    Move a mixin from mixins to matrix where it belongs.

--- hermes_common/include/mixins.h
+++ hermes_common/include/mixins.h
@@ -29,14 +29,6 @@
 
 namespace Hermes
 {
-  namespace Algebra
-  {
-    template<typename Scalar> class SparseMatrix;
-    template<typename Scalar> class Vector;
-    enum MatrixExportFormat;
-  }
-
-
   /** \defgroup g_mixins Mixins
   *  \brief Mixins are utility classes used for all kinds of other classes.
   *
@@ -172,77 +164,6 @@ namespace Hermes
 
       /// Verbose callback.
       callbackFn verbose_callback;
-    };
-
-    /// \ingroup g_mixins2d
-    /// Mixin that interfaces linear algebra structures output.
-    template<typename Scalar>
-    class HERMES_API MatrixRhsOutput
-    {
-    public:
-      /// Constructor.
-      /// Sets defaults (see individual set methods for values of those).
-      MatrixRhsOutput();
-
-      /// Processes the matrix.
-      void process_matrix_output(Hermes::Algebra::SparseMatrix<Scalar>* matrix, int iteration);
-      void process_matrix_output(Hermes::Algebra::SparseMatrix<Scalar>* matrix);
-
-      /// Processes the matrix.
-      void process_vector_output(Hermes::Algebra::Vector<Scalar>* rhs, int iteration);
-      void process_vector_output(Hermes::Algebra::Vector<Scalar>* rhs);
-
-      /// Sets this instance to output the matrix in several first iterations.
-      /// \param[in] firstIterations Only during so many first iterations. Default: -1 meaning, that during all iterations, the matrix will be saved.
-      void output_matrix(int firstIterations = -1);
-      /// Sets this instance to output matrix entries even though they are zero or not.
-      void set_print_zero_matrix_entries(bool to_set);
-      /// Sets filename for the matrix
-      /// Default: Matrix_'iteration number' with the ".m" extension in the case of matlab format.
-      /// \param[in] name sets the main part of the name, i.e. replacement for "Matrix_" in the default name.
-      void set_matrix_filename(std::string name);
-      /// Sets varname for the matrix
-      /// Default: "A".
-      void set_matrix_varname(std::string name);
-      /// Sets varname for the matrix
-      /// Default: "DF_MATLAB_SPARSE - matlab file".
-      void set_matrix_dump_format(Hermes::Algebra::MatrixExportFormat format);
-      /// Sets number format for the matrix output.
-      /// Default: "%lf".
-      void set_matrix_number_format(char* number_format);
-
-      /// Sets this instance to output the rhs in several first iterations.
-      /// \param[in] firstIterations Only during so many first iterations. Default: -1 meaning, that during all iterations, the rhs will be saved.
-      void output_rhs(int firstIterations = -1);
-      /// Sets filename for the rhs
-      /// Default: Rhs_'iteration number' with the ".m" extension in the case of matlab format.
-      /// \param[in] name sets the main part of the name, i.e. replacement for "Rhs_" in the default name.
-      void set_rhs_filename(std::string name);
-      /// Sets varname for the rhs
-      /// Default: "b".
-      void set_rhs_varname(std::string name);
-      /// Sets varname for the rhs
-      /// Default: "DF_MATLAB_SPARSE - matlab file".
-      void set_rhs_E_matrix_dump_format(Hermes::Algebra::MatrixExportFormat format);
-      /// Sets number format for the vector output.
-      /// Default: "%lf".
-      void set_rhs_number_format(char* number_format);
-
-    protected:
-      bool print_matrix_zero_values;
-      bool output_matrixOn;
-      int output_matrixIterations;
-      std::string matrixFilename;
-      std::string matrixVarname;
-      Hermes::Algebra::MatrixExportFormat matrixFormat;
-      char* matrix_number_format;
-
-      bool output_rhsOn;
-      int output_rhsIterations;
-      std::string RhsFilename;
-      std::string RhsVarname;
-      Hermes::Algebra::MatrixExportFormat RhsFormat;
-      char* rhs_number_format;
     };
 
     /// \brief Class using time measurement
