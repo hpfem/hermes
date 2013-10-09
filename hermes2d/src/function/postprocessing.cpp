94f3f81a5ea4d44a2ffb288f5da6189b79f7889c
Lukas Korous <lukas.korous@gmail.com>
2013-10-09 16:10:00 +0200

    Fix integral calculation.

--- hermes2d/src/function/postprocessing.cpp
+++ hermes2d/src/function/postprocessing.cpp
@@ -680,60 +680,62 @@ namespace Hermes
             Traverse::State* current_state = states[state_i];
             bool target_marker = false;
             for (int i = 0; i < internal_markers.size(); i++)
+            {
               if (current_state->e[0]->marker == internal_markers[i])
               {
                 target_marker = true;
                 break;
               }
-              if (target_marker)
-                continue;
+            }
+            if (!target_marker)
+              continue;
 
-              memset(result_local, 0, sizeof(Scalar)* this->number_of_integrals);
+            memset(result_local, 0, sizeof(Scalar)* this->number_of_integrals);
 
-              // Set active element.
-              for (int i = 0; i < source_functions_size; i++)
-              {
-                source_fuctions_cloned[i]->set_active_element(current_state->e[i]);
-                source_fuctions_cloned[i]->set_transform(current_state->sub_idx[i]);
-                refmap->set_active_element(current_state->e[i]);
-              }
+            // Set active element.
+            for (int i = 0; i < source_functions_size; i++)
+            {
+              source_fuctions_cloned[i]->set_active_element(current_state->e[i]);
+              source_fuctions_cloned[i]->set_transform(current_state->sub_idx[i]);
+              refmap->set_active_element(current_state->e[i]);
+            }
 
-              // Integration order.
-              Hermes::Ord order = Hermes::Ord(refmap->get_inv_ref_order());
-              memset(orders, 0, sizeof(nullptr) * this->number_of_integrals);
-              for (int i = 0; i < source_functions_size; i++)
-                func_ord[i] = init_fn_ord(this->source_functions[i]->get_fn_order());
+            // Integration order.
+            Hermes::Ord order = Hermes::Ord(refmap->get_inv_ref_order());
+            memset(orders, 0, sizeof(nullptr) * this->number_of_integrals);
+            for (int i = 0; i < source_functions_size; i++)
+              func_ord[i] = init_fn_ord(this->source_functions[i]->get_fn_order());
 
-              this->order(func_ord, orders);
+            this->order(func_ord, orders);
 
-              for (int i = 0; i < source_functions_size; i++)
-                order += orders[i];
+            for (int i = 0; i < source_functions_size; i++)
+              order += orders[i];
 
-              int order_int = order.get_order();
-              limit_order(order_int, refmap->get_active_element()->get_mode());
+            int order_int = order.get_order();
+            limit_order(order_int, refmap->get_active_element()->get_mode());
 
-              for (int i = 0; i < source_functions_size; i++)
-                func[i] = init_fn(source_fuctions_cloned[i], order_int);
+            for (int i = 0; i < source_functions_size; i++)
+              func[i] = init_fn(source_fuctions_cloned[i], order_int);
 
-              Geom<double>* geometry = init_geom_vol(refmap, order_int);
-              int n = init_geometry_points(&refmap, 1, order_int, geometry, jacobian_x_weights);
+            Geom<double>* geometry = init_geom_vol(refmap, order_int);
+            int n = init_geometry_points(&refmap, 1, order_int, geometry, jacobian_x_weights);
 
-              this->integral(n, jacobian_x_weights, func, geometry, result_local);
+            this->integral(n, jacobian_x_weights, func, geometry, result_local);
 
-              geometry->free();
-              delete geometry;
-              delete[] jacobian_x_weights;
+            geometry->free();
+            delete geometry;
+            delete[] jacobian_x_weights;
 
-              for (int i = 0; i < source_functions_size; i++)
-              {
-                func_ord[i]->free_ord();
-                delete func_ord[i];
-                func[i]->free_fn();
-                delete func[i];
-              }
+            for (int i = 0; i < source_functions_size; i++)
+            {
+              func_ord[i]->free_ord();
+              delete func_ord[i];
+              func[i]->free_fn();
+              delete func[i];
+            }
 
-              for (int i = 0; i < this->number_of_integrals; i++)
-                result_thread_local[i] += result_local[i];
+            for (int i = 0; i < this->number_of_integrals; i++)
+              result_thread_local[i] += result_local[i];
           }
 
           this->add_results(result_thread_local, result);
@@ -833,52 +835,54 @@ namespace Hermes
 
               bool target_marker = false;
               for (int i = 0; i < internal_markers.size(); i++)
+              {
                 if (current_state->e[0]->en[edge]->marker == internal_markers[i])
                 {
                   target_marker = true;
                   break;
                 }
-                if (target_marker)
-                  continue;
+              }
+              if (!target_marker)
+                continue;
 
-                memset(result_local, 0, sizeof(Scalar)* this->number_of_integrals);
+              memset(result_local, 0, sizeof(Scalar)* this->number_of_integrals);
 
-                // Integration order.
-                memset(orders, 0, sizeof(nullptr) * this->number_of_integrals);
-                for (int i = 0; i < source_functions_size; i++)
-                  func_ord[i] = init_fn_ord(this->source_functions[i]->get_fn_order());
+              // Integration order.
+              memset(orders, 0, sizeof(nullptr) * this->number_of_integrals);
+              for (int i = 0; i < source_functions_size; i++)
+                func_ord[i] = init_fn_ord(this->source_functions[i]->get_fn_order());
 
-                this->order(func_ord, orders);
+              this->order(func_ord, orders);
 
-                for (int i = 0; i < source_functions_size; i++)
-                  order += orders[i];
+              for (int i = 0; i < source_functions_size; i++)
+                order += orders[i];
 
-                int order_int = order.get_order();
-                limit_order(order_int, refmap->get_active_element()->get_mode());
+              int order_int = order.get_order();
+              limit_order(order_int, refmap->get_active_element()->get_mode());
 
-                double3* tan;
-                Geom<double>* geometry;
-                int n = init_surface_geometry_points(&refmap, 1, order_int, edge, current_state->e[0]->en[edge]->marker, geometry, jacobian_x_weights);
+              double3* tan;
+              Geom<double>* geometry;
+              int n = init_surface_geometry_points(&refmap, 1, order_int, edge, current_state->e[0]->en[edge]->marker, geometry, jacobian_x_weights);
 
-                for (int i = 0; i < source_functions_size; i++)
-                  func[i] = init_fn(source_fuctions_cloned[i], order_int);
+              for (int i = 0; i < source_functions_size; i++)
+                func[i] = init_fn(source_fuctions_cloned[i], order_int);
 
-                this->integral(n, jacobian_x_weights, func, geometry, result_local);
+              this->integral(n, jacobian_x_weights, func, geometry, result_local);
 
-                geometry->free();
-                delete geometry;
-                delete[] jacobian_x_weights;
+              geometry->free();
+              delete geometry;
+              delete[] jacobian_x_weights;
 
-                for (int i = 0; i < source_functions_size; i++)
-                {
-                  func_ord[i]->free_ord();
-                  delete func_ord[i];
-                  func[i]->free_fn();
-                  delete func[i];
-                }
+              for (int i = 0; i < source_functions_size; i++)
+              {
+                func_ord[i]->free_ord();
+                delete func_ord[i];
+                func[i]->free_fn();
+                delete func[i];
+              }
 
-                for (int i = 0; i < this->number_of_integrals; i++)
-                  result_thread_local[i] += .5 * result_local[i];
+              for (int i = 0; i < this->number_of_integrals; i++)
+                result_thread_local[i] += .5 * result_local[i];
             }
           }
 
