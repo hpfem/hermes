ae7bf3a515bc925b00c402e8779fad9ecc848b31
Lukas Korous <lukas.korous@gmail.com>
2013-12-13 13:30:29 +0100

    A temporary fix of the strange error - DO NOT MERGE.

--- hermes2d/src/quadrature/limit_order.cpp
+++ hermes2d/src/quadrature/limit_order.cpp
@@ -79,7 +79,9 @@ namespace Hermes
 
     HERMES_API void limit_order(int& o, ElementMode2D mode)
     {
-      if(o > g_quad_2d_std.get_safe_max_order(mode))
+      o = 24;
+      return;
+      if (o >= g_quad_2d_std.get_safe_max_order(mode))
       {
         o = g_quad_2d_std.get_safe_max_order(mode);
         warn_order();
