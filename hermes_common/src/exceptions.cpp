5a7c1d526f066700e3e0bc339f9348424aa8cb44
Lukas Korous <lukas.korous@gmail.com>
2014-01-06 12:55:45 +0100

    Fix the use of deleted fn.

--- hermes_common/src/exceptions.cpp
+++ hermes_common/src/exceptions.cpp
@@ -52,7 +52,7 @@ namespace Hermes
 
     void Exception::print_msg() const
     {
-      printf("Exception: %s\n", message);
+      printf("Exception: %s\n", message.str());
     }
 
     const char * Exception::what() const throw()
