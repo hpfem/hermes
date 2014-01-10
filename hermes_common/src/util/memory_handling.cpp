2223db8fbd7b401c95e4ad2f5974405987a8bef6
Lukas Korous <lukas.korous@gmail.com>
2014-01-10 18:21:23 +0100

    Work on PJLIB.

---
+++ hermes_common/src/util/memory_handling.cpp
@@ -0,0 +1,29 @@
+// This file is part of HermesCommon
+//
+// Copyright (c) 2009 hp-FEM group at the University of Nevada, Reno (UNR).
+// Email: hpfem-group@unr.edu, home page: http://hpfem.org/.
+//
+// Hermes2D is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation; either version 2 of the License,
+// or (at your option) any later version.
+//
+// Hermes2D is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Hermes2D; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+/*! \file memory_handling.cpp
+\brief File containing global PJLIB functionality.
+*/
+#include "memory_handling.h"
+namespace Hermes
+{
+  #ifdef WITH_PJLIB
+  HERMES_COMMON_API pj_caching_pool HermesCommonMemoryPoolCache;
+  HERMES_COMMON_API GlobalPoolCache hermesCommonGlobalPoolCache;
+  #endif
+}
