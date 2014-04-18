32053fec06ea12f2eaee4953f8dc48d3003118e9
Lukas Korous <lukas.korous@gmail.com>
2014-04-18 18:01:16 +0200

    Fixing libiberty.

--- cmake/FindLIBIBERTY.cmake
+++ cmake/FindLIBIBERTY.cmake
@@ -7,7 +7,7 @@ FIND_PATH(LIBIBERTY_INCLUDE_DIR libiberty.h ${LIBIBERTY_ROOT}/include/libiberty 
 if(WIN64)
     FIND_LIBRARY(LIBIBERTY_LIBRARY NAMES libiberty PATHS ${LIBIBERTY_ROOT}/lib/x64 ${LIBIBERTY_ROOT}/lib)
 else(WIN64) 
-    FIND_LIBRARY(LIBIBERTY_LIBRARY NAMES libiberty PATHS ${LIBIBERTY_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
+    FIND_LIBRARY(LIBIBERTY_LIBRARY NAMES libiberty.a PATHS ${LIBIBERTY_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 /usr/lib/x86_64-linux-gnu /usr/lib/x86-linux-gnu)
 endif(WIN64)
     
 # Report the found libraries, quit with fatal error if any required library has not been found.
