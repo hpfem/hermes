0e8c1a09b459e93650e101c0c5eac4cc87a04905
Milan Hanus <mhanus@kma.zcu.cz>
2014-06-04 09:13:13 -0500

    Simplification of OSX cmake.

--- OSX.cmake
+++ OSX.cmake
@@ -1,75 +1,39 @@
 #
 
-## ADDED BY COLMAN 
+## ADDED BY COLMAN
+
+## MODIFIED BY MILAN 
 
 ## BLAS
 FIND_LIBRARY(BLAS_LIBRARY NAMES blas  PATHS /usr/lib)
 
-IF (${BLAS_LIBRARY} MATCHES ".*libblas.dylib")
-    EXECUTE_PROCESS(COMMAND file ${BLAS_LIBRARY} OUTPUT_VARIABLE RESULT)
-            
-    IF (${RESULT} MATCHES ".*i386.*")
-        MESSAGE(STATUS "Using ${BLAS_LIBRARY} with architecture i386")
-    ELSE ()
-        MESSAGE(FATAL_ERROR "BLAS_LIBRARY ${BLAS_LIBRARY} is NOT i386!")
-    ENDIF ()
-ELSE ()
-    MESSAGE(FATAL_ERROR "BLAS_LIBRARY was not found!")
-ENDIF()
-
 IF (H2D_WITH_GLUT)
    
     ## OSMesa
-    FIND_LIBRARY(MESA_LIBRARY NAMES OSMesa PATHS /usr/X11/lib)
-    IF (${MESA_LIBRARY} MATCHES ".*libOSMesa.dylib")
-        EXECUTE_PROCESS(COMMAND file ${MESA_LIBRARY} OUTPUT_VARIABLE RESULT)
-            
-        IF (${RESULT} MATCHES ".*i386.*")
-            MESSAGE(STATUS "Using ${MESA_LIBRARY} with architecture i386")
-        ELSE ()
-            MESSAGE(FATAL_ERROR "MESA_LIBRARY ${MESA_LIBRARY} is NOT i386!")
-        ENDIF ()
-    ELSE ()
-        MESSAGE(FATAL_ERROR "MESA_LIBRARY was not found!")
-    ENDIF()
+    FIND_LIBRARY(MESA_LIBRARY NAMES OSMesa PATHS /usr/X11/lib /opt/local/lib)
 
     ## GLU 
-    FIND_LIBRARY(GLU_LIBRARY NAMES GLU PATHS /usr/X11/lib)
-    IF (${GLU_LIBRARY} MATCHES ".*libGLU.dylib")
-        EXECUTE_PROCESS(COMMAND file ${GLU_LIBRARY} OUTPUT_VARIABLE RESULT)
-            
-        IF (${RESULT} MATCHES ".*i386.*")
-            MESSAGE(STATUS "Using ${GLU_LIBRARY} with architecture i386")
-        ELSE ()
-            MESSAGE(FATAL_ERROR "GLU_LIBRARY ${MESA_LIBRARY} is NOT i386!")
-        ENDIF ()
-    ELSE ()
-        MESSAGE(FATAL_ERROR "GLU_LIBRARY was not found!")
-    ENDIF()
+    FIND_LIBRARY(GLU_LIBRARY NAMES GLU PATHS /usr/X11/lib /opt/local/lib)
 
     SET (ADDITIONAL_LIBS ${MESA_LIBRARY} ${GLU_LIBRARY})
 
     ## FIND_LIBRARY does not work here
-    FIND_FILE(GLUT_FILE "libglut.dylib" /usr/lib)
+    FIND_FILE(GLUT_FILE "libglut.dylib" /usr/lib /opt/local/lib)
 
     IF (${GLUT_FILE} MATCHES ".*libglut.dylib")
         SET (GLUT_LIBRARY ${GLUT_FILE} CACHE STRING "libglut shows strange behaviour" FORCE)
         EXECUTE_PROCESS(COMMAND file ${GLUT_LIBRARY} OUTPUT_VARIABLE RESULT)
 
-        IF (${RESULT} MATCHES ".*i386.*")
-            MESSAGE(STATUS "Using ${GLUT_LIBRARY} with architecture i386")
-            
-            ## The output is seen as one string
-            EXECUTE_PROCESS(COMMAND "nm" ${GLUT_LIBRARY} OUTPUT_VARIABLE RESULT)
-            STRING(REGEX MATCH ".*T([ ])+_glutSetOption.*" SYMBOL ${RESULT})
-            IF (${SYMBOL} STREQUAL ${RESULT})
-                MESSAGE(STATUS "SYMBOL _glutSetOption found and defined")
-            ELSE()
-                MESSAGE(FATAL_ERROR "SYMBOL _glutSetOption not found or not defined")
-            ENDIF()
-        ELSE ()
-            MESSAGE(FATAL_ERROR "GLUT_LIBRARY ${BLAS_LIBRARY} is NOT i386!")
-        ENDIF ()
+        MESSAGE(STATUS "Using ${GLUT_LIBRARY} as ${RESULT}")
+        
+        ## The output is seen as one string
+        EXECUTE_PROCESS(COMMAND "nm" ${GLUT_LIBRARY} OUTPUT_VARIABLE RESULT)
+        STRING(REGEX MATCH ".*T([ ])+_glutSetOption.*" SYMBOL ${RESULT})
+        IF (${SYMBOL} STREQUAL ${RESULT})
+            MESSAGE(STATUS "SYMBOL _glutSetOption found and defined")
+        ELSE()
+            MESSAGE(FATAL_ERROR "SYMBOL _glutSetOption not found or not defined")
+        ENDIF()
     ELSE ()
         MESSAGE(FATAL_ERROR "GLUT_LIBRARY was not found! ${GLUT_LIBRARY}")
     ENDIF()
@@ -80,13 +44,6 @@ IF (H2D_WITH_GLUT)
 
     IF (${GLEW_FILE} MATCHES ".*libGLEW.dylib")
         SET (GLEW_LIBRARY ${GLEW_FILE} CACHE STRING "libGLEW shows strange behaviour" FORCE)
-        EXECUTE_PROCESS(COMMAND file ${GLEW_LIBRARY} OUTPUT_VARIABLE RESULT)
-            
-        IF (${RESULT} MATCHES ".*i386.*")
-            MESSAGE(STATUS "Using ${GLEW_LIBRARY} with architecture i386")
-        ELSE ()
-            MESSAGE(FATAL_ERROR "GLEW_LIBRARY ${GLEW_LIBRARY} is NOT i386!")
-        ENDIF ()
     ELSE ()
         MESSAGE(FATAL_ERROR "GLEW_LIBRARY was not found!")
     ENDIF()
