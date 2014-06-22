d6b509d938ad098dd29c000715e189737ec1bfbe
Lukas Korous <lukas.korous@gmail.com>
2014-06-23 01:54:35 +0200

    Fix GL linking.

--- cmake/FindGLUT.cmake
+++ cmake/FindGLUT.cmake
@@ -4,10 +4,10 @@
 
 if(WIN64)
   FIND_LIBRARY(GLUT_LIBRARY NAMES freeglut glut PATHS ${GLUT_ROOT}/lib/x64 ${GLUT_ROOT}/lib)
-  FIND_LIBRARY(GL_LIBRARY NAMES gl PATHS ${GLUT_ROOT}/lib/x64 ${GLUT_ROOT}/lib)
+  FIND_LIBRARY(GL_LIBRARY NAMES GL PATHS ${GLUT_ROOT}/lib/x64 ${GLUT_ROOT}/lib)
 else(WIN64)  
   FIND_LIBRARY(GLUT_LIBRARY NAMES freeglut glut PATHS ${GLUT_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
-  FIND_LIBRARY(GL_LIBRARY NAMES gl PATHS ${GLUT_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
+  FIND_LIBRARY(GL_LIBRARY NAMES GL PATHS ${GLUT_ROOT}/lib /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64)
 endif(WIN64)
 
 INCLUDE(FindPackageHandleStandardArgs)
